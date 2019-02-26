84e29962b1ddcea85ef512ef558b3984e2f6727e
CyrusNajmabadi <cyrus.najmabadi@gmail.com>
2019-02-25 20:30:45 -0800

    Make it simpler to run go tests consistently (#344)

--- build/common.mk
+++ build/common.mk
@@ -112,6 +112,7 @@ default::
 ifneq ($(SUB_PROJECTS),)
 only_build:: $(SUB_PROJECTS:%=%_only_build)
 only_test:: $(SUB_PROJECTS:%=%_only_test)
+only_test_fast:: $(SUB_PROJECTS:%=%_only_test_fast)
 default:: $(SUB_PROJECTS:%=%_default)
 all:: $(SUB_PROJECTS:%=%_all)
 ensure:: $(SUB_PROJECTS:%=%_ensure)
@@ -172,6 +173,10 @@ install::
 	yarn link
 endif
 
+only_build:: build install
+only_test:: lint test_all
+only_test_fast:: lint test_fast
+
 # Generate targets for each sub project. This project's default and
 # all targets will depend on the sub project's targets, and the
 # individual targets for sub projects are added as a convience when
@@ -193,6 +198,12 @@ $(SUB_PROJECTS:%=%_install):
 	@$(MAKE) -C ./$(@:%_install=%) install
 $(SUB_PROJECTS:%=%_test_all):
 	@$(MAKE) -C ./$(@:%_test_all=%) test_all
+$(SUB_PROJECTS:%=%_only_build):
+	@$(MAKE) -C ./$(@:%_only_build=%) only_build
+$(SUB_PROJECTS:%=%_only_test):
+	@$(MAKE) -C ./$(@:%_only_test=%) only_test
+$(SUB_PROJECTS:%=%_only_test_fast):
+	@$(MAKE) -C ./$(@:%_only_test_fast=%) only_test_fast
 endif
 
 # As a convinece, we provide a format target that folks can build to
