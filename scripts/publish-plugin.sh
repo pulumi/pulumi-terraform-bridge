7f463166086722f4516e774350365b0d107e0089
James Nugent <james@jen20.com>
2019-05-16 17:07:35 -0500

    Specialize the publish-plugin.sh script

--- scripts/publish-plugin.sh
+++ scripts/publish-plugin.sh
@@ -6,7 +6,7 @@ set -o nounset -o errexit -o pipefail
 ROOT=$(dirname $0)/..
 WORK_PATH=$(mktemp -d)
 VERSION=$(jq -r '.version' < "${ROOT}/sdk/nodejs/bin/package.json")
-PLUGIN_PACKAGE_NAME="pulumi-resource-kubernetes-${VERSION}-$(go env GOOS)-$(go env GOARCH).tar.gz"
+PLUGIN_PACKAGE_NAME="pulumi-resource-terraform-${VERSION}-$(go env GOOS)-$(go env GOARCH).tar.gz"
 PLUGIN_PACKAGE_DIR="$(mktemp -d)"
 PLUGIN_PACKAGE_PATH="${PLUGIN_PACKAGE_DIR}/${PLUGIN_PACKAGE_NAME}"
 
@@ -17,9 +17,9 @@ if [ "$(go env GOOS)" = "windows" ]; then
 fi
 
 go build \
-   -ldflags "-X github.com/pulumi/pulumi-kubernetes/pkg/version.Version=${VERSION}" \
-   -o "${WORK_PATH}/pulumi-resource-kubernetes${BIN_SUFFIX}" \
-   "${ROOT}/cmd/pulumi-resource-kubernetes"
+   -ldflags "-X github.com/pulumi/pulumi-terraform/pkg/version.Version=${VERSION}" \
+   -o "${WORK_PATH}/pulumi-resource-terraform${BIN_SUFFIX}" \
+   "${ROOT}/cmd/pulumi-resource-terraform"
 
 # Tar up the plugin
 tar -czf ${PLUGIN_PACKAGE_PATH} -C ${WORK_PATH} .
@@ -27,13 +27,15 @@ tar -czf ${PLUGIN_PACKAGE_PATH} -C ${WORK_PATH} .
 # rel.pulumi.com is in our production account, so assume that role first
 CREDS_JSON=$(aws sts assume-role \
                  --role-arn "arn:aws:iam::058607598222:role/UploadPulumiReleases" \
-                 --role-session-name "upload-plugin-pulumi-resource-kubernetes" \
+                 --role-session-name "upload-plugin-pulumi-resource-aws" \
                  --external-id "upload-pulumi-release")
 
 # Use the credentials we just assumed
 export AWS_ACCESS_KEY_ID=$(echo ${CREDS_JSON}     | jq ".Credentials.AccessKeyId" --raw-output)
 export AWS_SECRET_ACCESS_KEY=$(echo ${CREDS_JSON} | jq ".Credentials.SecretAccessKey" --raw-output)
 export AWS_SECURITY_TOKEN=$(echo ${CREDS_JSON}    | jq ".Credentials.SessionToken" --raw-output)
+
+echo "Uploading ${PLUGIN_PACKAGE_NAME}..."
 
 aws s3 cp --only-show-errors "${PLUGIN_PACKAGE_PATH}" "s3://rel.pulumi.com/releases/plugins/${PLUGIN_PACKAGE_NAME}"
 
