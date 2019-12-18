782c819193ac0b219e7c638c681d4e8d596a3803
James Nugent <james@jen20.com>
2019-12-18 12:59:19 +0100

    Obtain documentation from the Go Module Cache
    This obviates the need for using gomod-doccopy during builds, and paves
    the way to not require vendoring at all for builds. Currently this loses
    the path caching which was recently introduced - this will be
    re-introduced after some downstream testing.

--- go.mod
+++ go.mod
@@ -5,9 +5,6 @@ go 1.12
 replace git.apache.org/thrift.git => github.com/apache/thrift v0.12.0
 
 require (
-	cloud.google.com/go/logging v1.0.0 // indirect
-	github.com/Azure/azure-amqp-common-go v1.1.4 // indirect
-	github.com/apache/thrift v0.12.0 // indirect
 	github.com/blang/semver v3.5.1+incompatible
 	github.com/gedex/inflector v0.0.0-20170307190818-16278e9db813
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
@@ -17,7 +14,6 @@ require (
 	github.com/hashicorp/hcl2 v0.0.0-20190821123243-0c888d1241f6
 	github.com/hashicorp/terraform-plugin-sdk v1.0.0
 	github.com/mitchellh/copystructure v1.0.0
-	github.com/openzipkin/zipkin-go v0.1.6 // indirect
 	github.com/pkg/errors v0.8.1
 	github.com/prometheus/client_golang v0.9.3-0.20190127221311-3c4408c8b829 // indirect
 	github.com/pulumi/pulumi v1.9.1
@@ -31,6 +27,7 @@ require (
 	github.com/xdg/stringprep v1.0.0 // indirect
 	go.mongodb.org/mongo-driver v1.0.1 // indirect
 	go.uber.org/atomic v1.3.2 // indirect
+	golang.org/x/mod v0.2.0
 	golang.org/x/net v0.0.0-20190926025831-c00fd9afed17
 	google.golang.org/grpc v1.24.0
 )
---
+++ pkg/tfgen/gomod.go
@@ -0,0 +1,100 @@
+// Copyright 2016-2019, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tfgen
+
+import (
+	"fmt"
+	"go/build"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/pkg/errors"
+	"golang.org/x/mod/modfile"
+)
+
+func LoadGoMod() (*modfile.File, error) {
+	exePath, err := os.Getwd()
+	if err != nil {
+		return nil, errors.Wrap(err, "error determining working directory")
+	}
+
+	moduleRoot := findModuleRoot(exePath)
+	if moduleRoot == "" {
+		return nil, errors.New("cannot find module root")
+	}
+
+	gomodContent, err := ioutil.ReadFile(filepath.Join(moduleRoot, "go.mod"))
+	if err != nil {
+		return nil, errors.Wrap(err, "error reading go.mod")
+	}
+
+	file, err := modfile.Parse("go.mod", gomodContent, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "error parsing go.mod")
+	}
+
+	return file, nil
+}
+
+func FindEffectiveModuleForImportPath(file *modfile.File, importPath string) (string, string, error) {
+	// If the module has been replaced, we need the replacement path and version
+	for _, rep := range file.Replace {
+		if rep.Old.Path != importPath {
+			continue
+		}
+
+		return rep.New.Path, rep.New.Version, nil
+	}
+
+	// Otherwise find the required version
+	for _, req := range file.Require {
+		if req.Mod.Path != importPath {
+			continue
+		}
+
+		return req.Mod.Path, req.Mod.Version, nil
+	}
+
+	return "", "", fmt.Errorf("%q does not appear to be required by go.mod", importPath)
+}
+
+func GetModuleRoot(repo string, version string) string {
+	gopath := os.Getenv("GOPATH")
+	if gopath == "" {
+		gopath = build.Default.GOPATH
+	}
+
+	baseAndVersion := fmt.Sprintf("%s@%s", filepath.Base(repo), version)
+	return filepath.Join(gopath, "pkg", "mod", filepath.Dir(repo), baseAndVersion)
+}
+
+// Copyright 2018 The Go Authors. - Taken from src/cmd/go/internal/modload/init.go
+func findModuleRoot(dir string) (root string) {
+	dir = filepath.Clean(dir)
+
+	// Look for enclosing go.mod.
+	for {
+		if fi, err := os.Stat(filepath.Join(dir, "go.mod")); err == nil && !fi.IsDir() {
+			return dir
+		}
+		d := filepath.Dir(dir)
+		if d == dir {
+			break
+		}
+		dir = d
+	}
+	return ""
+}
