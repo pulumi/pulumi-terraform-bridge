146117e84b99adee12026db1ca2cb9c4ead38811
Erin Krengel <ekrengel@pulumi.com>
2020-04-15 14:50:09 -0700

    create processArgumentReferenceSection fxn and add tests

--- pkg/tfgen/docs_test.go
+++ pkg/tfgen/docs_test.go
@@ -59,3 +59,80 @@ func TestURLRewrite(t *testing.T) {
 		assert.Equal(t, test.Expected, text)
 	}
 }
+
+func TestArgumentRegex(t *testing.T) {
+	tests := []struct {
+		input    []string
+		expected map[string]*argument
+	}{
+		{
+			input: []string{
+				"* `iam_instance_profile` - (Optional) The IAM Instance Profile to",
+				"launch the instance with. Specified as the name of the Instance Profile. Ensure your credentials have the correct permission to assign the instance profile according to the [EC2 documentation](http://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles_use_switch-role-ec2.html#roles-usingrole-ec2instance-permissions), notably `iam:PassRole`.",
+				"* `ipv6_address_count`- (Optional) A number of IPv6 addresses to associate with the primary network interface. Amazon EC2 chooses the IPv6 addresses from the range of your subnet.",
+				"* `ipv6_addresses` - (Optional) Specify one or more IPv6 addresses from the range of the subnet to associate with the primary network interface",
+				"* `tags` - (Optional) A mapping of tags to assign to the resource.",
+			},
+			expected: map[string]*argument{
+				"iam_instance_profile": &argument{
+					description: "The IAM Instance Profile to" + "\n" +
+						"launch the instance with. Specified as the name of the Instance Profile. Ensure your credentials have the correct permission to assign the instance profile according to the [EC2 documentation](http://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles_use_switch-role-ec2.html#roles-usingrole-ec2instance-permissions), notably `iam:PassRole`.",
+				},
+				"ipv6_address_count": &argument{
+					description: "A number of IPv6 addresses to associate with the primary network interface. Amazon EC2 chooses the IPv6 addresses from the range of your subnet.",
+				},
+				"ipv6_addresses": &argument{
+					description: "Specify one or more IPv6 addresses from the range of the subnet to associate with the primary network interface",
+				},
+				"tags": &argument{
+					description: "A mapping of tags to assign to the resource.",
+				},
+			},
+		},
+		{
+			input: []string{
+				"* `jwt_configuration` - (Optional) The configuration of a JWT authorizer. Required for the `JWT` authorizer type.",
+				"Supported only for HTTP APIs.",
+				"",
+				"The `jwt_configuration` object supports the following:",
+				"",
+				"* `audience` - (Optional) A list of the intended recipients of the JWT. A valid JWT must provide an aud that matches at least one entry in this list.",
+				"* `issuer` - (Optional) The base domain of the identity provider that issues JSON Web Tokens, such as the `endpoint` attribute of the [`aws_cognito_user_pool`](/docs/providers/aws/r/cognito_user_pool.html) resource.",
+			},
+			expected: map[string]*argument{
+				"jwt_configuration": &argument{
+					description: "The configuration of a JWT authorizer. Required for the `JWT` authorizer type." + "\n" +
+						"Supported only for HTTP APIs.",
+					arguments: map[string]string{
+						"audience": "A list of the intended recipients of the JWT. A valid JWT must provide an aud that matches at least one entry in this list.",
+						"issuer":   "The base domain of the identity provider that issues JSON Web Tokens, such as the `endpoint` attribute of the [`aws_cognito_user_pool`](/docs/providers/aws/r/cognito_user_pool.html) resource.",
+					},
+				},
+				"audience": &argument{
+					description: "A list of the intended recipients of the JWT. A valid JWT must provide an aud that matches at least one entry in this list.",
+					isNested:    true,
+				},
+				"issuer": &argument{
+					description: "The base domain of the identity provider that issues JSON Web Tokens, such as the `endpoint` attribute of the [`aws_cognito_user_pool`](/docs/providers/aws/r/cognito_user_pool.html) resource.",
+					isNested:    true,
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		ret := parsedDoc{
+			Arguments: make(map[string]*argument),
+		}
+		processArgumentReferenceSection(tt.input, &ret)
+
+		assert.Len(t, ret.Arguments, len(tt.expected))
+		for k, v := range tt.expected {
+			actualArg := ret.Arguments[k]
+			assert.NotNil(t, actualArg)
+			assert.Equal(t, v.description, actualArg.description)
+			assert.Equal(t, v.isNested, actualArg.isNested)
+			assert.Equal(t, v.arguments, actualArg.arguments)
+		}
+	}
+}
