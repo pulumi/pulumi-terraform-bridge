pulumi/pulumi-terraform-bridge

commit f53ae642830b219332d08c24934305afb2f06fb4

Adopt change from pulumi/pulumi#1853 (#242)

pkg/tfbridge/provider.go
@@ -92,11 +92,11 @@ func (p *Provider) configMod() tokens.Module     { return p.baseConfigMod() + "/
 func (p *Provider) setLoggingContext(ctx context.Context) {
 	log.SetOutput(&LogRedirector{
 		writers: map[string]func(string) error{
-			tfTracePrefix: func(msg string) error { return p.host.Log(ctx, diag.Debug, "", msg) },
-			tfDebugPrefix: func(msg string) error { return p.host.Log(ctx, diag.Debug, "", msg) },
-			tfInfoPrefix:  func(msg string) error { return p.host.Log(ctx, diag.Info, "", msg) },
-			tfWarnPrefix:  func(msg string) error { return p.host.Log(ctx, diag.Warning, "", msg) },
-			tfErrorPrefix: func(msg string) error { return p.host.Log(ctx, diag.Error, "", msg) },
+			tfTracePrefix: func(msg string) error { return p.host.Log(ctx, diag.Debug, "", msg, false) },
+			tfDebugPrefix: func(msg string) error { return p.host.Log(ctx, diag.Debug, "", msg, false) },
+			tfInfoPrefix:  func(msg string) error { return p.host.Log(ctx, diag.Info, "", msg, false) },
+			tfWarnPrefix:  func(msg string) error { return p.host.Log(ctx, diag.Warning, "", msg, false) },
+			tfErrorPrefix: func(msg string) error { return p.host.Log(ctx, diag.Error, "", msg, false) },
 		},
 	})
 }
@@ -233,7 +233,8 @@ func (p *Provider) Configure(ctx context.Context, req *pulumirpc.ConfigureReques
 	// Perform validation of the config state so we can offer nice errors.
 	warns, errs := p.tf.Validate(config)
 	for _, warn := range warns {
-		if err = p.host.Log(ctx, diag.Warning, "", fmt.Sprintf("provider config warning: %v", warn)); err != nil {
+		if err = p.host.Log(ctx, diag.Warning, "",
+			fmt.Sprintf("provider config warning: %v", warn), false); err != nil {
 			return nil, err
 		}
 	}
@@ -297,7 +298,8 @@ func (p *Provider) Check(ctx context.Context, req *pulumirpc.CheckRequest) (*pul
 	}
 	warns, errs := p.tf.ValidateResource(tfname, rescfg)
 	for _, warn := range warns {
-		if err = p.host.Log(ctx, diag.Warning, urn, fmt.Sprintf("%v verification warning: %v", urn, warn)); err != nil {
+		if err = p.host.Log(ctx, diag.Warning, urn,
+			fmt.Sprintf("%v verification warning: %v", urn, warn), false); err != nil {
 			return nil, err
 		}
 	}
@@ -617,7 +619,7 @@ func (p *Provider) Invoke(ctx context.Context, req *pulumirpc.InvokeRequest) (*p
 	}
 	warns, errs := p.tf.ValidateDataSource(tfname, rescfg)
 	for _, warn := range warns {
-		if err = p.host.Log(ctx, diag.Warning, "", fmt.Sprintf("%v verification warning: %v", tok, warn)); err != nil {
+		if err = p.host.Log(ctx, diag.Warning, "", fmt.Sprintf("%v verification warning: %v", tok, warn), false); err != nil {
 			return nil, err
 		}
 	}

pkg/tfgen/docs.go
@@ -66,7 +66,7 @@ func getDocsForProvider(provider string, kind DocKind, rawname string, docinfo *
 	markdownByts, err := readMarkdown(repo, kind, possibleMarkdownNames)
 	if err != nil {
 		cmdutil.Diag().Warningf(
-			diag.Message("", "Could not find docs for resource %v; consider overriding doc source location"), rawname)
+			diag.Message("", "Could not find docs for resource %v; consider overriding doc source location"), false, rawname)
 		return parsedDoc{}, nil
 	}
 	doc := parseTFMarkdown(kind, string(markdownByts), provider, rawname)
@@ -152,7 +152,7 @@ func parseTFMarkdown(kind DocKind, markdown string, provider string, rawname str
 		lines := strings.Split(section, "\n")
 		if len(lines) == 0 {
 			cmdutil.Diag().Warningf(
-				diag.Message("", "Unparseable doc section for  %v; consider overriding doc source location"), rawname)
+				diag.Message("", "Unparseable doc section for  %v; consider overriding doc source location"), false, rawname)
 		}
 		switch lines[0] {
 		case "Arguments Reference", "Argument Reference", "Nested Blocks", "Nested blocks":
@@ -197,7 +197,7 @@ func parseTFMarkdown(kind DocKind, markdown string, provider string, rawname str
 			subparts := strings.Split(section, "\n# ")
 			if len(subparts) != 2 {
 				cmdutil.Diag().Warningf(
-					diag.Message("", "Expected only a single H1 in markdown for resource %v"), rawname)
+					diag.Message("", "Expected only a single H1 in markdown for resource %v"), false, rawname)
 			}
 			sublines := strings.Split(subparts[1], "\n")
 			ret.Description += strings.Join(sublines[2:], "\n")

pkg/tfgen/generate.go
@@ -451,7 +451,7 @@ func (g *generator) gatherConfig() *module {
 	for key := range custom {
 		if _, has := cfg[key]; !has {
 			cmdutil.Diag().Warningf(
-				diag.Message("", "custom config schema %s was not present in the Terraform metadata"), key)
+				diag.Message("", "custom config schema %s was not present in the Terraform metadata"), false, key)
 		}
 	}
 
@@ -489,7 +489,7 @@ func (g *generator) gatherResources() (moduleMap, error) {
 		if info == nil {
 			// if this resource was missing, issue a warning and skip it.
 			cmdutil.Diag().Warningf(
-				diag.Message("", "resource %s not found in provider map; skipping"), r)
+				diag.Message("", "resource %s not found in provider map; skipping"), false, r)
 			continue
 		}
 		seen[r] = true
@@ -517,7 +517,7 @@ func (g *generator) gatherResources() (moduleMap, error) {
 		if !seen[name] {
 			cmdutil.Diag().Warningf(
 				diag.Message("", "resource %s (%s) wasn't found in the Terraform module; possible name mismatch?"),
-				name, g.info.Resources[name].Tok)
+				false, name, g.info.Resources[name].Tok)
 		}
 	}
 
@@ -609,7 +609,7 @@ func (g *generator) gatherResource(rawname string,
 		if _, has := schema.Schema[key]; !has {
 			cmdutil.Diag().Warningf(
 				diag.Message("", "custom resource schema %s.%s was not present in the Terraform metadata"),
-				name, key)
+				false, name, key)
 		}
 	}
 
@@ -639,7 +639,7 @@ func (g *generator) gatherDataSources() (moduleMap, error) {
 		if dsinfo == nil {
 			// if this data source was missing, issue a warning and skip it.
 			cmdutil.Diag().Warningf(
-				diag.Message("", "data source %s not found in provider map; skipping"), ds)
+				diag.Message("", "data source %s not found in provider map; skipping"), false, ds)
 			continue
 		}
 		seen[ds] = true
@@ -667,7 +667,7 @@ func (g *generator) gatherDataSources() (moduleMap, error) {
 		if !seen[name] {
 			cmdutil.Diag().Warningf(
 				diag.Message("", "data source %s (%s) wasn't found in the Terraform module; possible name mismatch?"),
-				name, g.info.DataSources[name].Tok)
+				false, name, g.info.DataSources[name].Tok)
 		}
 	}
 

commit 1e825e054bf89f27f632dd1bad29a11ad3a5d6a8

Ensure PreConfigureCallback gets called
It appears this was regressed by https://github.com/pulumi/pulumi-terraform-bridge/pull/242, which partially reverted https://github.com/pulumi/pulumi-terraform-bridge/pull/225 but left in some code that doesn't appear to be correct without the other reverted code (it's not immediately clear why it's correct even with that original code - but at least it seems intentional as part of the original change).

Also added tests to ensure that PreConfigureCallback is called.

Fixes #1316.

pkg/tfbridge/provider.go
@@ -524,24 +524,21 @@ func (p *Provider) Configure(ctx context.Context,
 		return nil, errors.Wrap(err, "could not marshal config state")
 	}
 
-	if req.Variables == nil {
-		// We only follow this path if the CLI hasbn't already called CheckConfig
-		if p.info.PreConfigureCallback != nil {
-			if err = p.info.PreConfigureCallback(vars, config); err != nil {
-				return nil, err
-			}
-		}
-
-		missingKeys, validationErrors := validateProviderConfig(ctx, p, config)
-		if len(missingKeys) > 0 {
-			err = rpcerror.WithDetails(
-				rpcerror.New(codes.InvalidArgument, "required configuration keys were missing"),
-				&pulumirpc.ConfigureErrorMissingKeys{MissingKeys: missingKeys})
+	if p.info.PreConfigureCallback != nil {
+		if err = p.info.PreConfigureCallback(vars, config); err != nil {
 			return nil, err
 		}
-		if validationErrors != nil {
-			return nil, validationErrors
-		}
+	}
+
+	missingKeys, validationErrors := validateProviderConfig(ctx, p, config)
+	if len(missingKeys) > 0 {
+		err = rpcerror.WithDetails(
+			rpcerror.New(codes.InvalidArgument, "required configuration keys were missing"),
+			&pulumirpc.ConfigureErrorMissingKeys{MissingKeys: missingKeys})
+		return nil, err
+	}
+	if validationErrors != nil {
+		return nil, validationErrors
 	}
 
 	// Now actually attempt to do the configuring and return its resulting error (if any).

pkg/tfbridge/provider_test.go
@@ -2,6 +2,7 @@ package tfbridge
 
 import (
 	"context"
+	"errors"
 	"sort"
 	"testing"
 
@@ -517,4 +518,36 @@ func TestProviderCheckV2(t *testing.T) {
 	assert.Equal(t, "", failures[1].Property)
 	assert.Equal(t, "Required attribute is not set", failures[2].Reason)
 	assert.Equal(t, "", failures[2].Property)
+}
+
+func testProviderPreConfigureCallback(t *testing.T, provider *Provider) {
+	expectedErr := errors.New("failedToPreConfigure")
+	provider.info = ProviderInfo{
+		PreConfigureCallback: func(vars resource.PropertyMap, config shim.ResourceConfig) error {
+			return expectedErr
+		},
+	}
+	_, err := provider.Configure(context.Background(), &pulumirpc.ConfigureRequest{
+		Variables: map[string]string{
+			"foo:config:bar": "invalid",
+		},
+	})
+	assert.Equal(t, expectedErr, err)
+}
+
+func TestProviderPreConfigureCallbackV1(t *testing.T) {
+	provider := &Provider{
+		tf:     shimv1.NewProvider(testTFProvider),
+		config: shimv1.NewSchemaMap(testTFProvider.Schema),
+	}
+	testProviderPreConfigureCallback(t, provider)
+
+}
+
+func TestProviderPreConfigureCallbackV2(t *testing.T) {
+	provider := &Provider{
+		tf:     shimv2.NewProvider(testTFProviderV2),
+		config: shimv2.NewSchemaMap(testTFProviderV2.Schema),
+	}
+	testProviderPreConfigureCallback(t, provider)
 }