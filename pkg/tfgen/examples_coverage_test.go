70c2b7273497383ce1ecd1fec8f532e57d58a23b
Vova Ivanov <jetvova@gmail.com>
2021-11-23 12:42:33 -0800

    Changed Diag sink creation

--- pkg/tfgen/examples_coverage_test.go
+++ pkg/tfgen/examples_coverage_test.go
@@ -18,8 +18,11 @@ package tfgen
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
+	"github.com/pulumi/pulumi/sdk/go/common/diag"
+	"github.com/pulumi/pulumi/sdk/go/common/diag/colors"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -27,73 +30,26 @@ func Test_HclConversion(t *testing.T) {
 
 	//============================= HCL code to be given to the converter =============================//
 	hcl := `
-	data "azurerm_client_config" "current" {}
-
-	resource "azurerm_resource_group" "example" {
-	  name     = "example-resources"
-	  location = "West Europe"
-	}
-	
-	resource "azurerm_key_vault" "example" {
-	  name                = "examplekv"
-	  location            = azurerm_resource_group.example.location
-	  resource_group_name = azurerm_resource_group.example.name
-	  tenant_id           = data.azurerm_client_config.current.tenant_id
-	  sku_name            = "standard"
-	
-	  purge_protection_enabled = true
-	}
-	
-	resource "azurerm_key_vault_access_policy" "storage" {
-	  key_vault_id = azurerm_key_vault.example.id
-	  tenant_id    = data.azurerm_client_config.current.tenant_id
-	  object_id    = azurerm_storage_account.example.identity.0.principal_id
-	
-	  key_permissions    = ["get", "create", "list", "restore", "recover", "unwrapkey", "wrapkey", "purge", "encrypt", "decrypt", "sign", "verify"]
-	  secret_permissions = ["get"]
-	}
-	
-	resource "azurerm_key_vault_access_policy" "client" {
-	  key_vault_id = azurerm_key_vault.example.id
-	  tenant_id    = data.azurerm_client_config.current.tenant_id
-	  object_id    = data.azurerm_client_config.current.object_id
-	
-	  key_permissions    = ["get", "create", "delete", "list", "restore", "recover", "unwrapkey", "wrapkey", "purge", "encrypt", "decrypt", "sign", "verify"]
-	  secret_permissions = ["get"]
-	}
-	
-	
-	resource "azurerm_key_vault_key" "example" {
-	  name         = "tfex-key"
-	  key_vault_id = azurerm_key_vault.example.id
-	  key_type     = "RSA"
-	  key_size     = 2048
-	  key_opts     = ["decrypt", "encrypt", "sign", "unwrapKey", "verify", "wrapKey"]
-	
-	  depends_on = [
-		azurerm_key_vault_access_policy.client,
-		azurerm_key_vault_access_policy.storage,
-	  ]
-	}
-	
-	
-	resource "azurerm_storage_account" "example" {
-	  name                     = "examplestor"
-	  resource_group_name      = azurerm_resource_group.example.name
-	  location                 = azurerm_resource_group.example.location
-	  account_tier             = "Standard"
-	  account_replication_type = "GRS"
-	
-	  identity {
-		type = "SystemAssigned"
+	resource "aws_vpc" "vpc" {
+		cidr_block = "10.0.0.0/16"
 	  }
-	}
-	
-	resource "azurerm_storage_account_customer_managed_key" "example" {
-	  storage_account_id = azurerm_storage_account.example.id
-	  key_vault_id       = azurerm_key_vault.example.id
-	  key_name           = azurerm_key_vault_key.example.name
-	}
+	  
+	  resource "aws_vpn_gateway" "vpn_gateway" {
+		vpc_id = aws_vpc.vpc.id
+	  }
+	  
+	  resource "aws_customer_gateway" "customer_gateway" {
+		bgp_asn    = 65000
+		ip_address = "172.0.0.1"
+		type       = "ipsec.1"
+	  }
+	  
+	  resource "aws_vpn_connection" "main" {
+		vpn_gateway_id      = aws_vpn_gateway.vpn_gateway.id
+		customer_gateway_id = aws_customer_gateway.customer_gateway.id
+		type                = "ipsec.1"
+		static_routes_only  = true
+	  }
 	`
 	//=================================================================================================//
 
@@ -107,6 +63,9 @@ func Test_HclConversion(t *testing.T) {
 		Debug:        false,
 		SkipDocs:     false,
 		SkipExamples: false,
+		Sink: diag.DefaultSink(os.Stdout, os.Stderr, diag.FormatOptions{
+			Color: colors.Never,
+		}),
 	})
 	assert.NoError(t, err, "Failed to create generator")
 
