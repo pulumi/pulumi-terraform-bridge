1367779d53dbcc48edf56ff774bca32a4fe31d71
Josh Kodroff <josh@pulumi.com>
2021-11-30 13:42:29 -0500

    Add boilerplate functions.
    Adds functions to tfbridge that were in the Terraform provider
    boilerplate code so that they are not repeated in each provider and have
    a consistent implementation.

--- pkg/tfbridge/info_test.go
+++ pkg/tfbridge/info_test.go
@@ -1,8 +1,10 @@
 package tfbridge
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetModuleMajorVersion(t *testing.T) {
@@ -34,4 +36,29 @@ func TestGetModuleMajorVersion(t *testing.T) {
 		majorVersion := GetModuleMajorVersion(test.version)
 		assert.Equal(t, test.expectedMajorVersion, majorVersion)
 	}
+}
+
+func TestMakeMember(t *testing.T) {
+	assert.Equal(t, "package:module:member", MakeMember("package", "module", "member").String())
+}
+
+func TestMakeType(t *testing.T) {
+	assert.Equal(t, "package:module:member", MakeType("package", "module", "member").String())
+}
+
+func TestMakeDataSource(t *testing.T) {
+	assert.Equal(t, "package:module/getSomething:getSomething", MakeDataSource("package", "module", "getSomething").String())
+	assert.Equal(t, "package:module/getSomething:GetSomething", MakeDataSource("package", "module", "GetSomething").String())
+}
+
+func TestMakeResource(t *testing.T) {
+	assert.Equal(t, "package:module/myResource:MyResource", MakeResource("package", "module", "MyResource").String())
+}
+
+func TestStringValue(t *testing.T) {
+	myMap := map[resource.PropertyKey]resource.PropertyValue{
+		"key1": {V: "value1"},
+	}
+	assert.Equal(t, "value1", StringValue(myMap, "key1"))
+	assert.Equal(t, "", StringValue(myMap, "keyThatDoesNotExist"))
 }
