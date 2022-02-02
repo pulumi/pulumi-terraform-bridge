3ecab346e11faa087a1b36962518ae0bd77f4600
Pat Gavlin <pat@pulumi.com>
2022-02-02 13:00:00 -0800

    expand a test

--- pkg/tfbridge/schema_test.go
+++ pkg/tfbridge/schema_test.go
@@ -1396,6 +1396,7 @@ func TestExtractInputsFromOutputs(t *testing.T) {
 			"input_i": {Type: schemav1.TypeString, Required: true},
 			"inout_j": {Type: schemav1.TypeString, Optional: true, Computed: true},
 			"inout_k": {Type: schemav1.TypeString, Optional: true, Computed: true, Default: "inout_k_default"},
+			"inout_l": {Type: schemav1.TypeBool, Optional: true, Computed: true, Default: true},
 		},
 		func(d *schemav1.ResourceData, meta interface{}) ([]*schemav1.ResourceData, error) {
 			return []*schemav1.ResourceData{d}, nil
@@ -1421,6 +1422,7 @@ func TestExtractInputsFromOutputs(t *testing.T) {
 		set(d, "input_i", "")
 		set(d, "inout_j", "")
 		set(d, "inout_k", "")
+		set(d, "inout_l", false)
 		return nil
 	}
 	tfres.Create = func(d *schemav1.ResourceData, meta interface{}) error {
@@ -1481,6 +1483,7 @@ func TestExtractInputsFromOutputs(t *testing.T) {
 		"inputI":  "",
 		"inoutJ":  "",
 		"inoutK":  "",
+		"inoutL":  false,
 	}), outs)
 
 	ins, err := plugin.UnmarshalProperties(resp.GetInputs(), plugin.MarshalOptions{})
@@ -1492,6 +1495,7 @@ func TestExtractInputsFromOutputs(t *testing.T) {
 		"inoutD":    "inout_d_read",
 		"inputI":    "",
 		"inoutK":    "",
+		"inoutL":    false,
 	})
 	assert.Equal(t, expected, ins)
 
@@ -1518,7 +1522,7 @@ func TestExtractInputsFromOutputs(t *testing.T) {
 	assert.NoError(t, err)
 	sortDefaultsList(checkedIns)
 	assert.Equal(t, resource.NewPropertyMapFromMap(map[string]interface{}{
-		defaultsKey: []interface{}{"inoutD", "inoutK", "inputF", "inputH"},
+		defaultsKey: []interface{}{"inoutD", "inoutK", "inoutL", "inputF", "inputH"},
 		"inputA":    "input_a_create",
 		"inoutD":    "inout_d_default",
 		"inputE": map[string]interface{}{
@@ -1528,6 +1532,7 @@ func TestExtractInputsFromOutputs(t *testing.T) {
 		"inputF": "input_f_default",
 		"inputH": "Input_H_Default",
 		"inoutK": "inout_k_default",
+		"inoutL": true,
 	}), checkedIns)
 
 	// Step 2: create a resource using the checked input bag. The inputs should be smuggled along with the state.
@@ -1551,6 +1556,7 @@ func TestExtractInputsFromOutputs(t *testing.T) {
 		"outputG": "output_g_create",
 		"inputH":  "input_h_default",
 		"inoutK":  "inout_k_default",
+		"inoutL":  true,
 	}), outs)
 
 	// Step 3: read the resource we just created. The read should make the following changes to the inputs:
@@ -1559,6 +1565,10 @@ func TestExtractInputsFromOutputs(t *testing.T) {
 	//   the new state matches the value in the olds state.
 	// - "inoutD" should change from "inout_d_default" to "inout_d_read", and should no longer be present in the list
 	//   of properties that were populated from defaults.
+	// - "inoutK" should change from "inout_k_default" to "", and should no longer be present in the list of
+	//   properties that were populated from defaults.
+	// - "inoutL" should change from true to false, and should no longer be present in the list of properties that
+	//   were populated from defaults.
 	resp, err = p.Read(context.Background(), &pulumirpc.ReadRequest{
 		Id:         string(id),
 		Urn:        string(urn),
@@ -1583,6 +1593,7 @@ func TestExtractInputsFromOutputs(t *testing.T) {
 		"inputI":  "",
 		"inoutJ":  "",
 		"inoutK":  "",
+		"inoutL":  false,
 	}), outs)
 
 	ins, err = plugin.UnmarshalProperties(resp.GetInputs(), plugin.MarshalOptions{})
@@ -1598,6 +1609,7 @@ func TestExtractInputsFromOutputs(t *testing.T) {
 		"inputF": "input_f_default",
 		"inputH": "Input_H_Default",
 		"inoutK": "",
+		"inoutL": false,
 	}), ins)
 
 	// Step 3a. delete the default annotations from the checked inputs and re-run the read. No default annotations
@@ -1631,6 +1643,7 @@ func TestExtractInputsFromOutputs(t *testing.T) {
 		"inputI":  "",
 		"inoutJ":  "",
 		"inoutK":  "",
+		"inoutL":  false,
 	}), outs)
 
 	ins, err = plugin.UnmarshalProperties(resp.GetInputs(), plugin.MarshalOptions{})
@@ -1644,6 +1657,7 @@ func TestExtractInputsFromOutputs(t *testing.T) {
 		"inputF": "input_f_default",
 		"inputH": "Input_H_Default",
 		"inoutK": "",
+		"inoutL": false,
 	}), ins)
 
 }
