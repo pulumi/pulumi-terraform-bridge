8be029495eebd50838bc381a99a80410c1647bae
stack72 <public@paulstack.co.uk>
2022-08-02 13:08:00 +0100

    [internal] Fixup linting issue

--- pkg/tf2pulumi/convert/convert.go
+++ pkg/tf2pulumi/convert/convert.go
@@ -48,7 +48,8 @@ const (
 )
 
 var (
-	ValidLanguages = []string{LanguageTypescript, LanguagePulumi, LanguagePython, LanguageCSharp, LanguageGo, LanguageJava, LanguageYaml}
+	ValidLanguages = []string{LanguageTypescript, LanguagePulumi, LanguagePython, LanguageCSharp,
+		LanguageGo, LanguageJava, LanguageYaml}
 )
 
 type Diagnostics struct {
