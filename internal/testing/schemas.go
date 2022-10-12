e6a63698152e9cbe64a30450bb6d0b6890be3970
Anton Tayanovskyy <anton@pulumi.com>
2022-10-12 16:01:15 -0400

    Make testing robust to Git windows checkouts with CRLF

--- internal/testing/schemas.go
+++ internal/testing/schemas.go
@@ -18,23 +18,36 @@ func AssertPackageSpecEquals(
 	spec pschema.PackageSpec,
 ) {
 
+	unmarshalSchema := func(r io.Reader) (s pschema.PackageSpec, err error) {
+		err = json.NewDecoder(r).Decode(&s)
+		return
+	}
+
+	readSchemaFromFile := func(file string) (pschema.PackageSpec, error) {
+		jsonBytes, err := os.ReadFile(file)
+		if err != nil {
+			return pschema.PackageSpec{}, err
+		}
+		return unmarshalSchema(bytes.NewBuffer(jsonBytes))
+	}
+
 	marshalSchema := func(s pschema.PackageSpec, w io.Writer) error {
 		enc := json.NewEncoder(w)
 		enc.SetIndent("", "  ")
 		return enc.Encode(s)
 	}
 
-	assertPackageSpecMatchesFile := func(spec pschema.PackageSpec, file string) {
-		expectedBytes, err := os.ReadFile(file)
-		assert.NoError(t, err)
-		expected := string(expectedBytes)
-
+	schemaToString := func(s pschema.PackageSpec) string {
 		buf := bytes.Buffer{}
-		err = marshalSchema(spec, &buf)
+		err := marshalSchema(s, &buf)
 		assert.NoError(t, err)
-		actual := buf.String()
+		return buf.String()
+	}
 
-		assert.Equal(t, expected, actual)
+	assertPackageSpecMatchesFile := func(actualSpec pschema.PackageSpec, file string) {
+		expectedSpec, err := readSchemaFromFile(file)
+		assert.NoError(t, err)
+		assert.Equal(t, schemaToString(expectedSpec), schemaToString(actualSpec))
 	}
 
 	if os.Getenv("PULUMI_ACCEPT") != "" {
