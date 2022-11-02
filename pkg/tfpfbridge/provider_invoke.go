720a6c84fd33eaf1a53f4e0aba5ae49d58ab1ed9
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2022-11-02 15:11:52 -0400

    Try up the code a bit

---
+++ pkg/tfpfbridge/provider_invoke.go
@@ -0,0 +1,27 @@
+// Copyright 2016-2022, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tfbridge
+
+import (
+	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/resource/plugin"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/tokens"
+)
+
+// Invoke dynamically executes a built-in function in the provider.
+func (p *Provider) Invoke(tok tokens.ModuleMember,
+	args resource.PropertyMap) (resource.PropertyMap, []plugin.CheckFailure, error) {
+	panic("TODO")
+}
