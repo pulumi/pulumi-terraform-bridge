5506b80289b968159a188e8180a3f21df92c671b
Anton Tayanovskyy <anton@pulumi.com>
2022-11-07 15:15:28 -0500

    Update testdata to gen separate preview files and not clobber them

--- pkg/tfpfbridge/tests/testdatagen/genrandom/generate.sh
+++ pkg/tfpfbridge/tests/testdatagen/genrandom/generate.sh
@@ -17,11 +17,15 @@ mkdir -p $OUT
 pulumi stack init generate
 pulumi stack select generate
 pulumi config set min 1
-PULUMI_DEBUG_GRPC=$OUT/random-initial-update.json pulumi up --yes
-PULUMI_DEBUG_GRPC=$OUT/random-empty-update.json pulumi up --yes
+PULUMI_DEBUG_GRPC=$OUT/random-initial-preview.json pulumi preview
+PULUMI_DEBUG_GRPC=$OUT/random-initial-update.json pulumi up --yes --skip-preview
+PULUMI_DEBUG_GRPC=$OUT/random-empty-preview.json pulumi preview
+PULUMI_DEBUG_GRPC=$OUT/random-empty-update.json pulumi up --yes --skip-preview
 pulumi config set min 2
-PULUMI_DEBUG_GRPC=$OUT/random-replace.json pulumi up --yes
+PULUMI_DEBUG_GRPC=$OUT/random-replace-preview.json pulumi up --yes
+PULUMI_DEBUG_GRPC=$OUT/random-replace-update.json pulumi up --yes --skip-preview
 pulumi config set min 0
-PULUMI_DEBUG_GRPC=$OUT/random-delete.json pulumi up --yes
+PULUMI_DEBUG_GRPC=$OUT/random-delete-preview.json pulumi preview
+PULUMI_DEBUG_GRPC=$OUT/random-delete-update.json pulumi up --yes --skip-preview
 pulumi destroy --yes
 pulumi stack rm --yes
