692d07d63ad2a27a0fc469f761f59334a487fe89
Anton Tayanovskyy <anton@pulumi.com>
2022-11-11 18:15:07 -0500

    WIP test on nested type

---
+++ pkg/tfpfbridge/tests/nested_attr_test.go
@@ -0,0 +1,36 @@
+// Copyright 2016-2022, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tfbridgetests
+
+import (
+	"context"
+	"testing"
+
+	"github.com/pulumi/pulumi-terraform-bridge/pkg/tfpfbridge/pfutils"
+	"github.com/pulumi/pulumi-terraform-bridge/pkg/tfpfbridge/tests/internal/testprovider"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNestedType(t *testing.T) {
+	ctx := context.TODO()
+	info := testprovider.SyntheticTestBridgeProvider()
+	res, err := pfutils.GatherResources(ctx, info.P())
+	require.NoError(t, err)
+	testresTypeName := res.All()[0]
+
+	testresType := res.Schema(testresTypeName).Type().TerraformType(ctx)
+
+	t.Logf("%v", testresType)
+}
