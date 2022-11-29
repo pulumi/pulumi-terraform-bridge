27664ee5a98df09bf3a9165318886cc53c96bbd1
Anton Tayanovskyy <anton@pulumi.com>
2022-11-29 14:29:52 -0500

    Dedicated convert package

---
+++ pkg/tfpfbridge/internal/convert/map.go
@@ -0,0 +1,95 @@
+// Copyright 2016-2022, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package convert
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
+
+	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
+)
+
+type mapEncoder struct {
+	elementType    tftypes.Type
+	elementEncoder Encoder
+}
+
+type mapDecoder struct {
+	elementDecoder Decoder
+}
+
+func newMapEncoder(elementType tftypes.Type, elementEncoder Encoder) (Encoder, error) {
+	return &mapEncoder{
+		elementType:    elementType,
+		elementEncoder: elementEncoder,
+	}, nil
+}
+
+func newMapDecoder(elementDecoder Decoder) (Decoder, error) {
+	return &mapDecoder{
+		elementDecoder: elementDecoder,
+	}, nil
+}
+
+func (enc *mapEncoder) FromPropertyValue(p resource.PropertyValue) (tftypes.Value, error) {
+	mapTy := tftypes.Map{ElementType: enc.elementType}
+	if propertyValueIsUnkonwn(p) {
+		return tftypes.NewValue(mapTy, tftypes.UnknownValue), nil
+	}
+	if p.IsNull() {
+		return tftypes.NewValue(mapTy, nil), nil
+	}
+	if !p.IsObject() {
+		return tftypes.NewValue(mapTy, nil),
+			fmt.Errorf("Expected an Object PropertyValue")
+	}
+	values := map[string]tftypes.Value{}
+	for key, pv := range p.ObjectValue() {
+		v, err := enc.elementEncoder.FromPropertyValue(pv)
+		if err != nil {
+			return tftypes.NewValue(mapTy, nil),
+				fmt.Errorf("encMap failed on %v", pv)
+		}
+		values[string(key)] = v
+	}
+	return tftypes.NewValue(mapTy, values), nil
+}
+
+func (dec *mapDecoder) ToPropertyValue(v tftypes.Value) (resource.PropertyValue, error) {
+	if !v.IsKnown() {
+		zero := resource.NewObjectProperty(make(resource.PropertyMap))
+		return resource.NewComputedProperty(resource.Computed{Element: zero}), nil
+	}
+	if v.IsNull() {
+		return resource.NewPropertyValue(nil), nil
+	}
+	elements := map[string]tftypes.Value{}
+	if err := v.As(&elements); err != nil {
+		return resource.PropertyValue{},
+			fmt.Errorf("decMap fails with %s: %w", v.String(), err)
+	}
+
+	values := make(resource.PropertyMap)
+	for k, e := range elements {
+		ev, err := dec.elementDecoder.ToPropertyValue(e)
+		if err != nil {
+			return resource.PropertyValue{},
+				fmt.Errorf("decMap fails with %s: %w", e.String(), err)
+		}
+		values[resource.PropertyKey(k)] = ev
+	}
+	return resource.NewObjectProperty(values), nil
+}
---
+++ pkg/tfpfbridge/internal/convert/number.go
@@ -0,0 +1,66 @@
+// Copyright 2016-2022, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package convert
+
+import (
+	"fmt"
+	"math/big"
+
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
+
+	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
+)
+
+type numberEncoder struct{}
+type numberDecoder struct{}
+
+func newNumberEncoder() Encoder {
+	return &numberEncoder{}
+}
+
+func newNumberDecoder() Decoder {
+	return &numberDecoder{}
+}
+
+func (*numberEncoder) FromPropertyValue(p resource.PropertyValue) (tftypes.Value, error) {
+	if propertyValueIsUnkonwn(p) {
+		return tftypes.NewValue(tftypes.Number, tftypes.UnknownValue), nil
+	}
+	if p.IsNull() {
+		return tftypes.NewValue(tftypes.Number, nil), nil
+	}
+	if !p.IsNumber() {
+		return tftypes.NewValue(tftypes.Number, nil),
+			fmt.Errorf("Expected a Number")
+	}
+	return tftypes.NewValue(tftypes.Number, p.NumberValue()), nil
+}
+
+func (*numberDecoder) ToPropertyValue(v tftypes.Value) (resource.PropertyValue, error) {
+	if !v.IsKnown() {
+		unknown := resource.NewComputedProperty(resource.Computed{Element: resource.NewNumberProperty(0)})
+		return unknown, nil
+	}
+	if v.IsNull() {
+		return resource.NewPropertyValue(nil), nil
+	}
+	var n big.Float
+	if err := v.As(&n); err != nil {
+		return resource.PropertyValue{},
+			fmt.Errorf("decNumber fails with %s: %w", v.String(), err)
+	}
+	f64, _ := n.Float64()
+	return resource.NewNumberProperty(f64), nil
+}
