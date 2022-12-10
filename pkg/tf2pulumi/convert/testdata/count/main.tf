6efdc4d4f6df89c432844f5c826f510eb0d20f92
Fraser Waters <fraser@pulumi.com>
2022-12-10 19:42:41 +0000

    Add data sources to the count test (#679)

--- pkg/tf2pulumi/convert/testdata/count/main.tf
+++ pkg/tf2pulumi/convert/testdata/count/main.tf
@@ -11,3 +11,17 @@ output "some_output_a" {
 output "some_output_b" {
     value = simple_resource.a_resource_with_count[1].result
 }
+
+data "simple_data_source" "a_data_source_with_count" {
+    count = 2
+    input_one = "Hello ${count.index}"
+    input_two = true
+}
+
+output "some_output_c" {
+    value = data.simple_data_source.a_data_source_with_count[0].result
+}
+
+output "some_output_d" {
+    value = data.simple_data_source.a_data_source_with_count[1].result
+}
