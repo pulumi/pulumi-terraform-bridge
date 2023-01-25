ba70700a156392005a3cec3841c6610ac9a0c58a
Anton Tayanovskyy <anton@pulumi.com>
2023-01-25 14:08:34 -0500

    Rename package to tfpfbrdige to match folder name

--- pkg/tfpfbridge/provider_checkconfig.go
+++ pkg/tfpfbridge/provider_checkconfig.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package tfbridge
+package tfpfbridge
 
 import (
 	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
