431b1c6765bbec34fe8990bf5c9d80784be356e8
Fraser Waters <fraser@pulumi.com>
2023-03-06 19:49:10 +0000

    Annonate the failures in rename test and add one working case (#869)

--- pkg/tf2pulumi/convert/testdata/renames/main.tf
+++ pkg/tf2pulumi/convert/testdata/renames/main.tf
@@ -9,12 +9,12 @@ output "some_output_a" {
     value = renames_resource.a_resource.result
 }
 
-// This doesn't convert correctly
-// Error: unknown property 'theInnerString' among [innerString]
-// Error: cannot traverse value of type union(none, object({innerString = union(none, string)}, annotated(0xc000681c40)))
 //output "some_output_b" {
 //    value = data.renames_data_source.a_data_source.a_resource.inner_string
 //}
+// The above doesn't convert correctly
+// Error: unknown property 'theInnerString' among [innerString]
+// Error: cannot traverse value of type union(none, object({innerString = union(none, string)}, annotated(0xc000681c40)))
 
 data "renames_data_source" "a_data_source" {
     a_number = 2
@@ -27,7 +27,22 @@ output "some_output_c" {
     value = data.renames_data_source.a_data_source.result
 }
 
-// This doesn't convert correctly
-//output "some_output_d" {
-//    value = data.renames_data_source.a_data_source.a_resource.inner_string
-//}
+// output "some_output_d" {
+//     value = data.renames_data_source.a_data_source.a_resource.inner_string
+// }
+// The above doesn't convert correctly
+// unknown property 'theInnerString' among [innerString];
+
+resource "renames_resource" "many_resource" {
+    count = 2
+    a_number = 1
+    a_resource = {
+        inner_string = "hello"
+    }
+}
+
+// output "the_inner_strings" {
+//     value = renames_resource.many_resource[*].a_resource.inner_string
+// }
+// The above doesn't convert correctly
+// unknown property 'aResource' among [urn myResult theNumber theResource id];
