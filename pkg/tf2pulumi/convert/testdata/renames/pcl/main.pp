13a2f6d366266d95c280c8002f9b417525d79c90
Fraser Waters <fraser@pulumi.com>
2023-03-06 19:19:14 +0000

    Annonate the failures in rename test and add one working case

--- pkg/tf2pulumi/convert/testdata/renames/pcl/main.pp
+++ pkg/tf2pulumi/convert/testdata/renames/pcl/main.pp
@@ -6,12 +6,12 @@ resource aResource "renames:index/index:resource" {
 }
 output someOutputA {
     value = aResource.myResult
-}// This doesn't convert correctly
-// Error: unknown property 'theInnerString' among [innerString]
-// Error: cannot traverse value of type union(none, object({innerString = union(none, string)}, annotated(0xc000681c40)))
-//output "some_output_b" {
+}//output "some_output_b" {
 //    value = data.renames_data_source.a_data_source.a_resource.inner_string
 //}
+// The above doesn't convert correctly
+// Error: unknown property 'theInnerString' among [innerString]
+// Error: cannot traverse value of type union(none, object({innerString = union(none, string)}, annotated(0xc000681c40)))
 
 aDataSource = invoke("renames:index/index:data_source", {
     theNumber = 2,
@@ -21,4 +21,19 @@ aDataSource = invoke("renames:index/index:data_source", {
 })
 output someOutputC {
     value = aDataSource.myResult
+}// output "some_output_d" {
+//     value = data.renames_data_source.a_data_source.a_resource.inner_string
+// }
+// The above doesn't convert correctly
+// unknown property 'theInnerString' among [innerString];
+
+resource manyResource "renames:index/index:resource" {
+options {
+    range = 2
+
+}
+    theNumber = 1
+    theResource = {
+        theInnerString = "hello"
+    }
 }
