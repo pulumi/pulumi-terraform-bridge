6856e6cf38e67dece972ea4d47e534df01ac4d9b
Anton Tayanovskyy <anton@pulumi.com>
2023-03-21 12:43:27 -0400

    Pulumi 3.58.0 (#893)
    * Script upgrading refs
    
    * go run scripts/build.go update-pulumi-deps 3.58.0
    
    * Lint
    
    * Lint

--- scripts/build.go
+++ scripts/build.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	//"flag"
 	"fmt"
 	"log"
 	"os"
@@ -20,37 +19,79 @@ func main() {
 	switch os.Args[1] {
 	case "lint":
 		lintMain()
+	case "update-pulumi-deps":
+		updatePulumiDeps()
 	default:
 		usage()
 	}
 }
 
 func usage() {
-	fmt.Println("Usage: go run scripts/build.go CMD, supported commands: lint")
+	fmt.Println("Usage: go run scripts/build.go CMD, supported commands: lint, update-pulumi-deps")
 	os.Exit(1)
+}
+
+func updatePulumiDeps() {
+	ver := os.Args[2]
+	roots := findGoModuleRoots()
+	for _, m := range roots {
+		edited := false
+		if fileContains(filepath.Join(m, "go.mod"), "github.com/pulumi/pulumi/pkg/v3") {
+			execCommandOrLogFatal(m, "go", "mod", "edit", "-droprequire", "github.com/pulumi/pulumi/pkg/v3")
+			execCommandOrLogFatal(m, "go", "mod", "edit", "-require", "github.com/pulumi/pulumi/pkg/v3@v"+ver)
+			edited = true
+		}
+		if fileContains(filepath.Join(m, "go.mod"), "github.com/pulumi/pulumi/sdk/v3") {
+			execCommandOrLogFatal(m, "go", "mod", "edit", "-droprequire", "github.com/pulumi/pulumi/sdk/v3")
+			execCommandOrLogFatal(m, "go", "mod", "edit", "-require", "github.com/pulumi/pulumi/sdk/v3@v"+ver)
+			edited = true
+		}
+		if edited {
+			execCommandOrLogFatal(m, "go", "mod", "tidy")
+		}
+	}
+}
+
+func fileContains(path string, search string) bool {
+	b, _ := os.ReadFile(path)
+	return bytes.Contains(b, []byte(search))
 }
 
 func lintMain() {
 	roots := findGoModuleRoots()
 	failed := false
 	for _, m := range roots {
-		var stderr, stdout bytes.Buffer
-		cmd := exec.Command("golangci-lint", "run")
-		cmd.Dir = m
-		cmd.Stderr = &stderr
-		cmd.Stdout = &stdout
-		err := cmd.Run()
+		err := execCommand(m, "golangci-lint", "run")
 		if err != nil {
-			fmt.Printf("cd %s && golangci-lint run\n", m)
-			fmt.Println(stdout.String())
-			fmt.Println(stderr.String())
-			fmt.Println(err)
-			fmt.Println()
 			failed = true
 		}
 	}
 	if failed {
 		log.Fatalf("lint failed")
+	}
+}
+
+func execCommand(cwd, name string, arg ...string) error {
+	var stderr, stdout bytes.Buffer
+	cmd := exec.Command(name, arg...)
+	cmd.Dir = cwd
+	cmd.Stderr = &stderr
+	cmd.Stdout = &stdout
+	err := cmd.Run()
+	if err != nil {
+		fmt.Printf("cd %s && %s %s\n", cwd, name, strings.Join(arg, " "))
+		fmt.Println(stdout.String())
+		fmt.Println(stderr.String())
+		fmt.Println(err)
+		fmt.Println()
+	}
+	return err
+}
+
+func execCommandOrLogFatal(cwd, name string, arg ...string) {
+	err := execCommand(cwd, name, arg...)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
 
