64fc1f2bf6c848f37ffe1f997661f6e7b2611666
phillip edwards <phillipedwards02@gmail.com>
2023-03-23 10:27:26 -0700

    added comments about origin of method

--- pkg/tfshim/diagnostics/path.go
+++ pkg/tfshim/diagnostics/path.go
@@ -8,6 +8,12 @@ import (
 	"github.com/hashicorp/go-cty/cty"
 )
 
+// FormatCtyPath is a helper function to produce a user-friendly string
+// representation of a cty.Path. The result uses a syntax similar to the
+// HCL expression language in the hope of it being familiar to users.
+//
+// This code has been cribbed from [1].
+// [1]: https://github.com/terraform-linters/tflint/blob/ec43cd65cd943d71a48419a72bb8e20e6559dfd2/terraform/tfdiags/config_traversals.go#L14
 func formatCtyPath(path cty.Path) string {
 	var buf bytes.Buffer
 	for _, step := range path {
