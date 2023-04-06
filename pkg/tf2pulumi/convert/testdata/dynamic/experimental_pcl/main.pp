d2ebc284b875d0b2af991a8f1576cffc650a7e63
Fraser Waters <fraser@pulumi.com>
2023-04-06 17:55:37 +0100

    Add iterator test for dynamic blocks

--- pkg/tf2pulumi/convert/testdata/dynamic/experimental_pcl/main.pp
+++ pkg/tf2pulumi/convert/testdata/dynamic/experimental_pcl/main.pp
@@ -3,3 +3,13 @@ resource "aResource" "blocks:index/index:resource" {
     innerString = entry.value
   }]
 }
+resource "bResource" "blocks:index/index:resource" {
+  aListOfResources = [for entry in entries(["hi", "bye"]) : {
+    innerString = entry.value
+  }]
+}
+resource "cResource" "blocks:index/index:resource" {
+  aListOfResources = [for entry in entries(["hi", "bye"]) : {
+    innerString = entry.value
+  }]
+}
--- pkg/tf2pulumi/convert/testdata/dynamic/main.tf
+++ pkg/tf2pulumi/convert/testdata/dynamic/main.tf
@@ -10,4 +10,26 @@ resource "blocks_resource" "a_resource" {
     }
 }
 
+resource "blocks_resource" "b_resource" {
+    dynamic "a_list_of_resources" {
+        for_each = ["hi", "bye"]
+        iterator = "thing"
+
+        content {
+            inner_string = thing.value
+        }
+    }
+}
+
+resource "blocks_resource" "c_resource" {
+    dynamic "a_list_of_resources" {
+        for_each = ["hi", "bye"]
+        iterator = "each"
+
+        content {
+            inner_string = each.value
+        }
+    }
+}
+
 #endif
