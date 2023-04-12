9d63896aec2852bc937021e064bb634bc62d28c1
Anton Tayanovskyy <anton@pulumi.com>
2023-04-12 18:04:29 -0400

    Sketch out tfgen/extend as alternative to muxing schemas

---
+++ pf/tfgen/extend.go
@@ -0,0 +1,182 @@
+// Copyright 2016-2023, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tfgen
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+
+	"context"
+	"github.com/hashicorp/terraform-plugin-framework/provider"
+	"github.com/pulumi/pulumi-terraform-bridge/pf/internal/schemashim"
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
+	shim "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim"
+	shimSchema "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim/schema"
+)
+
+// Supports extending schema generation with resources and functions build against another framework.
+type Extension interface {
+	Extend(*tfbridge.ProviderInfo) (*tfbridge.ProviderInfo, error)
+}
+
+func SchemaOnlyPluginFrameworkProvider(ctx context.Context, provider provider.Provider) shim.Provider {
+	return schemashim.ShimSchemaOnlyProvider(ctx, provider)
+}
+
+func Extend(info *tfbridge.ProviderInfo, extensions ...Extension) (*tfbridge.ProviderInfo, error) {
+	copy := *info
+	for _, e := range extensions {
+		extended, err := e.Extend(&copy)
+		if err != nil {
+			return nil, err
+		}
+		copy = *extended
+	}
+	return &copy, nil
+}
+
+func NewResourceExtension(provider shim.Provider, resources map[string]*tfbridge.ResourceInfo) Extension {
+	return &extension{
+		provider:  provider,
+		resources: resources,
+	}
+}
+
+func NewDataSourceExtension(provider shim.Provider, dataSources map[string]*tfbridge.DataSourceInfo) Extension {
+	return &extension{
+		provider:    provider,
+		dataSources: dataSources,
+	}
+}
+
+type extension struct {
+	provider    shim.Provider
+	resources   map[string]*tfbridge.ResourceInfo
+	dataSources map[string]*tfbridge.DataSourceInfo
+}
+
+var _ Extension = (*extension)(nil)
+
+func (ext *extension) Extend(info *tfbridge.ProviderInfo) (*tfbridge.ProviderInfo, error) {
+	copy := *info
+
+	if ext.resources != nil {
+		resources, err := disjointUnion(copy.P.ResourcesMap(), ext.provider.ResourcesMap())
+		if err != nil {
+			return nil, fmt.Errorf("failing to apply a ResourceExtension, ResourcesMap maps are not dijsoint: %v", err)
+		}
+		resourceInfos, err := disjointMapUnion(copy.Resources, ext.resources)
+		if err != nil {
+			return nil, fmt.Errorf("failing to apply a ResourceExtension, Resources maps are not dijsoint: %v", err)
+		}
+		copy.P = &simpleSchemaProvider{
+			schema:      copy.P.Schema(),
+			resources:   resources,
+			dataSources: copy.P.DataSourcesMap(),
+		}
+		copy.Resources = resourceInfos
+	}
+
+	if ext.dataSources != nil {
+		dsMap, err := disjointUnion(copy.P.DataSourcesMap(), ext.provider.DataSourcesMap())
+		if err != nil {
+			return nil, fmt.Errorf("failing to apply a DataSourceExtension, datasource maps are not dijsoint: %v", err)
+		}
+		dsInfos, err := disjointMapUnion(copy.DataSources, ext.dataSources)
+		if err != nil {
+			return nil, fmt.Errorf("failing to apply a DataSoruceExtension, datasource maps are not dijsoint: %v", err)
+		}
+		copy.P = &simpleSchemaProvider{
+			schema:      copy.P.Schema(),
+			resources:   copy.P.ResourcesMap(),
+			dataSources: dsMap,
+		}
+		copy.DataSources = dsInfos
+	}
+
+	return &copy, nil
+}
+
+type simpleSchemaProvider struct {
+	schemashim.SchemaOnlyProvider
+	schema      shim.SchemaMap
+	resources   shim.ResourceMap
+	dataSources shim.ResourceMap
+}
+
+func (p *simpleSchemaProvider) Schema() shim.SchemaMap {
+	return p.schema
+}
+
+func (p *simpleSchemaProvider) ResourcesMap() shim.ResourceMap {
+	return p.resources
+}
+
+func (p *simpleSchemaProvider) DataSourcesMap() shim.ResourceMap {
+	return p.dataSources
+}
+
+var _ shim.Provider = (*simpleSchemaProvider)(nil)
+
+func disjointUnion(baseline, extension shim.ResourceMap) (shim.ResourceMap, error) {
+	union, err := disjointMapUnion(toResourceMap(baseline), toResourceMap(extension))
+	if err != nil {
+		return nil, err
+	}
+	return shimSchema.ResourceMap(union), nil
+}
+
+func toResourceMap(rmap shim.ResourceMap) shimSchema.ResourceMap {
+	m := map[string]shim.Resource{}
+	rmap.Range(func(key string, value shim.Resource) bool {
+		m[key] = value
+		return true
+	})
+	return m
+}
+
+func disjointMapUnion[T any](baseline, extension map[string]T) (map[string]T, error) {
+	u := make(map[string]T)
+
+	conflictingKeys := map[string]bool{}
+
+	for k, v := range baseline {
+		if _, conflict := extension[k]; conflict {
+			conflictingKeys[k] = true
+		} else {
+			u[k] = v
+		}
+	}
+
+	for k, v := range extension {
+		if _, conflict := baseline[k]; conflict {
+			conflictingKeys[k] = true
+		} else {
+			u[k] = v
+		}
+	}
+
+	if len(conflictingKeys) > 0 {
+		var conflicts []string
+		for k := range conflictingKeys {
+			conflicts = append(conflicts, k)
+		}
+		sort.Strings(conflicts)
+		return nil, fmt.Errorf("conflicting keys: %s", strings.Join(conflicts, ", "))
+	}
+
+	return u, nil
+}
