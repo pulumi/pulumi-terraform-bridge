bffaa59ac2de2341979261e78a8cf2075868b72e
Anton Tayanovskyy <anton@pulumi.com>
2023-05-24 17:32:41 -0400

    First pass at CheckConfig running validation

--- pf/tfbridge/provider_checkconfig.go
+++ pf/tfbridge/provider_checkconfig.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2022, Pulumi Corporation.
+// Copyright 2016-2023, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -20,24 +20,114 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/resource/plugin"
 
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/pulumi/pulumi-terraform-bridge/pf/internal/convert"
 	"github.com/pulumi/pulumi-terraform-bridge/pf/internal/defaults"
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
+	shim "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim"
+	rprovider "github.com/pulumi/pulumi/pkg/v3/resource/provider"
 )
 
 // CheckConfig validates the configuration for this resource provider.
-func (p *provider) CheckConfigWithContext(ctx context.Context, urn resource.URN,
-	olds, news resource.PropertyMap, allowUnknowns bool) (resource.PropertyMap, []plugin.CheckFailure, error) {
+func (p *provider) CheckConfigWithContext(
+	ctx context.Context,
+	urn resource.URN,
+	_ resource.PropertyMap, // olds aka priorState, not used currently
+	inputs resource.PropertyMap, // aka news
+	_ bool, // a flag that is always true, historical artifact, ignore here
+) (resource.PropertyMap, []plugin.CheckFailure, error) {
 	ctx = p.initLogging(ctx, p.logSink, urn)
 
-	// Transform checkedInputs to apply Pulumi-level defaults.
-	newsWithDefaults := defaults.ApplyDefaultInfoValues(ctx, defaults.ApplyDefaultInfoValuesArgs{
+	// Transform news to apply Pulumi-level defaults.
+	news := defaults.ApplyDefaultInfoValues(ctx, defaults.ApplyDefaultInfoValuesArgs{
 		SchemaMap:      p.schemaOnlyProvider.Schema(),
 		SchemaInfos:    p.info.Config,
-		PropertyMap:    news,
-		ProviderConfig: news,
+		PropertyMap:    inputs,
+		ProviderConfig: inputs,
 	})
 
-	p.lastKnownProviderConfig = newsWithDefaults
+	// It is currently a breaking change to call PreConfigureCallback with unknown values. The user code does not
+	// expect them and may panic.
+	//
+	// Currently we do not call it at all if there are any unknowns.
+	//
+	// See pulumi/pulumi-terraform-bridge#1087
+	if !news.ContainsUnknowns() {
+		wc := &wrappedConfig{news}
 
-	// TODO[pulumi/pulumi-terraform-bridge#821] validate provider config
-	return newsWithDefaults, []plugin.CheckFailure{}, nil
+		if p.info.PreConfigureCallback != nil {
+			// NOTE: the user code may modify news in-place.
+			validationErrors := p.info.PreConfigureCallback(news, wc)
+			if validationErrors != nil {
+				return nil, nil, validationErrors
+			}
+		}
+
+		if p.info.PreConfigureCallbackWithLogger != nil {
+			// Usually logSink is a HostClient; PreConfigureCallbackWithLogger type should have better been
+			// expressed in terms of a diag.Sink.
+			hc, ok := p.logSink.(*rprovider.HostClient)
+			if !ok {
+				hc = &rprovider.HostClient{}
+			}
+			// NOTE: the user code may modify news in-place.
+			validationErrors := p.info.PreConfigureCallbackWithLogger(ctx, hc, news, wc)
+			if validationErrors != nil {
+				return nil, nil, validationErrors
+			}
+		}
+	}
+
+	/* now: validateProviderConfig */
+
+	// Store for use in subsequent ApplyDefaultInfoValues.
+	p.lastKnownProviderConfig = news
+
+	checkFailures, err := p.validateProviderConfig(ctx, news)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Ensure propreties marked secret in the schema have secret values.
+	secretNews := tfbridge.MarkSchemaSecrets(ctx, p.schemaOnlyProvider.Schema(), p.info.Config,
+		resource.NewObjectProperty(news)).ObjectValue()
+
+	return secretNews, checkFailures, nil
 }
+
+func (p *provider) validateProviderConfig(
+	ctx context.Context,
+	inputs resource.PropertyMap,
+) ([]plugin.CheckFailure, error) {
+	config, err := convert.EncodePropertyMapToDynamic(p.configEncoder, p.configType, inputs)
+	if err != nil {
+		return nil, fmt.Errorf("cannot encode provider configuration to call ValidateProviderConfig: %w", err)
+	}
+	req := &tfprotov6.ValidateProviderConfigRequest{
+		Config: config,
+	}
+	resp, err := p.tfServer.ValidateProviderConfig(ctx, req)
+	// According to the docs on resp.PrepareConfig for new providers it typicaly is equal to config passed in
+	// ValidateProviderConfigRequest so the code here ignores it for now.
+	if err != nil {
+		return nil, fmt.Errorf("error calling ValidateProviderConfig: %w", err)
+	}
+	/* Instead of typical processDiagnsotics, should we be interpreting these messages as validation failures? */
+	if err := p.processDiagnostics(resp.Diagnostics); err != nil {
+		return nil, err
+	}
+	return nil, nil
+}
+
+type wrappedConfig struct {
+	config resource.PropertyMap
+}
+
+func (wc *wrappedConfig) IsSet(key string) bool {
+	pk := resource.PropertyKey(key)
+	_, isSet := wc.config[pk]
+	return isSet
+}
+
+var _ shim.ResourceConfig = &wrappedConfig{}
