6734ab09aaa80d8932edf7afdc2ca2d95be1b718
Anton Tayanovskyy <anton@pulumi.com>
2023-09-27 18:51:36 -0400

    Dependencies: pulumi v3.86.0 (#1402)
    Updating the Pulumi PKG dependency to v3.86.0 
    
    Since there are breaking changes, borrowing @Frassle work from
    https://github.com/pulumi/pulumi-terraform-bridge/pull/1392 to
    accommodate.
    
    Doing some test maintenance on top.
    
    If we can land this it should simplify rebasing 1392 for @justinvp so we
    can land that as well.
    
    ---------
    
    Co-authored-by: Fraser Waters <fraser@pulumi.com>

--- pf/tests/provider_get_mapping_test.go
+++ pf/tests/provider_get_mapping_test.go
@@ -26,36 +26,53 @@ import (
 	"github.com/pulumi/pulumi-terraform-bridge/pf/tests/internal/testprovider"
 	"github.com/pulumi/pulumi-terraform-bridge/pf/tfbridge"
 	tfbridge0 "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/resource/plugin"
 )
 
 func TestGetMapping(t *testing.T) {
 	ctx := context.Background()
 	info := testprovider.RandomProvider()
 
-	p, err := tfbridge.NewProvider(ctx, info, genMetadata(t, info))
-	assert.NoError(t, err)
+	var p plugin.Provider
+
+	t.Run("generate-schema", func(t *testing.T) {
+		// This generates the schema on the fly but shells out to go mod download and
+		// generates spurious warnings; for separating into separate sub-test.
+		var err error
+		p, err = tfbridge.NewProvider(ctx, info, genMetadata(t, info))
+		assert.NoError(t, err)
+	})
 
 	{
-		m, p, err := p.GetMapping("unknown-key")
+		m, p, err := p.GetMapping("unknown-key", "")
 		assert.NoError(t, err)
 		assert.Empty(t, m)
 		assert.Empty(t, p)
 	}
 
+	{
+		m, p, err := p.GetMapping("terraform", "unknown-provider")
+		assert.Error(t, err) // this should error, e.g. "unknown-provider"
+		assert.Empty(t, m)
+		assert.Empty(t, p)
+	}
+
 	for _, key := range []string{"tf", "terraform"} {
-		m, p, err := p.GetMapping(key)
-		assert.NoError(t, err)
+		for _, provider := range []string{"", "random"} {
+			m, p, err := p.GetMapping(key, provider)
+			assert.NoError(t, err)
 
-		assert.Equal(t, "random", p)
+			assert.Equal(t, "random", p)
 
-		var info tfbridge0.MarshallableProviderInfo
-		err = json.Unmarshal(m, &info)
-		assert.NoError(t, err)
+			var info tfbridge0.MarshallableProviderInfo
+			err = json.Unmarshal(m, &info)
+			assert.NoError(t, err)
 
-		assert.Equal(t, "random", info.Name)
-		assert.Contains(t, info.Resources, "random_integer")
-		assert.Equal(t, "random:index/randomInteger:RandomInteger",
-			string(info.Resources["random_integer"].Tok))
+			assert.Equal(t, "random", info.Name)
+			assert.Contains(t, info.Resources, "random_integer")
+			assert.Equal(t, "random:index/randomInteger:RandomInteger",
+				string(info.Resources["random_integer"].Tok))
+		}
 	}
 }
 
@@ -67,36 +84,49 @@ func TestMuxedGetMapping(t *testing.T) {
 	server, err := tfbridge.MakeMuxedServer(ctx, "muxedrandom", info, genSDKSchema(t, info))(nil)
 	require.NoError(t, err)
 
-	req := func(key string) (context.Context, *pulumirpc.GetMappingRequest) {
-		return ctx, &pulumirpc.GetMappingRequest{Key: key}
+	req := func(key, provider string) (context.Context, *pulumirpc.GetMappingRequest) {
+		return ctx, &pulumirpc.GetMappingRequest{
+			Key:      key,
+			Provider: provider,
+		}
 	}
 
 	t.Run("unknown-key", func(t *testing.T) {
-		resp, err := server.GetMapping(req("unknown-key"))
+		resp, err := server.GetMapping(req("unknown-key", ""))
 		assert.NoError(t, err)
 		assert.Empty(t, resp.Data)
 		assert.Empty(t, resp.Provider)
 	})
 
+	t.Run("unknown-provider", func(t *testing.T) {
+		t.Skipf("TODO[pulumi/pulumi-terraform-bridge#1403] skipping due to a bug that is not fixed yet")
+		resp, err := server.GetMapping(req("terraform", "unknown-provider"))
+		assert.Error(t, err) // this should error, e.g. "unknown-provider"
+		assert.Empty(t, string(resp.Data))
+		assert.Empty(t, resp.Provider)
+	})
+
 	for _, key := range []string{"tf", "terraform"} {
-		resp, err := server.GetMapping(req(key))
-		assert.NoError(t, err)
+		for _, provider := range []string{"", "muxedrandom"} {
+			resp, err := server.GetMapping(req(key, provider))
+			assert.NoError(t, err)
 
-		assert.Equal(t, "muxedrandom", resp.Provider)
+			assert.Equal(t, "muxedrandom", resp.Provider)
 
-		var info tfbridge0.MarshallableProviderInfo
-		err = json.Unmarshal(resp.Data, &info)
-		assert.NoError(t, err)
+			var info tfbridge0.MarshallableProviderInfo
+			err = json.Unmarshal(resp.Data, &info)
+			assert.NoError(t, err)
 
-		assert.Equal(t, "muxedrandom", info.Name)
-		assert.Contains(t, info.Resources, "random_integer")
-		assert.Contains(t, info.Resources, "random_human_number")
+			assert.Equal(t, "muxedrandom", info.Name)
+			assert.Contains(t, info.Resources, "random_integer")
+			assert.Contains(t, info.Resources, "random_human_number")
 
-		// A PF based resource
-		assert.Equal(t, "muxedrandom:index/randomInteger:RandomInteger",
-			string(info.Resources["random_integer"].Tok))
-		// An SDK bases resource
-		assert.Equal(t, "muxedrandom:index/randomHumanNumber:RandomHumanNumber",
-			string(info.Resources["random_human_number"].Tok))
+			// A PF based resource
+			assert.Equal(t, "muxedrandom:index/randomInteger:RandomInteger",
+				string(info.Resources["random_integer"].Tok))
+			// An SDK bases resource
+			assert.Equal(t, "muxedrandom:index/randomHumanNumber:RandomHumanNumber",
+				string(info.Resources["random_human_number"].Tok))
+		}
 	}
 }
