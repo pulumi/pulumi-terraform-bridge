a3c094c86ea05f237874059f3731f908197ed6d9
Ian Wahbe <ian@wahbe.com>
2023-11-08 14:57:50 -0800

    WIP (untested) Convert TF wire to {datasource,resource,provider}/schema.Schema

--- pf/internal/pfutils/provider.go
+++ pf/internal/pfutils/provider.go
@@ -3,6 +3,7 @@ package pfutils
 import (
 	"context"
 
+	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	dschema "github.com/hashicorp/terraform-plugin-framework/datasource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
@@ -11,6 +12,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	rschema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/util/contract"
 )
 
 // Creates a pf provider that's backed by a protov6.ProviderServer
@@ -40,7 +43,7 @@ func (p *schemaOnlyProvider) Schema(ctx context.Context, _ provider.SchemaReques
 		resp.Diagnostics.AddError("Schema", err.Error())
 		return
 	}
-	resp.Schema, resp.Diagnostics = convertProviderSchema(schema)
+	resp.Schema, resp.Diagnostics = convertProviderSchema(schema.Provider)
 }
 
 func (p *schemaOnlyProvider) getSchema(ctx context.Context) (*tfprotov6.GetProviderSchemaResponse, error) {
@@ -53,7 +56,7 @@ func (p *schemaOnlyProvider) getSchema(ctx context.Context) (*tfprotov6.GetProvi
 }
 
 func (schemaOnlyProvider) Configure(context.Context, provider.ConfigureRequest, *provider.ConfigureResponse) {
-	panic("Not Implemented")
+	contract.Failf("Configure is not implemented on schemaOnlyProvider")
 }
 
 func (p *schemaOnlyProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
@@ -82,16 +85,237 @@ func (p *schemaOnlyProvider) Resources(ctx context.Context) []func() resource.Re
 	return resp
 }
 
-func convertProviderSchema(resp *tfprotov6.GetProviderSchemaResponse) (pschema.Schema, diag.Diagnostics) {
-	panic("Not Implemented")
+func convertProviderSchema(req *tfprotov6.Schema) (pschema.Schema, diag.Diagnostics) {
+	var schema pschema.Schema
+
+	schema.MarkdownDescription, schema.Description = getDescription(
+		req.Block.DescriptionKind, req.Block.Description)
+
+	if req.Block.Deprecated {
+		schema.DeprecationMessage = "This provider is deprecated"
+	}
+
+	attrs := make(map[string]pschema.Attribute)
+	for k, v := range convertDataSourceAttributes(req.Block.Attributes) {
+		attrs[k] = v.(pschema.Attribute) // This might be a VERY BAD IDEA, but it compiles
+	}
+	blocks := make(map[string]pschema.Block)
+	for k, v := range convertDataSourceNestedBlock(req.Block.BlockTypes) {
+		blocks[k] = v.(pschema.Block)
+	}
+	schema.Attributes = attrs
+	schema.Blocks = blocks
+
+	return schema, nil
 }
 
-func convertResourceSchema(*tfprotov6.Schema) (rschema.Schema, diag.Diagnostics) {
-	panic("Not Implemented")
+func convertResourceSchema(req *tfprotov6.Schema) (rschema.Schema, diag.Diagnostics) {
+	var schema rschema.Schema
+	schema.MarkdownDescription, schema.Description = getDescription(
+		req.Block.DescriptionKind, req.Block.Description)
+
+	if req.Block.Deprecated {
+		schema.DeprecationMessage = "This resource is deprecated"
+	}
+
+	attrs := make(map[string]rschema.Attribute)
+	for k, v := range convertDataSourceAttributes(req.Block.Attributes) {
+		attrs[k] = v.(rschema.Attribute) // This might be a VERY BAD IDEA, but it compiles
+	}
+	blocks := make(map[string]rschema.Block)
+	for k, v := range convertDataSourceNestedBlock(req.Block.BlockTypes) {
+		blocks[k] = v.(rschema.Block)
+	}
+	schema.Attributes = attrs
+	schema.Blocks = blocks
+
+	return schema, nil
 }
 
 func convertDataSourceSchema(req *tfprotov6.Schema) (dschema.Schema, diag.Diagnostics) {
-	panic("Not Implemented")
+	var schema dschema.Schema
+	schema.MarkdownDescription, schema.Description = getDescription(
+		req.Block.DescriptionKind, req.Block.Description)
+
+	if req.Block.Deprecated {
+		schema.DeprecationMessage = "This data source is deprecated"
+	}
+
+	schema.Attributes = convertDataSourceAttributes(req.Block.Attributes)
+	schema.Blocks = convertDataSourceNestedBlock(req.Block.BlockTypes)
+
+	return schema, nil
+}
+
+func getDescription(kind tfprotov6.StringKind, rawDesc string) (mdDesc, desc string) {
+	switch kind {
+	case tfprotov6.StringKindPlain:
+		return "", rawDesc
+	case tfprotov6.StringKindMarkdown:
+		return rawDesc, ""
+	default: // Unknown kind
+		return rawDesc, rawDesc
+	}
+
+}
+
+func convertDataSourceAttributes(attributes []*tfprotov6.SchemaAttribute) map[string]dschema.Attribute {
+	attrs := make(map[string]dschema.Attribute, len(attributes))
+	for _, a := range attributes {
+
+		if _, duplicate := attrs[a.Name]; duplicate {
+			contract.Failf("Duplicate attribute: %q", a.Name)
+		}
+
+		if a.NestedType != nil {
+			panic("NOT IMPLEMENTED YET")
+			continue
+		}
+
+		mdDesc, desc := getDescription(a.DescriptionKind, a.Description)
+		switch {
+		case a.Type.Is(tftypes.Bool):
+			attrs[a.Name] = dschema.BoolAttribute{
+				Required:            a.Required,
+				Optional:            a.Optional,
+				Computed:            a.Computed,
+				Sensitive:           a.Sensitive,
+				Description:         desc,
+				MarkdownDescription: mdDesc,
+			}
+		case a.Type.Is(tftypes.String):
+			attrs[a.Name] = dschema.StringAttribute{
+				Required:            a.Required,
+				Optional:            a.Optional,
+				Computed:            a.Computed,
+				Sensitive:           a.Sensitive,
+				Description:         desc,
+				MarkdownDescription: mdDesc,
+			}
+		case a.Type.Is(tftypes.Number):
+			attrs[a.Name] = dschema.NumberAttribute{
+				Required:            a.Required,
+				Optional:            a.Optional,
+				Computed:            a.Computed,
+				Sensitive:           a.Sensitive,
+				Description:         desc,
+				MarkdownDescription: mdDesc,
+			}
+		case a.Type.Is(tftypes.Map{}):
+			attrs[a.Name] = dschema.MapAttribute{
+				Required:            a.Required,
+				Optional:            a.Optional,
+				Computed:            a.Computed,
+				Sensitive:           a.Sensitive,
+				Description:         desc,
+				MarkdownDescription: mdDesc,
+				ElementType:         convertTftypeToAttrType(a.Type.(tftypes.Map).ElementType),
+			}
+		case a.Type.Is(tftypes.Set{}):
+			attrs[a.Name] = dschema.MapAttribute{
+				Required:            a.Required,
+				Optional:            a.Optional,
+				Computed:            a.Computed,
+				Sensitive:           a.Sensitive,
+				Description:         desc,
+				MarkdownDescription: mdDesc,
+				ElementType:         convertTftypeToAttrType(a.Type.(tftypes.Set).ElementType),
+			}
+		case a.Type.Is(tftypes.List{}):
+			attrs[a.Name] = dschema.MapAttribute{
+				Required:            a.Required,
+				Optional:            a.Optional,
+				Computed:            a.Computed,
+				Sensitive:           a.Sensitive,
+				Description:         desc,
+				MarkdownDescription: mdDesc,
+				ElementType:         convertTftypeToAttrType(a.Type.(tftypes.List).ElementType),
+			}
+		case a.Type.Is(tftypes.Object{}):
+			objAttrs := make(map[string]attr.Type, len(a.Type.(tftypes.Object).AttributeTypes))
+			for k, objA := range a.Type.(tftypes.Object).AttributeTypes {
+				objAttrs[k] = convertTftypeToAttrType(objA)
+			}
+			attrs[a.Name] = dschema.ObjectAttribute{
+				Required:            a.Required,
+				Optional:            a.Optional,
+				Computed:            a.Computed,
+				Sensitive:           a.Sensitive,
+				Description:         desc,
+				MarkdownDescription: mdDesc,
+				AttributeTypes:      objAttrs,
+			}
+		default:
+			contract.Failf("UNKNOWN TYPE: %s", a.Type.String())
+		}
+	}
+	return attrs
+}
+
+func convertTftypeToAttrType(ty tftypes.Type) attr.Type { return attrTypeShim{ty} }
+
+type attrTypeShim struct{ tftypes.Type }
+
+func (a attrTypeShim) TerraformType(context.Context) tftypes.Type {
+	return a.Type
+}
+
+func (attrTypeShim) ValueFromTerraform(context.Context, tftypes.Value) (attr.Value, error) {
+	contract.Failf("attrTypeShim does not implement ValueFromTerraform")
+	return nil, nil
+}
+func (attrTypeShim) ValueType(context.Context) attr.Value {
+	contract.Failf("attrTypeShim does not implement ValueType")
+	return nil
+}
+func (attrTypeShim) Equal(attr.Type) bool {
+	contract.Failf("attrTypeShim does not implement Equal")
+	return false
+}
+
+func convertDataSourceNestedBlock(protoBlocks []*tfprotov6.SchemaNestedBlock) map[string]dschema.Block {
+	blocks := make(map[string]dschema.Block, len(protoBlocks))
+	for _, protoBlock := range protoBlocks {
+		if _, duplicate := blocks[protoBlock.TypeName]; duplicate {
+			contract.Failf("Duplicate block: " + protoBlock.TypeName)
+		}
+
+		mdDesc, desc := getDescription(protoBlock.Block.DescriptionKind, protoBlock.Block.Description)
+
+		switch protoBlock.Nesting {
+		case tfprotov6.SchemaNestedBlockNestingModeList:
+			blocks[protoBlock.TypeName] = dschema.ListNestedBlock{
+				MarkdownDescription: mdDesc,
+				Description:         desc,
+				NestedObject:        convertDataSourceNestedBlockObject(protoBlock.Block),
+			}
+		case tfprotov6.SchemaNestedBlockNestingModeSet:
+			blocks[protoBlock.TypeName] = dschema.SetNestedBlock{
+				MarkdownDescription: mdDesc,
+				Description:         desc,
+				NestedObject:        convertDataSourceNestedBlockObject(protoBlock.Block),
+			}
+		case tfprotov6.SchemaNestedBlockNestingModeSingle:
+			obj := convertDataSourceNestedBlockObject(protoBlock.Block)
+			blocks[protoBlock.TypeName] = dschema.SingleNestedBlock{
+				MarkdownDescription: mdDesc,
+				Description:         desc,
+				Attributes:          obj.Attributes,
+				Blocks:              obj.Blocks,
+			}
+		default:
+			panic("Unknown block nesting: " + protoBlock.Nesting.String())
+		}
+	}
+
+	return blocks
+}
+
+func convertDataSourceNestedBlockObject(block *tfprotov6.SchemaBlock) dschema.NestedBlockObject {
+	return dschema.NestedBlockObject{
+		Attributes: convertDataSourceAttributes(block.Attributes),
+		Blocks:     convertDataSourceNestedBlock(block.BlockTypes),
+	}
 }
 
 type schemaOnlyDataSource struct {
@@ -112,7 +336,7 @@ func (s schemaOnlyDataSource) Schema(_ context.Context, _ datasource.SchemaReque
 // order to update state. Config values should be read from the
 // ReadRequest and new state values set on the ReadResponse.
 func (s schemaOnlyDataSource) Read(context.Context, datasource.ReadRequest, *datasource.ReadResponse) {
-	panic("schemaOnlyDataSource does not implement Read")
+	contract.Failf("schemaOnlyDataSource does not implement Read")
 }
 
 func (s schemaOnlyDataSource) identity() datasource.DataSource { return s }
@@ -135,21 +359,21 @@ func (s schemaOnlyResource) Schema(_ context.Context, _ resource.SchemaRequest, 
 // and planned state values should be read from the
 // CreateRequest and new state values set on the CreateResponse.
 func (s schemaOnlyResource) Create(context.Context, resource.CreateRequest, *resource.CreateResponse) {
-	panic("schemaOnlyResource does not implement Create")
+	contract.Failf("schemaOnlyResource does not implement Create")
 }
 
 // Read is called when the provider must read resource values in order
 // to update state. Planned state values should be read from the
 // ReadRequest and new state values set on the ReadResponse.
 func (s schemaOnlyResource) Read(context.Context, resource.ReadRequest, *resource.ReadResponse) {
-	panic("schemaOnlyResource does not implement Read")
+	contract.Failf("schemaOnlyResource does not implement Read")
 }
 
 // Update is called to update the state of the resource. Config, planned
 // state, and prior state values should be read from the
 // UpdateRequest and new state values set on the UpdateResponse.
 func (s schemaOnlyResource) Update(context.Context, resource.UpdateRequest, *resource.UpdateResponse) {
-	panic("schemaOnlyResource does not implement Update")
+	contract.Failf("schemaOnlyResource does not implement Update")
 }
 
 // Delete is called when the provider must delete the resource. Config
@@ -159,7 +383,7 @@ func (s schemaOnlyResource) Update(context.Context, resource.UpdateRequest, *res
 // call DeleteResponse.State.RemoveResource(), so it can be omitted
 // from provider logic.
 func (s schemaOnlyResource) Delete(context.Context, resource.DeleteRequest, *resource.DeleteResponse) {
-	panic("schemaOnlyResource does not implement Delete")
+	contract.Failf("schemaOnlyResource does not implement Delete")
 }
 
 func (s schemaOnlyResource) identity() resource.Resource { return s }
--- pf/tests/go.mod
+++ pf/tests/go.mod
@@ -1,6 +1,8 @@
 module github.com/pulumi/pulumi-terraform-bridge/pf/tests
 
-go 1.20
+go 1.21
+
+toolchain go1.21.0
 
 require (
 	github.com/hashicorp/terraform-plugin-framework v1.4.1
@@ -20,6 +22,7 @@ require (
 	github.com/Masterminds/semver v1.5.0 // indirect
 	github.com/apparentlymart/go-textseg/v15 v15.0.0 // indirect
 	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.17.3 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/charmbracelet/bubbles v0.16.1 // indirect
 	github.com/charmbracelet/bubbletea v0.24.2 // indirect
@@ -86,18 +89,18 @@ require (
 	github.com/armon/go-metrics v0.4.0 // indirect
 	github.com/armon/go-radix v1.0.0 // indirect
 	github.com/aws/aws-sdk-go v1.44.298 // indirect
-	github.com/aws/aws-sdk-go-v2 v1.17.3 // indirect
-	github.com/aws/aws-sdk-go-v2/config v1.15.15 // indirect
-	github.com/aws/aws-sdk-go-v2/credentials v1.12.10 // indirect
-	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.9 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.27 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.21 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.16 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.9 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.21.2 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.19.1 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.13.43 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.13.13 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.43 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.37 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.45 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.37 // indirect
 	github.com/aws/aws-sdk-go-v2/service/kms v1.18.1 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sso v1.11.13 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sts v1.16.10 // indirect
-	github.com/aws/smithy-go v1.13.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.15.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.23.2 // indirect
+	github.com/aws/smithy-go v1.15.0 // indirect
 	github.com/bgentry/go-netrc v0.0.0-20140422174119-9fd32a8b3d3d // indirect
 	github.com/bgentry/speakeasy v0.1.0 // indirect
 	github.com/blang/semver v3.5.1+incompatible // indirect
@@ -111,7 +114,7 @@ require (
 	github.com/edsrzf/mmap-go v1.1.0 // indirect
 	github.com/emirpasic/gods v1.18.1 // indirect
 	github.com/ettle/strcase v0.1.1 // indirect
-	github.com/fatih/color v1.13.0 // indirect
+	github.com/fatih/color v1.15.0 // indirect
 	github.com/gedex/inflector v0.0.0-20170307190818-16278e9db813 // indirect
 	github.com/go-git/gcfg v1.5.1-0.20230307220236-3a3c6141e376 // indirect
 	github.com/go-git/go-billy/v5 v5.4.1 // indirect
@@ -135,12 +138,12 @@ require (
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hashicorp/go-cty v1.4.1-0.20200414143053-d3edf31b6320 // indirect
-	github.com/hashicorp/go-getter v1.7.1 // indirect
+	github.com/hashicorp/go-getter v1.7.3 // indirect
 	github.com/hashicorp/go-hclog v1.5.0 // indirect
 	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
-	github.com/hashicorp/go-plugin v1.5.1 // indirect
-	github.com/hashicorp/go-retryablehttp v0.7.1 // indirect
+	github.com/hashicorp/go-plugin v1.5.2 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.4 // indirect
 	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
 	github.com/hashicorp/go-safetemp v1.0.0 // indirect
 	github.com/hashicorp/go-secure-stdlib/mlock v0.1.2 // indirect
@@ -151,7 +154,7 @@ require (
 	github.com/hashicorp/go-version v1.6.0 // indirect
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
-	github.com/hashicorp/hcl/v2 v2.18.0 // indirect
+	github.com/hashicorp/hcl/v2 v2.18.1 // indirect
 	github.com/hashicorp/hil v0.0.0-20190212132231-97b3a9cdfa93 // indirect
 	github.com/hashicorp/terraform-plugin-framework-validators v0.10.0
 	github.com/hashicorp/terraform-plugin-log v0.9.0 // indirect
@@ -161,7 +164,7 @@ require (
 	github.com/hashicorp/vault/api v1.8.2 // indirect
 	github.com/hashicorp/vault/sdk v0.6.1 // indirect
 	github.com/hashicorp/yamux v0.1.1 // indirect
-	github.com/huandu/xstrings v1.3.2 // indirect
+	github.com/huandu/xstrings v1.3.3 // indirect
 	github.com/iancoleman/strcase v0.2.0 // indirect
 	github.com/imdario/mergo v0.3.15 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
@@ -170,7 +173,7 @@ require (
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/kevinburke/ssh_config v1.2.0 // indirect
 	github.com/klauspost/compress v1.15.11 // indirect
-	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/mitchellh/cli v1.1.5 // indirect
@@ -227,7 +230,7 @@ require (
 	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
-	github.com/zclconf/go-cty v1.14.0 // indirect
+	github.com/zclconf/go-cty v1.14.1 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	gocloud.dev v0.27.0 // indirect
