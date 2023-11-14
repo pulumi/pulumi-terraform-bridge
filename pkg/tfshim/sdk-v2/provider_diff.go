4b9a1faeaddd868d81ab590e4e8f2c0d03907e73
Anton Tayanovskyy <anton@pulumi.com>
2023-11-14 18:01:06 -0500

    Fix panics on nil RawPlan

--- pkg/tfshim/sdk-v2/provider_diff.go
+++ pkg/tfshim/sdk-v2/provider_diff.go
@@ -74,7 +74,7 @@ func (p v2Provider) simpleDiff(
 	c *terraform.ResourceConfig,
 	rawConfigVal hcty.Value,
 	meta interface{},
-) (*terraform.InstanceDiff, error) {
+) (finalInstanceDiff *terraform.InstanceDiff, finalError error) {
 	ctx := context.TODO()
 
 	switch diffStrat {
@@ -165,7 +165,27 @@ func simpleDiffViaPlanState(
 	if state.RawConfig.IsNull() {
 		state.RawConfig = rawConfigVal
 	}
-	return res.SimpleDiff(ctx, state, planned, meta)
+	diff, err := res.SimpleDiff(ctx, state, planned, meta)
+	if err != nil {
+		return nil, err
+	}
+
+	// TF gRPC servers compenstate for the fact that SimpleDiff may return
+	// terraform.NewInstanceDiff(), nil dropping any available information on RawPlan.
+	//
+	// See for example this code in PlanResourceChange:
+	//
+	// See https://github.com/hashicorp/terraform-plugin-sdk/blob/28e631776d97f0a5a5942b3524814addbef90875/helper/schema/grpc_provider.go#L797
+	//
+	// In TF this is communicated from PlanResourceChange to ApplyResourceChange; unlike
+	// TF, in the current codebase tpphe InstanceDiff is passed directly to Apply. If
+	// RawPlan is not set on the diff it may cause nil panics in the provider.
+	if diff != nil && len(diff.Attributes) == 0 {
+		diff.RawPlan = priorStateVal
+		// TODO[pulumi/pulumi-terraform-bridge#1505] handle private state similar to upstream
+	}
+
+	return diff, nil
 }
 
 func showDiffChangeType(b byte) string {
