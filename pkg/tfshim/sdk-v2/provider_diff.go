0ac62de9562500626c052245833e1b14997eb17b
Anton Tayanovskyy <anton@pulumi.com>
2023-11-14 21:01:26 -0500

    Fix raw plan availability (#1521)
    Making a surgical change to avoid nil pointer panics on accessing
    RawPlan, coming from the AWS panic in
    https://github.com/pulumi/pulumi-aws/issues/2904
    
    I have verified that this resolves 2904.
    
    ---------
    
    Co-authored-by: Ian Wahbe <ian@wahbe.com>

--- pkg/tfshim/sdk-v2/provider_diff.go
+++ pkg/tfshim/sdk-v2/provider_diff.go
@@ -165,7 +165,28 @@ func simpleDiffViaPlanState(
 	if state.RawConfig.IsNull() {
 		state.RawConfig = rawConfigVal
 	}
-	return res.SimpleDiff(ctx, state, planned, meta)
+	diff, err := res.SimpleDiff(ctx, state, planned, meta)
+	if err != nil {
+		return nil, err
+	}
+
+	// TF gRPC servers compensate for the fact that SimpleDiff may return
+	// terraform.NewInstanceDiff(), nil dropping any available information on RawPlan.
+	//
+	// See for example this code in PlanResourceChange:
+	//
+	// See https://github.com/hashicorp/terraform-plugin-sdk/blob/
+	//         28e631776d97f0a5a5942b3524814addbef90875/helper/schema/grpc_provider.go#L797
+	//
+	// In TF this is communicated from PlanResourceChange to ApplyResourceChange; unlike TF, in
+	// the current codebase InstanceDiff is passed directly to Apply. If RawPlan is not set on
+	// the diff it may cause nil panics in the provider.
+	if diff != nil && len(diff.Attributes) == 0 {
+		diff.RawPlan = priorStateVal
+		// TODO[pulumi/pulumi-terraform-bridge#1505] handle private state similar to upstream
+	}
+
+	return diff, nil
 }
 
 func showDiffChangeType(b byte) string {
