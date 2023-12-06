9f0802d84965df821f334a7a3eefbda28a0ab0fe
Venelin <venelin@pulumi.com>
2023-12-06 17:45:56 +0000

    pass tf schema information to elements in maps

--- pkg/tfbridge/schema.go
+++ pkg/tfbridge/schema.go
@@ -458,6 +458,14 @@ func (ctx *conversionContext) MakeTerraformInput(name string, old, v resource.Pr
 		if ps != nil {
 			psflds = ps.Fields
 		}
+		
+		if elemSchema, isSchema := tfs.Elem().(shim.Schema); isSchema &&
+			tfflds == nil && tfs.Type() == shim.TypeMap {
+			tfflds = schema.SchemaMap{}
+			for field := range v.ObjectValue() {
+				tfflds.Set(string(field), elemSchema)
+			}
+		}
 		var oldObject resource.PropertyMap
 		if old.IsObject() {
 			oldObject = old.ObjectValue()
