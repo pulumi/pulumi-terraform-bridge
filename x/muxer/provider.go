6e488ea3b1b174422868938ef8fbcdaa28346a12
Thomas Meckel <tmeckel@users.noreply.github.com>
2023-12-29 19:02:36 +0000

    chore: add comment explaining the purpose of Provider interface x/muxer/provider.go

--- x/muxer/provider.go
+++ x/muxer/provider.go
@@ -6,6 +6,8 @@ import (
 	pulumirpc "github.com/pulumi/pulumi/sdk/v3/proto/go"
 )
 
+// Provider defines an interface which must be implemented by providers
+// that shall be used as mixins of a wrapped Terraform provider
 type Provider interface {
 	GetSpec() (schema.PackageSpec, error)
 	GetInstance(host *provider.HostClient) (pulumirpc.ResourceProviderServer, error)
