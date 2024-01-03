4afb08904c0d74d287a83c6087caeed5cf01fbee
Thomas Meckel <tmeckel@users.noreply.github.com>
2024-01-03 17:07:17 +0000

    test: update resource token creation to use tfbridge.MakeResource in pkg/tfgen/internal/testprovider/minimuxed_replace.go

--- pkg/tfgen/internal/testprovider/minimuxed_replace.go
+++ pkg/tfgen/internal/testprovider/minimuxed_replace.go
@@ -1,34 +1,18 @@
 package testprovider
 
 import (
-	"unicode"
-
 	testproviderdata "github.com/pulumi/pulumi-terraform-bridge/v3/internal/testprovider"
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
 	shimv2 "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim/sdk-v2"
 	"github.com/pulumi/pulumi-terraform-bridge/x/muxer"
 	"github.com/pulumi/pulumi/pkg/v3/codegen/schema"
 	"github.com/pulumi/pulumi/pkg/v3/resource/provider"
-	"github.com/pulumi/pulumi/sdk/v3/go/common/tokens"
 	pulumirpc "github.com/pulumi/pulumi/sdk/v3/proto/go"
 )
 
 func ProviderMiniMuxedReplace() tfbridge.ProviderInfo {
 	minimuxedPkg := "minimuxed"
 	minimuxedMod := "index"
-
-	minimuxedMember := func(mod string, mem string) tokens.ModuleMember {
-		return tokens.ModuleMember(minimuxedPkg + ":" + mod + ":" + mem)
-	}
-
-	minimuxedType := func(mod string, typ string) tokens.Type {
-		return tokens.Type(minimuxedMember(mod, typ))
-	}
-
-	minimuxedResource := func(mod string, res string) tokens.Type {
-		fn := string(unicode.ToLower(rune(res[0]))) + res[1:]
-		return minimuxedType(mod+"/"+fn, res)
-	}
 
 	return tfbridge.ProviderInfo{
 		P:           shimv2.NewProvider(testproviderdata.ProviderMiniMuxed()),
@@ -39,7 +23,7 @@ func ProviderMiniMuxedReplace() tfbridge.ProviderInfo {
 		Homepage:    "https://pulumi.io",
 		Repository:  "https://github.com/pulumi/pulumi-minimuxed",
 		Resources: map[string]*tfbridge.ResourceInfo{
-			"minimuxed_integer": {Tok: minimuxedResource(minimuxedMod, "MinimuxedInteger")},
+			"minimuxed_integer": {Tok: tfbridge.MakeResource(minimuxedPkg, minimuxedMod, "MinimuxedInteger")},
 		},
 		MuxWith: []muxer.Provider{
 			newMuxReplaceProvider(),
