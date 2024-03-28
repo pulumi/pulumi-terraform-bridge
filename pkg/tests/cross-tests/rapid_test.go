2d113614c91063321e902fe4c8c8359d056768fb
Anton Tayanovskyy <anton@pulumi.com>
2024-03-27 23:07:39 -0400

    WIP on rapid

--- pkg/tests/cross-tests/rapid_test.go
+++ pkg/tests/cross-tests/rapid_test.go
@@ -6,26 +6,32 @@ import (
 	"testing"
 	"time"
 
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"pgregory.net/rapid"
 )
 
 func TestDiffConvergence(outerT *testing.T) {
+	t.Skipf("Work in progress")
 	log.SetOutput(io.Discard)
 	tvg := &tvGen{}
 
 	rapid.Check(outerT, func(t *rapid.T) {
+
+		outerT.Logf("Iterating..")
 		tv := tvg.GenObject(3).Draw(t, "tv")
 
 		c1 := tv.valueGen.Draw(t, "config1")
 		c2 := tv.valueGen.Draw(t, "config2")
+		ty := tv.typ.(tftypes.Object)
 
 		tc := diffTestCase{
 			Resource: &schema.Resource{
 				Schema: tv.schema.Elem.(*schema.Resource).Schema,
 			},
-			Config1: c1,
-			Config2: c2,
+			Config1:    c1.inner,
+			Config2:    c2.inner,
+			ObjectType: &ty,
 		}
 
 		runDiffCheck(&rapidTWithCleanup{t, outerT}, tc)
