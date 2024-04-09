e3d45ab0bfb6a791fd1064c0a61c2d9c6a2f4f6c
Venelin <venelin@pulumi.com>
2024-04-09 13:02:16 +0100

    revert diff

--- pkg/tfshim/sdk-v2/provider2.go
+++ pkg/tfshim/sdk-v2/provider2.go
@@ -391,8 +391,7 @@ func (s *grpcServer) PlanResourceChange(
 	PlannedState cty.Value
 	PlannedMeta  map[string]interface{}
 	PlannedDiff  *terraform.InstanceDiff
-}, error,
-) {
+}, error) {
 	configVal, err := msgpack.Marshal(config, ty)
 	if err != nil {
 		return nil, err
