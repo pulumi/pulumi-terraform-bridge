8d25602bddba3d0de28ffc0ec8824bf5229a0656
Anton Tayanovskyy <anton@pulumi.com>
2024-05-09 15:28:28 -0400

    Tests

--- pkg/tfbridge/deconflict_test.go
+++ pkg/tfbridge/deconflict_test.go
@@ -15,10 +15,64 @@
 package tfbridge
 
 import (
+	"context"
+	"fmt"
 	"testing"
 
+	shim "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim"
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim/schema"
+	"github.com/pulumi/pulumi-terraform-bridge/v3/unstable/logging"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
 	"github.com/stretchr/testify/require"
 )
+
+func TestDeconflict(t *testing.T) {
+	ctx := context.Background()
+
+	ctx = logging.InitLogging(ctx, logging.LogOptions{})
+	schema := &schema.SchemaMap{
+		"availability_zone": (&schema.Schema{
+			Type:          shim.TypeString,
+			Optional:      true,
+			Computed:      true,
+			ForceNew:      true,
+			ConflictsWith: []string{"availability_zone_id"},
+		}).Shim(),
+		"availability_zone_id": (&schema.Schema{
+			Type:          shim.TypeString,
+			Optional:      true,
+			Computed:      true,
+			ForceNew:      true,
+			ConflictsWith: []string{"availability_zone"},
+		}).Shim(),
+	}
+
+	type testCase struct {
+		inputs   resource.PropertyMap
+		expected resource.PropertyMap
+	}
+
+	testCases := []testCase{
+		{
+			inputs: resource.PropertyMap{
+				"availabilityZone":   resource.NewStringProperty("us-east-1c"),
+				"availabilityZoneId": resource.NewStringProperty("use1-az1"),
+			},
+			expected: resource.PropertyMap{
+				"availabilityZone":   resource.NewStringProperty("us-east-1c"),
+				"availabilityZoneId": resource.NewNullProperty(),
+			},
+		},
+	}
+
+	for i, tc := range testCases {
+		tc := tc
+		t.Run(fmt.Sprintf("case-%d", i), func(t *testing.T) {
+			actual := deconflict(ctx, schema, nil, tc.inputs)
+			require.Equal(t, tc.expected, actual)
+		})
+	}
+}
 
 func TestParseConflictsWith(t *testing.T) {
 	cw := "capacity_reservation_specification.0.capacity_reservation_target.0.capacity_reservation_id"
