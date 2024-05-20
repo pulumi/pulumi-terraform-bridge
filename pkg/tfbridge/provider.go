82ef773d72a0af4603f8b8c6a6be5311c119701c
corymhall <43035978+corymhall@users.noreply.github.com>
2024-05-20 11:24:50 -0400

    chore: better type checking error messages
    This is what the error message looks like
    
    ```console
      aws:ecs:Service (service):
        warning: Type checking failed:
          Unexpected type at field networkConfiguration.subnets:
            expected array type, got string type
        warning: Type checking is still experimental. If you believe that a warning is incorrect,
        please let us know by creating an issue at https://github.com/pulumi/pulumi-terraform-bridge/issues.
        This will become a hard error in the future.
    ```
    
    fixes #1986

--- pkg/tfbridge/provider.go
+++ pkg/tfbridge/provider.go
@@ -803,9 +803,6 @@ func (p *Provider) Check(ctx context.Context, req *pulumirpc.CheckRequest) (*pul
 			iv := NewInputValidator(urn, *schema)
 			typeFailures := iv.ValidateInputs(t, news)
 			if typeFailures != nil {
-				logger.Warn("Type checking failed. If any of these are incorrect, please let us know by creating an" +
-					"issue at https://github.com/pulumi/pulumi-terraform-bridge/issues.",
-				)
 				for _, e := range *typeFailures {
 					if validateShouldError {
 						pp := NewCheckFailurePath(schemaMap, schemaInfos, e.ResourcePath)
@@ -815,9 +812,16 @@ func (p *Provider) Check(ctx context.Context, req *pulumirpc.CheckRequest) (*pul
 							Property: string(cf.Property),
 						})
 					} else {
-						logger.Warn(fmt.Sprintf("%v verification warning: %s: Examine values at %s", urn, e.Reason, e.ResourcePath))
+						logger.Warn(
+							fmt.Sprintf("Type checking failed:\n  Unexpected type at field %s: \n    %s", e.ResourcePath, e.Reason),
+						)
 					}
 				}
+				logger.Warn("Type checking is still experimental. If you believe that a warning is incorrect,\n" +
+					"please let us know by creating an " +
+					"issue at https://github.com/pulumi/pulumi-terraform-bridge/issues.\n" +
+					"This will become a hard error in the future.",
+				)
 			}
 		}
 	}
