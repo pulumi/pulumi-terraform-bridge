bd290edd92efe975bac5cd4bb9be0e779ff0f766
Anton Tayanovskyy <anton@pulumi.com>
2024-06-26 12:34:17 -0400

    Schema simplification - first pass

--- pkg/tfgen/internal/unrec/package_spec_util.go
+++ pkg/tfgen/internal/unrec/package_spec_util.go
@@ -15,10 +15,13 @@
 package unrec
 
 import (
+	"encoding/json"
+	"fmt"
 	"strings"
 
 	pschema "github.com/pulumi/pulumi/pkg/v3/codegen/schema"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/tokens"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/util/contract"
 )
 
 func parseLocalRef(rawRef string) (tokens.Type, bool) {
@@ -55,4 +58,160 @@ func newXPropertyMap(s pschema.ObjectTypeSpec) xPropertyMap {
 		m[prop] = p
 	}
 	return m
+}
+
+type typeRefs map[tokens.Type]struct{}
+
+func newTypeRefs(tok ...tokens.Type) typeRefs {
+	tr := make(typeRefs)
+	tr.Add(tok...)
+	return tr
+}
+
+func (refs typeRefs) Add(tok ...tokens.Type) {
+	for _, t := range tok {
+		refs[t] = struct{}{}
+	}
+}
+
+func (refs typeRefs) Slice() []tokens.Type {
+	x := []tokens.Type{}
+	for k := range refs {
+		x = append(x, k)
+	}
+	return x
+}
+
+func (refs typeRefs) Best() tokens.Type {
+	var best tokens.Type
+	for k := range refs {
+		if best == "" || len(k) < len(best) {
+			best = k
+		}
+	}
+	contract.Assertf(best != "", "expected to find at least one element")
+	return best
+}
+
+func findGenericTypeReferences(x any) (typeRefs, error) {
+	bytes, err := json.Marshal(x)
+	if err != nil {
+		return nil, err
+	}
+
+	var s any
+	if err := json.Unmarshal(bytes, &s); err != nil {
+		return nil, err
+	}
+	tr := make(typeRefs, 0)
+	transformMaps(func(m map[string]any) {
+		ref, ok := detectRef(m)
+		if ok {
+			tr[ref] = struct{}{}
+		}
+	}, s)
+	return tr, nil
+}
+
+func detectRef(m map[string]any) (tokens.Type, bool) {
+	ref, gotRef := m["$ref"]
+	if !gotRef {
+		return "", false
+	}
+	refs, ok := ref.(string)
+	if !ok {
+		return "", false
+	}
+	return parseLocalRef(refs)
+}
+
+func transformMaps(transform func(map[string]any), value any) any {
+	switch value := value.(type) {
+	case map[string]any:
+		transform(value)
+		for k := range value {
+			value[k] = transformMaps(transform, value[k])
+		}
+		return value
+	case []any:
+		for i := range value {
+			value[i] = transformMaps(transform, value[i])
+		}
+		return value
+	default:
+		return value
+	}
+}
+
+func rewriteTypeRefs(rewrites map[tokens.Type]tokens.Type, schema *pschema.PackageSpec) error {
+	bytes, err := json.Marshal(schema)
+	if err != nil {
+		return err
+	}
+
+	var s any
+	if err := json.Unmarshal(bytes, &s); err != nil {
+		return err
+	}
+
+	rewrite := func(m map[string]any) {
+		cleanRef, ok := detectRef(m)
+		if !ok {
+			return
+		}
+		if modifiedRef, ok := rewrites[cleanRef]; ok {
+			m["$ref"] = fmt.Sprintf("#/types/%s", modifiedRef)
+		}
+	}
+
+	modifiedS := transformMaps(rewrite, s)
+
+	modifiedBytes, err := json.Marshal(modifiedS)
+	if err != nil {
+		return err
+	}
+
+	var modifiedSchema pschema.PackageSpec
+	if err := json.Unmarshal(
+		modifiedBytes, &modifiedSchema); err != nil {
+		return err
+	}
+	*schema = modifiedSchema
+
+	return nil
+}
+
+func findResourceTypeReferences(r pschema.ResourceSpec) (typeRefs, error) {
+	return findGenericTypeReferences(r)
+}
+
+func findFunctionTypeReferences(f pschema.FunctionSpec) (typeRefs, error) {
+	return findGenericTypeReferences(f)
+}
+
+func findTypeReferenceTransitiveClosure(spec *pschema.PackageSpec, refs typeRefs) (typeRefs, error) {
+	queue := []tokens.Type{}
+	for r := range refs {
+		queue = append(queue, r)
+	}
+	seen := typeRefs{}
+	for len(queue) > 0 {
+		r := queue[0]
+		queue = queue[1:]
+		if _, ok := seen[r]; ok {
+			continue
+		}
+		t, ok := spec.Types[string(r)]
+		if ok {
+			moreRefs, err := findGenericTypeReferences(t)
+			if err != nil {
+				return nil, err
+			}
+			for m := range moreRefs {
+				queue = append(queue, m)
+			}
+		}
+		seen[r] = struct{}{}
+	}
+	return seen, nil
 }
---
+++ pkg/tfgen/internal/unrec/simplify.go
@@ -0,0 +1,132 @@
+// Copyright 2016-2024, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package unrec
+
+import (
+	"context"
+	"fmt"
+
+	pschema "github.com/pulumi/pulumi/pkg/v3/codegen/schema"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/tokens"
+)
+
+func SimplifyRecursiveTypes(ctx context.Context, schema *pschema.PackageSpec) error {
+	rewriteRules, err := computeAllRewriteRules(schema)
+	if err != nil {
+		return err
+	}
+	return rewriteTypeRefs(rewriteRules, schema)
+}
+
+func computeAllRewriteRules(schema *pschema.PackageSpec) (map[tokens.Type]tokens.Type, error) {
+	cmp := &comparer{schema: schema}
+
+	rewriteRules := make(map[tokens.Type]tokens.Type)
+
+	for _, r := range schema.Resources {
+		typeRefs, err := findResourceTypeReferences(r)
+		if err != nil {
+			return nil, err
+		}
+		if err := computeRewriteRules(cmp, schema, typeRefs, rewriteRules); err != nil {
+			return nil, err
+		}
+	}
+
+	for _, f := range schema.Functions {
+		typeRefs, err := findFunctionTypeReferences(f)
+		if err != nil {
+			return nil, err
+		}
+		if err := computeRewriteRules(cmp, schema, typeRefs, rewriteRules); err != nil {
+			return nil, err
+		}
+	}
+
+	fmt.Println("REWRITE RULES", len(rewriteRules))
+	for k, v := range rewriteRules {
+		fmt.Println("REWRITE", k, v)
+
+	}
+
+	return rewriteRules, nil
+}
+
+// Starting from roots, detect recursive type roots. Simplify all types reachable from the recursive type roots.
+// Simplification rewrites type A with type B if A<=B according to [comparer.LessThanTypeRefs].
+//
+// Although scoped to the progeny of recursive types, this may still end up over-eagerly rewriting logically distinct
+// but structurally identical types and may require some refinement.
+func computeRewriteRules(
+	cmp *comparer,
+	schema *pschema.PackageSpec,
+	roots typeRefs,
+	rewriteRules map[tokens.Type]tokens.Type,
+) error {
+	rd := newRecursionDetector(schema)
+	recursionRoots := rd.Detect(roots.Slice())
+
+	allRefs, err := findTypeReferenceTransitiveClosure(schema, newTypeRefs(recursionRoots...))
+	if err != nil {
+		return err
+	}
+	eqcs := typeEqualityClasses(cmp, allRefs)
+
+	for i, eqc := range eqcs {
+		best := eqc.Best()
+
+		// Check if there are any other classes that have bigger elements.
+		isDominant := true
+		for j, otherc := range eqcs {
+			if i != j && cmp.LessThanTypeRefs(best, otherc.Best()) {
+				isDominant = false
+				break
+			}
+		}
+
+		// Only consider classes that are not dominated by another class.
+		if isDominant {
+			// Add rewrite rules for all types in the class itself.
+			for _, typ := range eqc.Slice() {
+				if typ != best {
+					rewriteRules[typ] = best
+				}
+			}
+			// Add rewrite rules for all classes it dominates.
+			for j, otherc := range eqcs {
+				if i != j && cmp.LessThanTypeRefs(otherc.Best(), best) {
+					for _, typ := range otherc.Slice() {
+						rewriteRules[typ] = best
+					}
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func typeEqualityClasses(cmp *comparer, types typeRefs) []typeRefs {
+	eq := func(a, b tokens.Type) bool {
+		return cmp.EqualTypeRefs(a, b)
+	}
+	eClasses := equalityClasses(eq, types.Slice())
+	var result []typeRefs
+	for _, c := range eClasses {
+		tr := make(typeRefs)
+		tr.Add(c...)
+		result = append(result, tr)
+	}
+	return result
+}
