a0d44d8c4da29e1a512bdfc99924b77e01e838e4
Anton Tayanovskyy <anton@pulumi.com>
2024-06-26 11:21:54 -0400

    Add recursive type detector algo

---
+++ pkg/tfgen/internal/unrec/recursion_detector.go
@@ -0,0 +1,97 @@
+// Copyright 2016-2024, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package unrec
+
+import (
+	"sort"
+
+	pschema "github.com/pulumi/pulumi/pkg/v3/codegen/schema"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/tokens"
+)
+
+// Detects unrolled recursion.
+//
+// TF does not support recursive type definitions but Pulumi does. The idea here is to take a projection of TF recursion
+// unrolling trick and fold it back.
+//
+// Suppose ancestor(T1, T2) if T2 describes a sub-property of T1.
+//
+// Suppose T1 <= T2 if T1 has the subset of properties of T2 with types that are themselves Tx <= Ty.
+//
+// Suppose props(T) defines the set of property names for an object type.
+//
+// The algo looks for this pattern to detect recursion:
+//
+//	ancestor(T1, T2), ancestor(T2, T3),  T3 <= T2 <= T1, props(T1)==props(T2)
+//
+// This needs to use an approximate and not strict equality because the leaf node of a recursively unrolled type will
+// drop recursive properties and therefore not strictly match the ancestor.
+type recursionDetector struct {
+	schema                 *pschema.PackageSpec
+	detectedRecursiveTypes map[tokens.Type]struct{}
+	cmp                    comparer
+}
+
+func newRecursionDetector(schema *pschema.PackageSpec) *recursionDetector {
+	return &recursionDetector{
+		schema:                 schema,
+		detectedRecursiveTypes: map[tokens.Type]struct{}{},
+		cmp:                    comparer{schema: schema},
+	}
+}
+
+func (rd *recursionDetector) Detect(roots []tokens.Type) []tokens.Type {
+	vis := &typeVisitor{Schema: rd.schema, Visit: rd.visit}
+	vis.VisitRoots(roots)
+	result := []tokens.Type{}
+	for t := range rd.detectedRecursiveTypes {
+		result = append(result, t)
+	}
+	sort.Slice(result, func(i, j int) bool {
+		return result[i] < result[j]
+	})
+	return result
+}
+
+func (rd *recursionDetector) visit(ancestors []tokens.Type, current tokens.Type) {
+	for i, ai := range ancestors {
+		if _, visited := rd.detectedRecursiveTypes[ai]; visited {
+			continue
+		}
+		for _, aj := range ancestors[i+1:] {
+			if rd.detect(ai, aj, current) {
+				if rd.detectedRecursiveTypes == nil {
+					rd.detectedRecursiveTypes = map[tokens.Type]struct{}{}
+				}
+				rd.detectedRecursiveTypes[ai] = struct{}{}
+			}
+		}
+	}
+}
+
+func (rd *recursionDetector) detect(t1, t2, t3 tokens.Type) bool {
+	return rd.cmp.LessThanOrEqualTypeRefs(t3, t2) &&
+		rd.cmp.LessThanOrEqualTypeRefs(t2, t1) &&
+		rd.hasSameProps(t1, t2)
+}
+
+func (rd *recursionDetector) hasSameProps(t1, t2 tokens.Type) bool {
+	cts1, ok1 := rd.schema.Types[string(t1)]
+	cts2, ok2 := rd.schema.Types[string(t2)]
+	if !ok1 || !ok2 {
+		return false
+	}
+	return len(cts1.Properties) == len(cts2.Properties)
+}
