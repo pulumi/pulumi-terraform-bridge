01ccdac7b5ae0e4aa624b1d5eb1d0dcfbd31402c
Anton Tayanovskyy <anton@pulumi.com>
2024-06-26 11:05:11 -0400

    Add an example schema

---
+++ pkg/tfgen/internal/unrec/example_schema_test.go
@@ -0,0 +1,38 @@
+// Copyright 2016-2024, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package unrec
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"encoding/json"
+	pschema "github.com/pulumi/pulumi/pkg/v3/codegen/schema"
+)
+
+//go:generate go run gen.go
+func exampleSchema(t *testing.T) *pschema.PackageSpec {
+	b, err := os.ReadFile(filepath.Join("testdata", "test-schema.json"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	var r pschema.PackageSpec
+	err = json.Unmarshal(b, &r)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return &r
+}
---
+++ pkg/tfgen/internal/unrec/gen.go
@@ -0,0 +1,189 @@
+//go:build ignore
+
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge/info"
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfgen"
+	shim "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim/sdk-v2"
+	pschema "github.com/pulumi/pulumi/pkg/v3/codegen/schema"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/diag"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/diag/colors"
+)
+
+func main() {
+	schema, err := exampleSchema()
+	if err != nil {
+		log.Fatal(err)
+	}
+	bytes, err := json.MarshalIndent(schema, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	p := filepath.Join("testdata", "test-schema.json")
+	err = os.WriteFile(p, bytes, 0600)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Wrote", p)
+}
+
+func exampleSchema() (pschema.PackageSpec, error) {
+	return tfgen.GenerateSchema(info.Provider{
+		P: shim.NewProvider(
+			&schema.Provider{
+				ResourcesMap: map[string]*schema.Resource{
+					"web_acl": {
+						Schema: map[string]*schema.Schema{
+							"statement": webACLRootStatementSchema(3),
+						},
+					},
+				},
+			},
+		),
+		Name: "myprov",
+		Resources: map[string]*info.Resource{
+			"web_acl": {
+				Tok: "myprov:index:WebAcl",
+			},
+		},
+	}, diag.DefaultSink(io.Discard, io.Discard, diag.FormatOptions{Color: colors.Never}))
+}
+
+func webACLRootStatementSchema(level int) *schema.Schema {
+	return &schema.Schema{
+		Type:     schema.TypeList,
+		Required: true,
+		MaxItems: 1,
+		Elem: &schema.Resource{
+			Schema: map[string]*schema.Schema{
+				"and_statement":        statementSchema(level),
+				"rate_based_statement": rateBasedStatementSchema(level),
+			},
+		},
+	}
+}
+
+func rateBasedStatementSchema(level int) *schema.Schema {
+	return &schema.Schema{
+		Type:     schema.TypeList,
+		Optional: true,
+		MaxItems: 1,
+		Elem: &schema.Resource{
+			Schema: map[string]*schema.Schema{
+				"aggregate_key_type": {
+					Type:     schema.TypeString,
+					Optional: true,
+				},
+				"scope_down_statement": scopeDownStatementSchema(level - 1),
+			},
+		},
+	}
+}
+
+func scopeDownStatementSchema(level int) *schema.Schema {
+	return &schema.Schema{
+		Type:     schema.TypeList,
+		Optional: true,
+		MaxItems: 1,
+		Elem: &schema.Resource{
+			Schema: map[string]*schema.Schema{
+				"and_statement":       statementSchema(level),
+				"xss_match_statement": xssMatchStatementSchema(),
+			},
+		},
+	}
+}
+
+func statementSchema(level int) *schema.Schema {
+	if level > 1 {
+		return &schema.Schema{
+			Type:     schema.TypeList,
+			Optional: true,
+			MaxItems: 1,
+			Elem: &schema.Resource{
+				Schema: map[string]*schema.Schema{
+					"statement": {
+						Type:     schema.TypeList,
+						Required: true,
+						Elem: &schema.Resource{
+							Schema: map[string]*schema.Schema{
+								"and_statement":       statementSchema(level - 1),
+								"xss_match_statement": xssMatchStatementSchema(),
+							},
+						},
+					},
+				},
+			},
+		}
+	}
+
+	return &schema.Schema{
+		Type:     schema.TypeList,
+		Optional: true,
+		MaxItems: 1,
+		Elem: &schema.Resource{
+			Schema: map[string]*schema.Schema{
+				"statement": {
+					Type:     schema.TypeList,
+					Required: true,
+					Elem: &schema.Resource{
+						Schema: map[string]*schema.Schema{
+							"xss_match_statement": xssMatchStatementSchema(),
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func xssMatchStatementSchema() *schema.Schema {
+	return &schema.Schema{
+		Type:     schema.TypeList,
+		Optional: true,
+		MaxItems: 1,
+		Elem: &schema.Resource{
+			Schema: map[string]*schema.Schema{
+				"field_to_match": fieldToMatchSchema(),
+			},
+		},
+	}
+}
+
+func fieldToMatchSchema() *schema.Schema {
+	return &schema.Schema{
+		Type:     schema.TypeList,
+		Optional: true,
+		MaxItems: 1,
+		Elem:     fieldToMatchBaseSchema(),
+	}
+}
+
+var listOfEmptyObjectSchema *schema.Schema = &schema.Schema{
+	Type:     schema.TypeList,
+	Optional: true,
+	MaxItems: 1,
+	Elem: &schema.Resource{
+		Schema: map[string]*schema.Schema{},
+	},
+}
+
+func emptySchema() *schema.Schema {
+	return listOfEmptyObjectSchema
+}
+
+func fieldToMatchBaseSchema() *schema.Resource {
+	return &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"method": emptySchema(),
+		},
+	}
+}
