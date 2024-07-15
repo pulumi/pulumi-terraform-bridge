e0871b12b9a42faac538214f639cc8fb1f27cbfd
guineveresaenger <guinevere@pulumi.com>
2024-07-15 14:37:51 -0700

    Handle basetypes.MapTypeable for type schema and add test

---
+++ pf/internal/schemashim/type_schema_test.go
@@ -0,0 +1,27 @@
+package schemashim
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
+	shim "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestMapAttribute(t *testing.T) {
+	mapAttr := schema.MapAttribute{
+		Optional:    true,
+		ElementType: basetypes.StringType{},
+	}
+	shimmed := &typeSchema{mapAttr.GetType(), nil}
+	assertIsMapType(t, shimmed)
+	s := shimmed.Elem().(*typeSchema)
+	assert.Equal(t, shim.TypeString, s.Type())
+}
+
+func assertIsMapType(t *testing.T, shimmed shim.Schema) {
+	assert.Equal(t, shim.TypeMap, shimmed.Type())
+	assert.NotNil(t, shimmed.Elem())
+	schema, isTypeSchema := shimmed.Elem().(*typeSchema)
+	assert.Truef(t, isTypeSchema, "expected shim.Elem() to be of type %T", *schema)
+}
