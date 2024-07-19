59ab72c0661441a4823f4d43fa6d3cb952a61a5d
Guinevere Saenger <guinevere@pulumi.com>
2024-07-19 16:12:38 -0700

    Apply default docs edit rules to installation docs (#2211)
    Adds default doc edit rules plus a few more installation-doc specific
    edit rules to the plain docs parser functionality.
    
    Fixes #2208

--- pkg/tfgen/installation_docs.go
+++ pkg/tfgen/installation_docs.go
@@ -23,11 +23,15 @@ func plainDocsParser(docFile *DocFile, g *Generator) ([]byte, error) {
 
 	//TODO: See https://github.com/pulumi/pulumi-terraform-bridge/issues/2078
 	// - translate code blocks with code choosers
-	// - apply default edit rules
 	// - reformat TF names
-	// - Translation for certain headers such as "Arguments Reference" or "Configuration block"
 	// - Ability to omit irrelevant sections
-	return []byte(contentStr), nil
+
+	// Apply edit rules to transform the doc for Pulumi-ready presentation
+	contentBytes, err := applyEditRules([]byte(contentStr), docFile)
+	if err != nil {
+		return nil, err
+	}
+	return contentBytes, nil
 }
 
 func writeFrontMatter(title string) string {
@@ -90,4 +94,33 @@ func writeInstallationInstructions(goImportBasePath, providerName string) string
 		cSharpName,
 		goImportBasePath,
 	)
+}
+
+func applyEditRules(contentBytes []byte, docFile *DocFile) ([]byte, error) {
+	// Obtain default edit rules for documentation files
+	edits := defaultEditRules()
+
+	// Additional edit rules for installation files
+	edits = append(edits,
+		// Replace all "T/terraform" with "P/pulumi"
+		reReplace(`Terraform`, `Pulumi`),
+		reReplace(`terraform`, `pulumi`),
+		// Replace all "H/hashicorp" strings
+		reReplace(`Hashicorp`, `Pulumi`),
+		reReplace(`hashicorp`, `pulumi`),
+		// Reformat certain headers
+		reReplace(`The following arguments are supported`,
+			`The following configuration inputs are supported`),
+		reReplace(`Argument Reference`,
+			`Configuration Reference`),
+		reReplace(`block contains the following arguments`,
+			`input has the following nested fields`))
+	var err error
+	for _, rule := range edits {
+		contentBytes, err = rule.Edit(docFile.FileName, contentBytes)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return contentBytes, nil
 }
