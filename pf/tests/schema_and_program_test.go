b406b57063f2ef81aea87b99583249c3af3876e9
Venelin <venelin@pulumi.com>
2024-07-24 14:25:15 +0300

    plan modifier repro

--- pf/tests/schema_and_program_test.go
+++ pf/tests/schema_and_program_test.go
@@ -1,11 +1,14 @@
 package tfbridgetests
 
 import (
+	"context"
 	"testing"
 
 	pschema "github.com/hashicorp/terraform-plugin-framework/provider/schema"
 	rschema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 	"github.com/pulumi/pulumi-terraform-bridge/pf/tests/internal/providerbuilder"
 	"github.com/pulumi/pulumi/sdk/v3/go/auto/optpreview"
 	"github.com/stretchr/testify/require"
@@ -60,6 +63,69 @@ func TestDefaults(t *testing.T) {
 						"change_reason": rschema.StringAttribute{
 							Optional: true,
 							Default:  stringdefault.StaticString("Default val"),
+						},
+					},
+				},
+			},
+		},
+	}
+
+	prov := bridgedProvider(&provBuilder)
+
+	program := `
+name: test
+runtime: yaml
+resources:
+    mainRes:
+        type: prov:index:Test
+        properties:
+            otherProp: "val"
+outputs:
+    changeReason: ${mainRes.changeReason}`
+
+	pt := pulCheck(t, prov, program)
+	upRes := pt.Up()
+	t.Logf(upRes.StdOut)
+
+	require.Equal(t, "Default val", upRes.Outputs["changeReason"].Value)
+
+	pt.Preview(optpreview.Diff(), optpreview.ExpectNoChanges())
+}
+
+type changeReasonPlanModifier struct {
+	planmodifier.String
+}
+
+func (c changeReasonPlanModifier) PlanModifyString(ctx context.Context, req planmodifier.StringRequest, resp *planmodifier.StringResponse) {
+	resp.PlanValue = basetypes.NewStringValue("Default val")
+}
+
+func (c changeReasonPlanModifier) Description(context.Context) string {
+	return "Change reason plan modifier"
+}
+
+func (c changeReasonPlanModifier) MarkdownDescription(context.Context) string {
+	return "Change reason plan modifier"
+}
+
+func TestPlanModifiers(t *testing.T) {
+	provBuilder := providerbuilder.Provider{
+		TypeName:       "prov",
+		Version:        "0.0.1",
+		ProviderSchema: pschema.Schema{},
+		AllResources: []providerbuilder.Resource{
+			{
+				Name: "test",
+				ResourceSchema: rschema.Schema{
+					Attributes: map[string]rschema.Attribute{
+						"other_prop": rschema.StringAttribute{
+							Optional: true,
+						},
+						"change_reason": rschema.StringAttribute{
+							Optional: true,
+							PlanModifiers: []planmodifier.String{
+								changeReasonPlanModifier{},
+							},
 						},
 					},
 				},
