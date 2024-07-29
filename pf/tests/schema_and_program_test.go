ce3be6b03d3094a89d2d284042b290cde6f833db
Venelin <venelin@pulumi.com>
2024-07-29 12:24:43 +0300

    update repro

--- pf/tests/schema_and_program_test.go
+++ pf/tests/schema_and_program_test.go
@@ -98,20 +98,20 @@ outputs:
 	pt.Preview(optpreview.Diff(), optpreview.ExpectNoChanges())
 }
 
-type changeReasonPlanModifier struct {
+type modifyValuePlanModifier struct {
 	planmodifier.String
 }
 
-func (c changeReasonPlanModifier) PlanModifyString(ctx context.Context, req planmodifier.StringRequest, resp *planmodifier.StringResponse) {
+func (c modifyValuePlanModifier) PlanModifyString(ctx context.Context, req planmodifier.StringRequest, resp *planmodifier.StringResponse) {
 	resp.PlanValue = basetypes.NewStringValue("Modified val")
 }
 
-func (c changeReasonPlanModifier) Description(context.Context) string {
-	return "Change reason plan modifier"
+func (c modifyValuePlanModifier) Description(context.Context) string {
+	return "Modify value plan modifier"
 }
 
-func (c changeReasonPlanModifier) MarkdownDescription(context.Context) string {
-	return "Change reason plan modifier"
+func (c modifyValuePlanModifier) MarkdownDescription(context.Context) string {
+	return "Modify value plan modifier"
 }
 
 func TestPlanModifiers(t *testing.T) {
@@ -131,7 +131,7 @@ func TestPlanModifiers(t *testing.T) {
 							Optional: true,
 							Computed: true,
 							PlanModifiers: []planmodifier.String{
-								changeReasonPlanModifier{},
+								modifyValuePlanModifier{},
 							},
 						},
 					},
@@ -162,6 +162,75 @@ outputs:
 	pt.Preview(optpreview.Diff(), optpreview.ExpectNoChanges())
 }
 
+func TestDefaultAndPlanModifier(t *testing.T) {
+	// Note plan modifiers trump defaults!
+	provBuilder := providerbuilder.Provider{
+		TypeName:       "prov",
+		Version:        "0.0.1",
+		ProviderSchema: pschema.Schema{},
+		AllResources: []providerbuilder.Resource{
+			{
+				Name: "test",
+				ResourceSchema: rschema.Schema{
+					Attributes: map[string]rschema.Attribute{
+						"other_prop": rschema.StringAttribute{
+							Optional: true,
+						},
+						"change_reason": rschema.StringAttribute{
+							Optional: true,
+							Computed: true,
+							Default:  stringdefault.StaticString("Default val"),
+							PlanModifiers: []planmodifier.String{
+								modifyValuePlanModifier{},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	prov := bridgedProvider(&provBuilder)
+
+	program := `
+name: test
+runtime: yaml
+resources:
+    mainRes:
+        type: prov:index:Test
+        properties:
+            otherProp: "val"
+outputs:
+    changeReason: ${mainRes.changeReason}`
+
+	pt := pulCheck(t, prov, program)
+	upRes := pt.Up()
+	t.Logf(upRes.StdOut)
+
+	require.Equal(t, "Modified val", upRes.Outputs["changeReason"].Value)
+}
+
+func ChangeReasonModifier() planmodifier.String {
+	return &changeReasonPlanModifier{}
+}
+
+type changeReasonPlanModifier struct{}
+
+func (id *changeReasonPlanModifier) Description(_ context.Context) string {
+	return "Update change reason in place"
+}
+
+func (id *changeReasonPlanModifier) MarkdownDescription(ctx context.Context) string {
+	return id.Description(ctx)
+}
+
+func (id *changeReasonPlanModifier) PlanModifyString(_ context.Context,
+	req planmodifier.StringRequest, resp *planmodifier.StringResponse,
+) {
+	resp.PlanValue = req.PlanValue
+	resp.RequiresReplace = false
+}
+
 func TestImportAndRefreshWithDefault(t *testing.T) {
 	provBuilder := providerbuilder.Provider{
 		TypeName:       "prov",
@@ -179,6 +248,9 @@ func TestImportAndRefreshWithDefault(t *testing.T) {
 							Optional: true,
 							Computed: true,
 							Default:  stringdefault.StaticString("Default val"),
+							PlanModifiers: []planmodifier.String{
+								ChangeReasonModifier(),
+							},
 						},
 					},
 				},
@@ -227,61 +299,4 @@ outputs:
 
 	prevRes := pt.Preview(optpreview.Diff(), optpreview.ExpectNoChanges())
 	t.Logf(prevRes.StdOut)
-}
-
-func TestDefaultAndPlanModifier(t *testing.T) {
-	provBuilder := providerbuilder.Provider{
-		TypeName:       "prov",
-		Version:        "0.0.1",
-		ProviderSchema: pschema.Schema{},
-		AllResources: []providerbuilder.Resource{
-			{
-				Name: "test",
-				ResourceSchema: rschema.Schema{
-					Attributes: map[string]rschema.Attribute{
-						"other_prop": rschema.StringAttribute{
-							Optional: true,
-						},
-						"change_reason": rschema.StringAttribute{
-							Optional: true,
-							Computed: true,
-							Default:  stringdefault.StaticString("Default val"),
-							PlanModifiers: []planmodifier.String{
-								changeReasonPlanModifier{},
-							},
-						},
-					},
-				},
-				ReadFunc: func(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
-					resp.State.SetAttribute(ctx, path.Root("id"), "test-id")
-					resp.State.SetAttribute(ctx, path.Root("other_prop"), "val")
-					resp.State.SetAttribute(ctx, path.Root("change_reason"), "Default val")
-				},
-				ImportStateFunc: func(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
-					resp.State.SetAttribute(ctx, path.Root("id"), "test-id")
-					resp.State.SetAttribute(ctx, path.Root("other_prop"), "val")
-					resp.State.SetAttribute(ctx, path.Root("change_reason"), "Default val")
-				},
-			},
-		},
-	}
-
-	prov := bridgedProvider(&provBuilder)
-
-	program := `
-name: test
-runtime: yaml
-resources:
-    mainRes:
-        type: prov:index:Test
-        properties:
-            otherProp: "val"
-outputs:
-    changeReason: ${mainRes.changeReason}`
-
-	pt := pulCheck(t, prov, program)
-	upRes := pt.Up()
-	t.Logf(upRes.StdOut)
-
-	require.Equal(t, "Modified val", upRes.Outputs["changeReason"].Value)
 }
