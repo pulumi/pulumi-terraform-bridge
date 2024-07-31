9b70513fd74776916dbd88aef983253961cfc994
Venelin <venelin@pulumi.com>
2024-07-31 16:24:55 +0300

    additional tests

--- pkg/tests/schema_pulumi_test.go
+++ pkg/tests/schema_pulumi_test.go
@@ -1579,6 +1579,7 @@ outputs:
 	})
 }
 
+// TODO[pulumi/pulumi-terraform-bridge#2274]: Move to actual cross-test suite once the plumbing is done
 func TestConfigureCrossTest(t *testing.T) {
 	resMap := map[string]*schema.Resource{
 		"prov_test": {
@@ -1591,44 +1592,47 @@ func TestConfigureCrossTest(t *testing.T) {
 		},
 	}
 
-	var tfRd *schema.ResourceData
-	var puRd *schema.ResourceData
-	_ = puRd // ignore unused warning
-	tfp := &schema.Provider{
-		ResourcesMap: resMap,
-		Schema: map[string]*schema.Schema{
-			"config": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-		},
-		ConfigureContextFunc: func(ctx context.Context, rd *schema.ResourceData) (interface{}, diag.Diagnostics) {
-			if tfRd == nil {
-				tfRd = rd
-			} else {
-				puRd = rd
-			}
+	runTest := func(t *testing.T, sch map[string]*schema.Schema, pulumiProgram, tfProgram string) {
+		var tfRd *schema.ResourceData
+		var puRd *schema.ResourceData
+		_ = puRd // ignore unused warning
+		tfp := &schema.Provider{
+			ResourcesMap: resMap,
+			Schema:       sch,
+			ConfigureContextFunc: func(ctx context.Context, rd *schema.ResourceData) (interface{}, diag.Diagnostics) {
+				if tfRd == nil {
+					tfRd = rd
+				} else {
+					puRd = rd
+				}
 
-			return nil, nil
-		},
+				return nil, nil
+			},
+		}
+
+		tfdriver := tfcheck.NewTfDriver(t, t.TempDir(), "prov", tfp)
+		tfdriver.Write(t, tfProgram)
+		tfdriver.Plan(t)
+		require.NotNil(t, tfRd)
+		require.Nil(t, puRd)
+
+		bridgedProvider := pulcheck.BridgedProvider(t, "prov", tfp)
+
+		pt := pulcheck.PulCheck(t, bridgedProvider, pulumiProgram)
+		pt.Preview()
+		require.NotNil(t, puRd)
+		require.Equal(t, tfRd.GetRawConfig(), puRd.GetRawConfig())
 	}
 
-	tfdriver := tfcheck.NewTfDriver(t, t.TempDir(), "prov", tfp)
-	tfdriver.Write(t, `
-provider "prov" {
-	config = "val"
-}
-
-resource "prov_test" "test" {
-	test = "val"
-}`,
-	)
-	tfdriver.Plan(t)
-	require.NotNil(t, tfRd)
-	require.Nil(t, puRd)
-
-	bridgedProvider := pulcheck.BridgedProvider(t, "prov", tfp)
-	program := `
+	t.Run("string attr", func(t *testing.T) {
+		runTest(t,
+			map[string]*schema.Schema{
+				"config": {
+					Type:     schema.TypeString,
+					Optional: true,
+				},
+			},
+			`
 name: test
 runtime: yaml
 resources:
@@ -1641,9 +1645,127 @@ resources:
 		type: prov:index:Test
 		properties:
 			test: "val"
-`
-	pt := pulcheck.PulCheck(t, bridgedProvider, program)
-	pt.Up()
-	require.NotNil(t, puRd)
-	require.Equal(t, tfRd.GetRawConfig(), puRd.GetRawConfig())
+`,
+			`
+provider "prov" {
+	config = "val"
+}
+
+resource "prov_test" "test" {
+	test = "val"
+}`)
+	})
+
+	t.Run("object block", func(t *testing.T) {
+		runTest(t,
+			map[string]*schema.Schema{
+				"config": {
+					Type:     schema.TypeList,
+					Optional: true,
+					Elem: &schema.Resource{
+						Schema: map[string]*schema.Schema{
+							"prop": {
+								Type:     schema.TypeString,
+								Optional: true,
+							},
+						},
+					},
+					MaxItems: 1,
+				},
+			},
+			`
+name: test
+runtime: yaml
+resources:
+	prov:
+		type: pulumi:providers:prov
+		defaultProvider: true
+		properties:
+			config: {"prop": "val"}
+	mainRes:
+		type: prov:index:Test
+		properties:
+			test: "val"
+`,
+			`
+provider "prov" {
+	config {
+		prop = "val"
+	}
+}
+
+resource "prov_test" "test" {
+	test = "val"
+}`)
+	})
+
+	t.Run("list config", func(t *testing.T) {
+		runTest(t,
+			map[string]*schema.Schema{
+				"config": {
+					Type:     schema.TypeList,
+					Optional: true,
+					Elem: &schema.Schema{
+						Type: schema.TypeString,
+					},
+				},
+			},
+			`
+name: test
+runtime: yaml
+resources:
+	prov:
+		type: pulumi:providers:prov
+		defaultProvider: true
+		properties:
+			configs: ["val"]
+	mainRes:
+		type: prov:index:Test
+		properties:
+			test: "val"
+`,
+			`
+provider "prov" {
+	config = ["val"]
+}
+
+resource "prov_test" "test" {
+	test = "val"
+}`)
+	})
+
+	t.Run("set config", func(t *testing.T) {
+		runTest(t,
+			map[string]*schema.Schema{
+				"config": {
+					Type:     schema.TypeSet,
+					Optional: true,
+					Elem: &schema.Schema{
+						Type: schema.TypeString,
+					},
+				},
+			},
+			`
+name: test
+runtime: yaml
+resources:
+	prov:
+		type: pulumi:providers:prov
+		defaultProvider: true
+		properties:
+			configs: ["val"]
+	mainRes:
+		type: prov:index:Test
+		properties:
+			test: "val"
+`,
+			`
+provider "prov" {
+	config = ["val"]
+}
+
+resource "prov_test" "test" {
+	test = "val"
+}`)
+	})
 }
