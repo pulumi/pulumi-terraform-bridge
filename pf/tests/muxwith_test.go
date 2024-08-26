83c6a0a5580483fd7d2b924652cf6135140f6570
Anton Tayanovskyy <anton@pulumi.com>
2024-08-26 16:57:21 -0400

    Simplify using MuxWith with PF (#2360)
    Currently `info.Provider` allows overriding resource or function
    implementations by specifying `MuxWith` section with a
    list of `MuxProvider` instances. This PR adds a function to easily wrap
    a Plugin Framework based provider into a
    `MuxProvider` instance:
    
    pf.NewMuxProvider(context.Context, info.Provider, *ProviderMetadata)
    (info.MuxProvider, error)
    
    This also enables bridge developers to more easily create end-to-end
    tests against providers implemented with the Plugin
    Framework.

---
+++ pf/tests/muxwith_test.go
@@ -0,0 +1,67 @@
+// Copyright 2016-2023, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tfbridgetests
+
+import (
+	"context"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	pb "github.com/pulumi/pulumi-terraform-bridge/pf/tests/internal/providerbuilder"
+	"github.com/stretchr/testify/require"
+)
+
+// Add coverage for pf.NewMuxProvider.
+func TestNewMuxProvider(t *testing.T) {
+	createCallCount := 0
+
+	r := pb.Resource{
+		Name: "r",
+		CreateFunc: func(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+			t.Logf("CREATE called: %v", req.Plan.Raw.String())
+			diags := resp.State.SetAttribute(ctx, path.Root("id"), "id0")
+			resp.Diagnostics = append(resp.Diagnostics, diags...)
+			createCallCount++
+		},
+		ResourceSchema: schema.Schema{
+			Attributes: map[string]schema.Attribute{
+				"p": schema.StringAttribute{
+					Optional: true,
+				},
+			},
+		},
+	}
+
+	p := pb.NewProvider(pb.NewProviderArgs{
+		AllResources: []pb.Resource{r},
+	})
+
+	// Providers using newPulumiTest utilize NewMuxProvider and MuxWith to connect up a PF provider.
+	pt := newPulumiTest(t, p, `
+		name: test-program
+		runtime: yaml
+		resources:
+		  my-res:
+		    type: testprovider:index:R
+		    properties:
+		      p: "FOO"
+		`)
+
+	pt.Up()
+
+	require.Equal(t, 1, createCallCount)
+}
