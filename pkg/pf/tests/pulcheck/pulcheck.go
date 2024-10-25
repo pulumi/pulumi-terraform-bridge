81e768a54992a08c557f02f1ecb7f7b64f824b81
Venelin <venelin@pulumi.com>
2024-10-24 19:05:47 -0700

    expose pf pulcheck

---
+++ pkg/pf/tests/pulcheck/pulcheck.go
@@ -0,0 +1,116 @@
+package pulcheck
+
+import (
+	"bytes"
+	"context"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"testing"
+
+	"github.com/pulumi/providertest/providers"
+	"github.com/pulumi/providertest/pulumitest"
+	"github.com/pulumi/providertest/pulumitest/opttest"
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/pf/tfbridge"
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/pf/tfgen"
+	tfbridge0 "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge/info"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/diag"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/diag/colors"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/util/rpcutil"
+	pulumirpc "github.com/pulumi/pulumi/sdk/v3/proto/go"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+)
+
+func testSink(t *testing.T) diag.Sink {
+	var stdout, stderr bytes.Buffer
+
+	testSink := diag.DefaultSink(&stdout, &stderr, diag.FormatOptions{
+		Color: colors.Never,
+	})
+
+	t.Cleanup(func() {
+		if strings.TrimSpace(stdout.String()) != "" {
+			t.Logf("%s\n", stdout.String())
+		}
+		if strings.TrimSpace(stderr.String()) != "" {
+			t.Logf("%s\n", stderr.String())
+		}
+	})
+
+	return testSink
+}
+
+func genMetadata(t *testing.T, info tfbridge0.ProviderInfo) (tfbridge.ProviderMetadata, error) {
+	generated, err := tfgen.GenerateSchema(context.Background(), tfgen.GenerateSchemaOptions{
+		ProviderInfo:    info,
+		DiagnosticsSink: testSink(t),
+	})
+	if err != nil {
+		return tfbridge.ProviderMetadata{}, err
+	}
+	return generated.ProviderMetadata, nil
+}
+
+func newProviderServer(t *testing.T, info tfbridge0.ProviderInfo) (pulumirpc.ResourceProviderServer, error) {
+	ctx := context.Background()
+	meta, err := genMetadata(t, info)
+	if err != nil {
+		return nil, err
+	}
+	srv, err := tfbridge.NewProviderServer(ctx, nil, info, meta)
+	require.NoError(t, err)
+	return srv, nil
+}
+
+func startPulumiProvider(t *testing.T, prov pulumirpc.ResourceProviderServer) *rpcutil.ServeHandle {
+	handle, err := rpcutil.ServeWithOptions(rpcutil.ServeOptions{
+		Init: func(srv *grpc.Server) error {
+			pulumirpc.RegisterResourceProviderServer(srv, prov)
+			return nil
+		},
+	})
+	require.NoErrorf(t, err, "rpcutil.ServeWithOptions failed")
+	return &handle
+}
+
+func skipUnlessLinux(t *testing.T) {
+	if ci, ok := os.LookupEnv("CI"); ok && ci == "true" && !strings.Contains(strings.ToLower(runtime.GOOS), "linux") {
+		// TODO[pulumi/pulumi-terraform-bridge#2221]
+		t.Skip("Skipping on non-Linux platforms")
+	}
+}
+
+// TODO: docs
+func PulCheck(t *testing.T, bridgedProvider info.Provider, program string, opts ...opttest.Option) (*pulumitest.PulumiTest, error) {
+	skipUnlessLinux(t)
+	puwd := t.TempDir()
+	p := filepath.Join(puwd, "Pulumi.yaml")
+
+	err := os.WriteFile(p, []byte(program), 0o600)
+	require.NoError(t, err)
+
+	prov, err := newProviderServer(t, bridgedProvider)
+	if err != nil {
+		return nil, err
+	}
+
+	defaultOpts := []opttest.Option{
+		opttest.Env("DISABLE_AUTOMATIC_PLUGIN_ACQUISITION", "true"),
+		opttest.TestInPlace(),
+		opttest.SkipInstall(),
+		opttest.AttachProvider(
+			bridgedProvider.Name,
+			func(ctx context.Context, pt providers.PulumiTest) (providers.Port, error) {
+				handle := startPulumiProvider(t, prov)
+				return providers.Port(handle.Port), nil
+			},
+		),
+	}
+
+	defaultOpts = append(defaultOpts, opts...)
+
+	return pulumitest.NewPulumiTest(t, puwd, defaultOpts...), nil
+}
