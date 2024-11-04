001c582610b7cfa0053c202b81725dd2b869c5ec
Venelin <venelin@pulumi.com>
2024-11-04 20:32:31 +0000

    add tests

--- pkg/pf/tests/internal/cross-tests/tfwrite_test.go
+++ pkg/pf/tests/internal/cross-tests/tfwrite_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	pschema "github.com/hashicorp/terraform-plugin-framework/provider/schema"
+	rschema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hexops/autogold/v2"
 	"github.com/stretchr/testify/require"
 	"github.com/zclconf/go-cty/cty"
@@ -176,6 +177,178 @@ func TestWritePFHCLProvider(t *testing.T) {
 
 			var actual bytes.Buffer
 			err := writeProvider(&actual, tt.schema, "test", tt.value)
+			require.NoError(t, err)
+			tt.expect.Equal(t, actual.String())
+		})
+	}
+}
+
+func TestWritePFHCLResource(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name   string
+		value  map[string]cty.Value
+		schema rschema.Schema
+		expect autogold.Value
+	}{
+		{
+			name: "string-attr",
+			value: map[string]cty.Value{
+				"key": cty.StringVal("value"),
+			},
+			schema: rschema.Schema{
+				Attributes: map[string]rschema.Attribute{
+					"key": rschema.StringAttribute{Optional: true},
+				},
+			},
+			expect: autogold.Expect(`resource "testprovider_test" "test" {
+  key = "value"
+}
+`),
+		},
+		{
+			name: "bool-attr",
+			value: map[string]cty.Value{
+				"key": cty.BoolVal(true),
+			},
+			schema: rschema.Schema{
+				Attributes: map[string]rschema.Attribute{
+					"key": rschema.BoolAttribute{Optional: true},
+				},
+			},
+			expect: autogold.Expect(`resource "testprovider_test" "test" {
+  key = true
+}
+`),
+		},
+		{
+			name: "null-attr",
+			value: map[string]cty.Value{
+				"key": cty.NullVal(cty.String),
+			},
+			schema: rschema.Schema{
+				Attributes: map[string]rschema.Attribute{
+					"key": rschema.StringAttribute{Optional: true},
+				},
+			},
+			expect: autogold.Expect(`resource "testprovider_test" "test" {
+  key = null
+}
+`),
+		},
+		{
+			name: "single-nested-attr",
+			value: map[string]cty.Value{
+				"key": cty.ObjectVal(map[string]cty.Value{
+					"n1": cty.NumberIntVal(123),
+					"n2": cty.ObjectVal(map[string]cty.Value{
+						"dn1": cty.BoolVal(true),
+					}),
+				}),
+			},
+			schema: rschema.Schema{
+				Attributes: map[string]rschema.Attribute{
+					"key": rschema.SingleNestedAttribute{
+						Attributes: map[string]rschema.Attribute{
+							"n1": rschema.NumberAttribute{Optional: true},
+							"n2": rschema.SingleNestedAttribute{
+								Attributes: map[string]rschema.Attribute{
+									"dn1": rschema.BoolAttribute{Optional: true},
+								},
+							},
+						},
+					},
+				},
+			},
+			expect: autogold.Expect(`resource "testprovider_test" "test" {
+  key = {
+    n1 = 123
+    n2 = {
+      dn1 = true
+    }
+  }
+}
+`),
+		},
+		{
+			name: "single-nested-block",
+			value: map[string]cty.Value{
+				"key": cty.ObjectVal(map[string]cty.Value{
+					"n1": cty.NumberIntVal(123),
+					"n2": cty.ObjectVal(map[string]cty.Value{
+						"dn1": cty.BoolVal(true),
+					}),
+				}),
+			},
+			schema: rschema.Schema{
+				Blocks: map[string]rschema.Block{
+					"key": rschema.SingleNestedBlock{
+						Attributes: map[string]rschema.Attribute{
+							"n1": rschema.NumberAttribute{Optional: true},
+						},
+						Blocks: map[string]rschema.Block{
+							"n2": rschema.SingleNestedBlock{
+								Attributes: map[string]rschema.Attribute{
+									"dn1": rschema.BoolAttribute{Optional: true},
+								},
+							},
+						},
+					},
+				},
+			},
+			expect: autogold.Expect(`resource "testprovider_test" "test" {
+  key {
+    n1 = 123
+    n2 {
+      dn1 = true
+    }
+  }
+}
+`),
+		},
+		{
+			name: "list-nested-block",
+			value: map[string]cty.Value{
+				"key": cty.ListVal([]cty.Value{
+					cty.ObjectVal(map[string]cty.Value{
+						"dn1": cty.BoolVal(true),
+					}),
+					cty.ObjectVal(map[string]cty.Value{
+						"dn1": cty.BoolVal(false),
+					}),
+				}),
+			},
+			schema: rschema.Schema{
+				Blocks: map[string]rschema.Block{
+					"key": rschema.ListNestedBlock{
+						NestedObject: rschema.NestedBlockObject{
+							Attributes: map[string]rschema.Attribute{
+								"dn1": rschema.BoolAttribute{Optional: true},
+							},
+						},
+					},
+				},
+			},
+			expect: autogold.Expect(`resource "testprovider_test" "test" {
+  key {
+    dn1 = true
+  }
+  key {
+    dn1 = false
+  }
+}
+`),
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			var actual bytes.Buffer
+			err := writeResource(&actual, tt.schema, "testprovider_test", "test", tt.value)
 			require.NoError(t, err)
 			tt.expect.Equal(t, actual.String())
 		})
