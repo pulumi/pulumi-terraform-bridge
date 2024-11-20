ad7f844dccaf4bbbeb414f4d9bb213bc0cc3ec72
Venelin <venelin@pulumi.com>
2024-11-20 14:13:35 +0000

    lint

--- pkg/pf/tests/diff_secret_test.go
+++ pkg/pf/tests/diff_secret_test.go
@@ -7,13 +7,14 @@ import (
 	rschema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hexops/autogold/v2"
+	"github.com/pulumi/pulumi/sdk/v3/go/auto/optpreview"
+	"github.com/stretchr/testify/require"
+
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/pf/tests/internal/providerbuilder"
 	pb "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/pf/tests/internal/providerbuilder"
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/pf/tests/pulcheck"
 	tfbridge0 "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge/info"
-	"github.com/pulumi/pulumi/sdk/v3/go/auto/optpreview"
-	"github.com/stretchr/testify/require"
 )
 
 func TestSecretBasic(t *testing.T) {
