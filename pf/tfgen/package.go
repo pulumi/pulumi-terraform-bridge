0852d643b60d93da91993e93d195227da15ac207
VenelinMartinov <venelin.v.martinov@gmail.com>
2024-12-06 11:42:32 +0000

    Delete `pf` module (#2690)
    This deletes the top-level `pf` module as it is no longer used and
    changes the bridge release trigger to fire off `pkg` releases.
    
    Reverts https://github.com/pulumi/pulumi-terraform-bridge/pull/2498,
    which gets https://github.com/pulumi/pulumi-terraform-bridge/pull/2492
    back in.

--- pf/tfgen/package.go
+++
@@ -1,4 +0,0 @@
-// Package tfgen implements build-time introspection and generating a [Pulumi Package Schema].
-//
-// [Pulumi Package Schema]: https://www.pulumi.com/docs/guides/pulumi-packages/schema/
-package tfgen
