1f0872880cc47694ed50829dae9e0f82eed161e8
Anton Tayanovskyy <anton@pulumi.com>
2024-12-10 18:04:24 -0500

    Vendor tfplugin5 as well as tfplugin6

--- pkg/vendored/proto/flake.nix
+++ pkg/vendored/proto/flake.nix
@@ -28,6 +28,24 @@
         system = sys;
       };
 
+      tfplugin5-protos = pkgs.stdenv.mkDerivation {
+        coreutils = pkgs.coreutils;
+        gnused = pkgs.gnused;
+        name = "tfplugin5-protos";
+        builder = "${pkgs.bash}/bin/bash";
+        args = [ "-c" ''
+          export PATH=$coreutils/bin:$gnused/bin
+          mkdir -p $out
+          orig=github.com/opentofu/opentofu/internal/tfplugin5
+          dest=github.com/pulumi/pulumi-terraform-bridge/v3/pkg/vendored/tfplugin5
+          cat $src/docs/plugin-protocol/tfplugin5.5.proto |
+              sed -r "s#$orig#$dest#g" |
+              sed -r "s#package tfplugin5;#package tfplugin5_pulumi;#g" >$out/tfplugin5_pulumi.proto
+        ''];
+        src = opentofu_src;
+        system = sys;
+      };
+
       tfplugin6-go = pkgs.stdenv.mkDerivation {
         name = "tfplugin6-go";
         src = ./.;
@@ -49,8 +67,31 @@
         system = sys;
       };
 
+      tfplugin5-go = pkgs.stdenv.mkDerivation {
+        name = "tfplugin5-go";
+        src = ./.;
+        builder = "${pkgs.bash}/bin/bash";
+        coreutils = pkgs.coreutils;
+        protoc = pkgs.protobuf3_20;
+        gengo = pkgs.protoc-gen-go;
+        gengogrpc = pkgs.protoc-gen-go-grpc;
+        args = [ "-c" ''
+          set -euo pipefail
+          export PATH=$coreutils/bin:$protoc/bin:$gengo/bin:$gengogrpc/bin
+          mkdir -p $out
+          cp ${tfplugin5-protos}/tfplugin5_pulumi.proto $out/tfplugin5_pulumi.proto
+          cd $out
+          protoc --go_out=. --go-grpc_out=. --proto_path ${tfplugin5-protos} ${tfplugin5-protos}/tfplugin5_pulumi.proto
+          mv $out/github.com/pulumi/pulumi-terraform-bridge/v3/pkg/vendored/tfplugin5/* $out/
+          rm -rf $out/github.com
+        ''];
+        system = sys;
+      };
+
     in {
-      default = tfplugin6-go;
+      inherit tfplugin6-go tfplugin5-go;
+
+      default = pkgs.symlinkJoin { name = "protos"; paths = [ tfplugin6-go tfplugin5-go ]; };
     };
   in {
     packages = {
