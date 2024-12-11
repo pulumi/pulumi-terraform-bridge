6bbab4ae48a311f7ad0e7f38ae3c97ab8319056c
Anton Tayanovskyy <anton@pulumi.com>
2024-12-10 20:34:33 -0500

    Fix rebase

--- dynamic/main.go
+++ dynamic/main.go
@@ -23,7 +23,6 @@ import (
 
 	"github.com/blang/semver"
 	"github.com/pkg/errors"
-	"github.com/pulumi/pulumi-terraform-bridge/dynamic/internal/shim/run"
 	"github.com/pulumi/pulumi/pkg/v3/codegen/schema"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/diag"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/diag/colors"
