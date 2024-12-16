e58abcac2d965631dc7ecd597f2ab74c0c1b89c4
Venelin <venelin@pulumi.com>
2024-12-16 13:32:55 +0200

    Only return a detaied diff if a diff is detected

--- pkg/tfbridge/provider.go
+++ pkg/tfbridge/provider.go
@@ -1175,13 +1175,13 @@ func (p *Provider) Diff(ctx context.Context, req *pulumirpc.DiffRequest) (*pulum
 			changes = pulumirpc.DiffResponse_DIFF_NONE
 		} else {
 			changes = pulumirpc.DiffResponse_DIFF_SOME
-		}
 
-		replaceDecision := diff.RequiresNew()
-		detailedDiff, err = makeDetailedDiffV2(
-			ctx, schema, fields, res.TF, p.tf, state, diff, assets, p.supportsSecrets, news, &replaceDecision)
-		if err != nil {
-			return nil, err
+			replaceDecision := diff.RequiresNew()
+			detailedDiff, err = makeDetailedDiffV2(
+				ctx, schema, fields, res.TF, p.tf, state, diff, assets, p.supportsSecrets, news, &replaceDecision)
+			if err != nil {
+				return nil, err
+			}
 		}
 	} else {
 		dd := makeDetailedDiffExtra(ctx, schema, fields, olds, news, diff)
