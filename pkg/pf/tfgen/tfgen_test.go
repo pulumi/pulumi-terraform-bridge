14686d1b0659336a4424d058fb4e3f9bb56bd21b
Venelin <venelin@pulumi.com>
2025-01-02 17:33:43 +0200

    repro for 2803

--- pkg/pf/tfgen/tfgen_test.go
+++ pkg/pf/tfgen/tfgen_test.go
@@ -23,6 +23,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-framework-validators/listvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	dschema "github.com/hashicorp/terraform-plugin-framework/datasource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/provider"
 	pschema "github.com/hashicorp/terraform-plugin-framework/provider/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
@@ -31,11 +32,12 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hexops/autogold/v2"
 	pulumiSchema "github.com/pulumi/pulumi/pkg/v3/codegen/schema"
-	pulumischema "github.com/pulumi/pulumi/pkg/v3/codegen/schema"
 	"github.com/stretchr/testify/require"
 
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/pf/internal/providerbuilder"
 	pftfbridge "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/pf/tfbridge"
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge/info"
 )
 
 // Regressing an issue with AWS provider not recognizing that assume_role config setting is singular via
@@ -453,7 +455,7 @@ func TestTypeOverride(t *testing.T) {
 						"test_res": tt.info,
 					},
 					// Trim the schema for easier comparison
-					SchemaPostProcessor: func(p *pulumischema.PackageSpec) {
+					SchemaPostProcessor: func(p *pulumiSchema.PackageSpec) {
 						p.Language = nil
 						p.Provider.Description = ""
 					},
@@ -470,4 +472,361 @@ func TestTypeOverride(t *testing.T) {
 			autogold.ExpectFile(t, autogold.Raw(b.String()))
 		})
 	}
+}
+
+func TestDataSourceAttributeVsBlockSchemaNameOverride(t *testing.T) {
+	t.Parallel()
+
+	attrRes := dschema.Schema{
+		Attributes: map[string]dschema.Attribute{
+			"id": dschema.StringAttribute{Computed: true},
+			"collection": dschema.ListNestedAttribute{
+				NestedObject: dschema.NestedAttributeObject{
+					Attributes: map[string]dschema.Attribute{
+						"prop": dschema.StringAttribute{
+							Optional: true,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	blockRes := dschema.Schema{
+		Attributes: map[string]dschema.Attribute{
+			"id": dschema.StringAttribute{Computed: true},
+		},
+		Blocks: map[string]dschema.Block{
+			"collection": dschema.ListNestedBlock{
+				NestedObject: dschema.NestedBlockObject{
+					Attributes: map[string]dschema.Attribute{
+						"prop": dschema.StringAttribute{
+							Optional: true,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	prov := providerbuilder.NewProvider(
+		providerbuilder.NewProviderArgs{
+			AllDataSources: []providerbuilder.DataSource{
+				{
+					Name:             "attr_res",
+					DataSourceSchema: attrRes,
+				},
+				{
+					Name:             "block_res",
+					DataSourceSchema: blockRes,
+				},
+			},
+		},
+	)
+
+	providerInfo := prov.ToProviderInfo()
+
+	providerInfo.DataSources["testprovider_attr_res"].Fields = map[string]*tfbridge.SchemaInfo{
+		"collection": {
+			Name: "collection1",
+			Elem: &tfbridge.SchemaInfo{
+				Fields: map[string]*tfbridge.SchemaInfo{
+					"prop": {Name: "prop1"},
+				},
+			},
+		},
+	}
+
+	providerInfo.DataSources["testprovider_block_res"].Fields = map[string]*tfbridge.SchemaInfo{
+		"collection": {
+			Name: "collection1",
+			Elem: &tfbridge.SchemaInfo{
+				Fields: map[string]*tfbridge.SchemaInfo{
+					"prop": {Name: "prop1"},
+				},
+			},
+		},
+	}
+
+	res, err := GenerateSchema(context.Background(), GenerateSchemaOptions{
+		ProviderInfo: providerInfo,
+	})
+	require.NoError(t, err)
+	var prettyJSON bytes.Buffer
+	if err := json.Indent(&prettyJSON, res.ProviderMetadata.PackageSchema, "", "    "); err != nil {
+		t.Fatal(err)
+	}
+
+	autogold.ExpectFile(t, autogold.Raw(prettyJSON.String()))
+}
+
+func TestResourceAttributeVsBlockSchemaNameOverride(t *testing.T) {
+	t.Skip("TODO[pulumi/pulumi-terraform-bridge#2803] Skip until fixed")
+	t.Parallel()
+
+	attrRes := rschema.Schema{
+		Attributes: map[string]rschema.Attribute{
+			"id": rschema.StringAttribute{Computed: true},
+			"collection": rschema.ListNestedAttribute{
+				NestedObject: rschema.NestedAttributeObject{
+					Attributes: map[string]rschema.Attribute{
+						"prop": rschema.StringAttribute{
+							Optional: true,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	blockRes := rschema.Schema{
+		Attributes: map[string]rschema.Attribute{
+			"id": rschema.StringAttribute{Computed: true},
+		},
+		Blocks: map[string]rschema.Block{
+			"collection": rschema.ListNestedBlock{
+				NestedObject: rschema.NestedBlockObject{
+					Attributes: map[string]rschema.Attribute{
+						"prop": rschema.StringAttribute{
+							Optional: true,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	prov := providerbuilder.NewProvider(
+		providerbuilder.NewProviderArgs{
+			AllResources: []providerbuilder.Resource{
+				{
+					Name:           "attr_res",
+					ResourceSchema: attrRes,
+				},
+				{
+					Name:           "block_res",
+					ResourceSchema: blockRes,
+				},
+			},
+		},
+	)
+
+	providerInfo := prov.ToProviderInfo()
+
+	providerInfo.Resources["testprovider_attr_res"].Fields = map[string]*tfbridge.SchemaInfo{
+		"collection": {
+			Name: "collection1",
+			Elem: &tfbridge.SchemaInfo{
+				Fields: map[string]*tfbridge.SchemaInfo{
+					"prop": {Name: "prop1"},
+				},
+			},
+		},
+	}
+
+	providerInfo.Resources["testprovider_block_res"].Fields = map[string]*tfbridge.SchemaInfo{
+		"collection": {
+			Name: "collection1",
+			Elem: &tfbridge.SchemaInfo{
+				Fields: map[string]*tfbridge.SchemaInfo{
+					"prop": {Name: "prop1"},
+				},
+			},
+		},
+	}
+
+	res, err := GenerateSchema(context.Background(), GenerateSchemaOptions{
+		ProviderInfo: providerInfo,
+	})
+	require.NoError(t, err)
+	var prettyJSON bytes.Buffer
+	if err := json.Indent(&prettyJSON, res.ProviderMetadata.PackageSchema, "", "    "); err != nil {
+		t.Fatal(err)
+	}
+
+	autogold.ExpectFile(t, autogold.Raw(prettyJSON.String()))
+}
+
+func TestRegressDatabricksNestedCsharpOverrideBlocksSchema(t *testing.T) {
+	t.Parallel()
+	dsSchemaBlocks := dschema.Schema{
+		Blocks: map[string]dschema.Block{
+			"endpoints": dschema.ListNestedBlock{
+				NestedObject: dschema.NestedBlockObject{
+					Blocks: map[string]dschema.Block{
+						"ai_gateway": dschema.ListNestedBlock{
+							NestedObject: dschema.NestedBlockObject{
+								Blocks: map[string]dschema.Block{
+									"guardrails": dschema.ListNestedBlock{
+										NestedObject: dschema.NestedBlockObject{
+											Blocks: map[string]dschema.Block{
+												"input": dschema.ListNestedBlock{
+													NestedObject: dschema.NestedBlockObject{
+														Attributes: map[string]dschema.Attribute{
+															"max_batch_size": dschema.Int64Attribute{},
+														},
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	schemaProvider := providerbuilder.NewProvider(
+		providerbuilder.NewProviderArgs{
+			AllDataSources: []providerbuilder.DataSource{
+				{
+					Name:             "serving_endpoints",
+					DataSourceSchema: dsSchemaBlocks,
+					ReadFunc: func(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+					},
+				},
+			},
+		},
+	)
+	providerInfo := schemaProvider.ToProviderInfo()
+
+	providerInfo.DataSources["testprovider_serving_endpoints"].Fields = map[string]*info.Schema{
+		"endpoints": {
+			Name: "endpoints1",
+			Elem: &info.Schema{
+				Fields: map[string]*info.Schema{
+					"ai_gateway": {
+						Name: "aiGateway1",
+						Elem: &info.Schema{
+							Fields: map[string]*info.Schema{
+								"guardrails": {
+									Name: "guardrails1",
+									Elem: &info.Schema{
+										Fields: map[string]*info.Schema{
+											"input": {
+												Name: "input1",
+												Elem: &info.Schema{
+													Fields: map[string]*info.Schema{
+														"max_batch_size": {
+															CSharpName: "MaxBatchSize1",
+														},
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	res, err := GenerateSchema(context.Background(), GenerateSchemaOptions{
+		ProviderInfo: providerInfo,
+	})
+	require.NoError(t, err)
+	var prettyJSON bytes.Buffer
+	if err := json.Indent(&prettyJSON, res.ProviderMetadata.PackageSchema, "", "    "); err != nil {
+		t.Fatal(err)
+	}
+
+	autogold.ExpectFile(t, autogold.Raw(prettyJSON.String()))
+}
+
+func TestRegressDatabricksNestedCsharpOverrideAttributesSchema(t *testing.T) {
+	t.Parallel()
+	dsSchemaAttributes := dschema.Schema{
+		Attributes: map[string]dschema.Attribute{
+			"endpoints": dschema.ListNestedAttribute{
+				NestedObject: dschema.NestedAttributeObject{
+					Attributes: map[string]dschema.Attribute{
+						"ai_gateway": dschema.ListNestedAttribute{
+							NestedObject: dschema.NestedAttributeObject{
+								Attributes: map[string]dschema.Attribute{
+									"guardrails": dschema.ListNestedAttribute{
+										NestedObject: dschema.NestedAttributeObject{
+											Attributes: map[string]dschema.Attribute{
+												"input": dschema.ListNestedAttribute{
+													NestedObject: dschema.NestedAttributeObject{
+														Attributes: map[string]dschema.Attribute{
+															"max_batch_size": dschema.Int64Attribute{},
+														},
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	schemaProvider := providerbuilder.NewProvider(
+		providerbuilder.NewProviderArgs{
+			AllDataSources: []providerbuilder.DataSource{
+				{
+					Name:             "serving_endpoints",
+					DataSourceSchema: dsSchemaAttributes,
+					ReadFunc: func(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+					},
+				},
+			},
+		},
+	)
+	providerInfo := schemaProvider.ToProviderInfo()
+
+	providerInfo.DataSources["testprovider_serving_endpoints"].Fields = map[string]*info.Schema{
+		"endpoints": {
+			Name: "endpoints1",
+			Elem: &info.Schema{
+				Fields: map[string]*info.Schema{
+					"ai_gateway": {
+						Name: "aiGateway1",
+						Elem: &info.Schema{
+							Fields: map[string]*info.Schema{
+								"guardrails": {
+									Name: "guardrails1",
+									Elem: &info.Schema{
+										Fields: map[string]*info.Schema{
+											"input": {
+												Name: "input1",
+												Elem: &info.Schema{
+													Fields: map[string]*info.Schema{
+														"max_batch_size": {
+															CSharpName: "MaxBatchSize1",
+														},
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	res, err := GenerateSchema(context.Background(), GenerateSchemaOptions{
+		ProviderInfo: providerInfo,
+	})
+	require.NoError(t, err)
+	var prettyJSON bytes.Buffer
+	if err := json.Indent(&prettyJSON, res.ProviderMetadata.PackageSchema, "", "    "); err != nil {
+		t.Fatal(err)
+	}
+
+	autogold.ExpectFile(t, autogold.Raw(prettyJSON.String()))
 }
