1ef41e91da5e17f868df7c7200c96b79ec6d2193
Ian Wahbe <me@iwahbe.com>
2025-01-07 16:40:09 +0100

    [dynamic] send an empty config on destroy calls
    Similar to https://github.com/pulumi/pulumi-terraform-provider/issues/36, older versions
    of terraform-plugin-sdk/v2 require an empty (non-nil) `config` field to avoid crashing.
    
    ```go
    	configVal, err := msgpack.Unmarshal(req.Config.MsgPack, schemaBlock.ImpliedType())
    	if err != nil {
    		resp.Diagnostics = convert.AppendProtoDiag(ctx, resp.Diagnostics, err)
    		return resp, nil
    	}
    ```
    
    ([src](https://github.com/pulumi/terraform-plugin-sdk/blob/1798ae4ae71f7ffff29312a07c92d41205aff03b/helper/schema/grpc_provider.go#L926-L930))
    
    Newer versions of SDKv2 add a
    [guard](https://github.com/pulumi/terraform-plugin-sdk/blob/da29621efae69dd03dfdc540aa5f63beab1b764c/helper/schema/grpc_provider.go#L810-L822)
    before this line is reached.
    
    Fixes #55

--- dynamic/log_replay_test.go
+++ dynamic/log_replay_test.go
@@ -9,25 +9,26 @@ import (
 	"github.com/hashicorp/terraform-plugin-go/tftypes"
 	"github.com/pulumi/providertest/pulumitest/optnewstack"
 	"github.com/pulumi/providertest/pulumitest/opttest"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/resource/plugin"
 	"github.com/stretchr/testify/require"
+	"gotest.tools/v3/assert"
 
 	"github.com/pulumi/pulumi-terraform-bridge/v3/dynamic/internal/shim/grpcutil"
 	v6shim "github.com/pulumi/pulumi-terraform-bridge/v3/dynamic/internal/shim/protov6"
 	"github.com/pulumi/pulumi-terraform-bridge/v3/dynamic/parameterize"
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/pf/tests/pulcheck"
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/pf/tfbridge"
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge/info"
 )
 
 func TestLogReplayProvider(t *testing.T) {
 	t.Parallel()
 	grpcLogs, err := os.ReadFile("./testdata/TestLogReplayProvider/grpc_log_random.json")
-	if err != nil {
-		t.Fatalf("failed to read grpc log: %v", err)
-	}
+	require.NoError(t, err)
 
 	provPlugin := grpcutil.NewLogReplayProvider("random", "0.0.1", grpcLogs)
 	prov := v6shim.New(provPlugin)
-	require.NoError(t, err)
 
 	resp, err := prov.GetProviderSchema(context.Background(), &tfprotov6.GetProviderSchemaRequest{})
 	require.NoError(t, err)
@@ -47,6 +48,50 @@ func TestLogReplayProvider(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, "\x80", string(configResp.PreparedConfig.MsgPack),
 		"the config is is msgpack encoded, so we compare the bytes")
+}
+
+// TestConfigInDestroy asserts that a nil config is passed to SDKv2 providers on destroy.
+//
+// This acts as a regression test for https://github.com/pulumi/pulumi-terraform-provider/issues/55.
+func TestConfigInDestroy(t *testing.T) {
+	t.Parallel()
+
+	logs, err := os.ReadFile("./testdata/TestConfigInDestroy/logs.json")
+	require.NoError(t, err)
+	p := makeLogReplayProvider(t, "phpipam", "0.0.1", logs)
+
+	ctx := context.Background()
+	prov, err := tfbridge.NewProvider(ctx, p, tfbridge.ProviderMetadata{PackageSchema: []byte(`{}`)})
+	require.NoError(t, err)
+
+	resp, err := prov.Delete(ctx, plugin.DeleteRequest{
+		ID:   "12",
+		URN:  "urn:pulumi:prod::space-ipam::phpipam:index/section:Section::section",
+		Name: "section",
+		Type: "phpipam:index/section:Section",
+		Inputs: resource.PropertyMap{
+			"name": resource.NewProperty("Section Name"),
+		},
+		Outputs: resource.PropertyMap{
+			"__meta":                  resource.NewProperty("{\"private_state\":\"bnVsbA==\"}"),
+			"description":             resource.NewProperty(""),
+			"displayOrder":            resource.NewProperty(0.0),
+			"dnsResolverId":           resource.NewProperty(0.0),
+			"editDate":                resource.NewProperty(""),
+			"masterSectionId":         resource.NewProperty(0.0),
+			"name":                    resource.NewProperty("Section Name"),
+			"permissions":             resource.NewProperty(""),
+			"phpipamSectionId":        resource.NewProperty("12"),
+			"sectionId":               resource.NewProperty(12.0),
+			"showSupernetOnly":        resource.NewProperty(false),
+			"showVlanInSubnetListing": resource.NewProperty(false),
+			"showVrfInSubnetListing":  resource.NewProperty(false),
+			"strictMode":              resource.NewProperty(true),
+			"subnetOrdering":          resource.NewProperty(""),
+		},
+	})
+	require.NoError(t, err)
+	assert.Equal(t, plugin.DeleteResponse{}, resp)
 }
 
 type runProvider struct {
