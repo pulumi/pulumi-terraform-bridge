5c51da68e0c4338c503d896c55427f2d6c452cc3
Anton Tayanovskyy <anton@pulumi.com>
2025-01-28 11:20:45 -0500

    Lint

--- pkg/x/muxer/main.go
+++ pkg/x/muxer/main.go
@@ -18,11 +18,12 @@ import (
 	"context"
 	"encoding/json"
 
-	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/providerserver"
 	"github.com/pulumi/pulumi/pkg/v3/codegen/schema"
 	"github.com/pulumi/pulumi/pkg/v3/resource/provider"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/util/contract"
 	pulumirpc "github.com/pulumi/pulumi/sdk/v3/proto/go"
+
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/providerserver"
 )
 
 // Mux multiple rpc servers into a single server by routing based on request type and urn.
