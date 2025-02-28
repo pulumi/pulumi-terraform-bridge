8864b73229e67d42824bb5c16b20f783855ab419
Anton Tayanovskyy <anton@pulumi.com>
2025-02-28 16:28:51 -0500

    PluginFramework encoders now parse "" as nil numbers (#2921)
    This is a workaround until #1667 can guarantee that RawState is parsed
    correctly and passed to migration machinery.
    
    Fixes https://github.com/pulumi/pulumi-terraform-bridge/issues/2903
    
    In theory there should be no harm to be a little tolerant when parsing
    incorrectly typed data, but there may be other use cases that do not
    agree that "" should be interpreted as nil.
    
    For the moment this workaround fixes a popular issue in `pulumi-aws`:
    
        https://github.com/pulumi/pulumi-aws/issues/5222
    
    ---------
    
    Co-authored-by: corymhall <43035978+corymhall@users.noreply.github.com>

---
+++ pkg/convert/number_test.go
@@ -0,0 +1,30 @@
+// Copyright 2016-2025, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package convert
+
+import (
+	"testing"
+
+	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_numberEncoder_emptyStringToNull(t *testing.T) {
+	t.Parallel()
+	n := newNumberEncoder()
+	v, err := n.fromPropertyValue(resource.NewStringProperty(""))
+	require.NoError(t, err)
+	require.True(t, v.IsKnown())
+}
