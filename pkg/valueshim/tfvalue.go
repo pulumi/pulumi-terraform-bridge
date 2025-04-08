1cd2d57601c1214ccf28f9f06243d7278af69adb
Anton Tayanovskyy <anton@pulumi.com>
2025-04-08 18:43:24 -0400

    chore: shim over cty.Value and tftypes.Value representations

---
+++ pkg/valueshim/tfvalue.go
@@ -0,0 +1,308 @@
+// Copyright 2016-2025, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package valueshim
+
+import (
+	"encoding/json"
+	"math/big"
+
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/util/contract"
+)
+
+// Wrap a tftypes.Value as Value.
+func FromTValue(v tftypes.Value) Value {
+	return tValueShim(v)
+}
+
+// Wrap a tftypes.Type as Type.
+func FromTType(t tftypes.Type) Type {
+	return tTypeShim{t: t}
+}
+
+type tValueShim tftypes.Value
+
+var _ Value = (*tValueShim)(nil)
+
+func (v tValueShim) val() tftypes.Value {
+	return tftypes.Value(v)
+}
+
+func (v tValueShim) IsNull() bool {
+	return v.val().IsNull()
+}
+
+func (v tValueShim) GoString() string {
+	return v.val().String()
+}
+
+func (v tValueShim) Type() Type {
+	return FromTType(v.val().Type())
+}
+
+func (v tValueShim) AsValueSlice() []Value {
+	var s []tftypes.Value
+	err := v.val().As(&s)
+	contract.AssertNoErrorf(err, "AsValueSlice failed")
+	res := make([]Value, len(s))
+	for i, v := range s {
+		res[i] = tValueShim(v)
+	}
+	return res
+}
+
+func (v tValueShim) AsValueMap() map[string]Value {
+	var m map[string]tftypes.Value
+	err := v.val().As(&m)
+	contract.AssertNoErrorf(err, "AsValueMap failed")
+	res := make(map[string]Value, len(m))
+	for k, v := range m {
+		res[k] = tValueShim(v)
+	}
+	return res
+}
+
+func (v tValueShim) Marshal() (json.RawMessage, error) {
+	inmem, err := jsonMarshal(v.val(), tftypes.NewAttributePath())
+	if err != nil {
+		return nil, err
+	}
+	return json.Marshal(inmem)
+}
+
+func (v tValueShim) Remove(prop string) Value {
+	if !v.Type().IsObjectType() {
+		return v
+	}
+
+	var m map[string]tftypes.Value
+	err := v.val().As(&m)
+	contract.AssertNoErrorf(err, "AsValueMap failed")
+
+	delete(m, prop)
+
+	t1 := tftypes.Object{}
+	t0 := v.val().Type().(tftypes.Object)
+
+	for k, v := range t0.AttributeTypes {
+		if k == prop {
+			continue
+		}
+		if t1.AttributeTypes == nil {
+			t1.AttributeTypes = make(map[string]tftypes.Type)
+		}
+		t1.AttributeTypes[k] = v
+	}
+	for k, v := range t0.OptionalAttributes {
+		if k == prop {
+			continue
+		}
+		if t1.OptionalAttributes == nil {
+			t1.OptionalAttributes = make(map[string]struct{})
+		}
+		t1.OptionalAttributes[k] = v
+	}
+	return tValueShim(tftypes.NewValue(t1, m))
+}
+
+func jsonMarshal(v tftypes.Value, p *tftypes.AttributePath) (interface{}, error) {
+	if v.IsNull() {
+		return nil, nil
+	}
+	if !v.IsKnown() {
+		return nil, p.NewErrorf("unknown values cannot be serialized to JSON")
+	}
+	typ := v.Type()
+	switch {
+	case typ.Is(tftypes.String):
+		return jsonMarshalString(v, p)
+	case typ.Is(tftypes.Number):
+		return jsonMarshalNumber(v, p)
+	case typ.Is(tftypes.Bool):
+		return jsonMarshalBool(v, p)
+	case typ.Is(tftypes.List{}):
+		return jsonMarshalList(v, p)
+	case typ.Is(tftypes.Set{}):
+		return jsonMarshalSet(v, p)
+	case typ.Is(tftypes.Map{}):
+		return jsonMarshalMap(v, p)
+	case typ.Is(tftypes.Tuple{}):
+		return jsonMarshalTuple(v, p)
+	case typ.Is(tftypes.Object{}):
+		return jsonMarshalObject(v, p)
+	}
+
+	return nil, p.NewErrorf("unknown type %s", typ)
+}
+
+func jsonMarshalString(v tftypes.Value, p *tftypes.AttributePath) (interface{}, error) {
+	var stringValue string
+	err := v.As(&stringValue)
+	if err != nil {
+		return nil, p.NewError(err)
+	}
+	return stringValue, nil
+}
+
+func jsonMarshalNumber(v tftypes.Value, p *tftypes.AttributePath) (interface{}, error) {
+	var n big.Float
+	err := v.As(&n)
+	if err != nil {
+		return nil, p.NewError(err)
+	}
+	return json.Number(n.Text('f', -1)), nil
+}
+
+func jsonMarshalBool(v tftypes.Value, p *tftypes.AttributePath) (interface{}, error) {
+	var b bool
+	err := v.As(&b)
+	if err != nil {
+		return nil, p.NewError(err)
+	}
+	return b, nil
+}
+
+func jsonMarshalList(v tftypes.Value, p *tftypes.AttributePath) (interface{}, error) {
+	var vs []tftypes.Value
+	err := v.As(&vs)
+	if err != nil {
+		return nil, p.NewError(err)
+	}
+	res := make([]any, len(vs))
+	for i, v := range vs {
+		ep := p.WithElementKeyInt(i)
+		e, err := jsonMarshal(v, ep)
+		if err != nil {
+			return nil, ep.NewError(err)
+		}
+		res[i] = e
+	}
+	return res, nil
+}
+
+// Important to preserve original order of tftypes.Value here.
+func jsonMarshalSet(v tftypes.Value, p *tftypes.AttributePath) (interface{}, error) {
+	var vs []tftypes.Value
+	err := v.As(&vs)
+	if err != nil {
+		return nil, p.NewError(err)
+	}
+	res := make([]any, len(vs))
+	for i, v := range vs {
+		ep := p.WithElementKeyValue(v)
+		e, err := jsonMarshal(v, ep)
+		if err != nil {
+			return nil, ep.NewError(err)
+		}
+		res[i] = e
+	}
+	return res, nil
+}
+
+func jsonMarshalMap(v tftypes.Value, p *tftypes.AttributePath) (interface{}, error) {
+	var vs map[string]tftypes.Value
+	err := v.As(&vs)
+	if err != nil {
+		return nil, p.NewError(err)
+	}
+	res := make(map[string]any, len(vs))
+	for k, v := range vs {
+		ep := p.WithElementKeyValue(v)
+		e, err := jsonMarshal(v, ep)
+		if err != nil {
+			return nil, ep.NewError(err)
+		}
+		res[k] = e
+	}
+	return res, nil
+}
+
+func jsonMarshalTuple(v tftypes.Value, p *tftypes.AttributePath) (interface{}, error) {
+	var vs []tftypes.Value
+	err := v.As(&vs)
+	if err != nil {
+		return nil, p.NewError(err)
+	}
+	res := make([]any, len(vs))
+	for i, v := range vs {
+		ep := p.WithElementKeyInt(i)
+		e, err := jsonMarshal(v, ep)
+		if err != nil {
+			return nil, ep.NewError(err)
+		}
+		res[i] = e
+	}
+	return res, nil
+}
+
+func jsonMarshalObject(v tftypes.Value, p *tftypes.AttributePath) (interface{}, error) {
+	var vs map[string]tftypes.Value
+	err := v.As(&vs)
+	if err != nil {
+		return nil, p.NewError(err)
+	}
+	res := make(map[string]any, len(vs))
+	for k, v := range vs {
+		ep := p.WithAttributeName(k)
+		e, err := jsonMarshal(v, ep)
+		if err != nil {
+			return nil, ep.NewError(err)
+		}
+		res[k] = e
+	}
+	return res, nil
+}
+
+type tTypeShim struct {
+	t tftypes.Type
+}
+
+var _ Type = (*tTypeShim)(nil)
+
+func (t tTypeShim) ty() tftypes.Type {
+	return t.t
+}
+
+func (t tTypeShim) IsNumberType() bool {
+	return t.ty().Is(tftypes.Number)
+}
+
+func (t tTypeShim) IsBooleanType() bool {
+	return t.ty().Is(tftypes.Bool)
+}
+
+func (t tTypeShim) IsStringType() bool {
+	return t.ty().Is(tftypes.String)
+}
+
+func (t tTypeShim) IsListType() bool {
+	return t.ty().Is(tftypes.List{})
+}
+
+func (t tTypeShim) IsMapType() bool {
+	return t.ty().Is(tftypes.Map{})
+}
+
+func (t tTypeShim) IsSetType() bool {
+	return t.ty().Is(tftypes.Set{})
+}
+
+func (t tTypeShim) IsObjectType() bool {
+	return t.ty().Is(tftypes.Object{})
+}
+
+func (t tTypeShim) GoString() string {
+	return t.ty().String()
+}
---
+++ pkg/valueshim/tfvalue_test.go
@@ -0,0 +1,266 @@
+// Copyright 2016-2025, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package valueshim_test
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
+	"github.com/hexops/autogold/v2"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/valueshim"
+)
+
+func Test_TValue_IsNull(t *testing.T) {
+	t.Parallel()
+	assert.True(t, valueshim.FromTValue(tftypes.NewValue(tftypes.String, nil)).IsNull())
+	assert.False(t, valueshim.FromTValue(tftypes.NewValue(tftypes.String, "OK")).IsNull())
+}
+
+func Test_TValue_GoString(t *testing.T) {
+	t.Parallel()
+	v := valueshim.FromTValue(tftypes.NewValue(tftypes.String, "OK"))
+	assert.Equal(t, `tftypes.String<"OK">`, v.GoString())
+}
+
+func Test_TValue_Type(t *testing.T) {
+	t.Parallel()
+	v := valueshim.FromTValue(tftypes.NewValue(tftypes.String, "OK"))
+	assert.True(t, v.Type().IsStringType())
+}
+
+func Test_TValue_AsValueSlice(t *testing.T) {
+	t.Parallel()
+	for _, ls := range []tftypes.Type{
+		tftypes.List{ElementType: tftypes.String},
+		tftypes.Set{ElementType: tftypes.String},
+	} {
+		abc := []tftypes.Value{
+			tftypes.NewValue(tftypes.String, "a"),
+			tftypes.NewValue(tftypes.String, "b"),
+			tftypes.NewValue(tftypes.String, "c"),
+		}
+		actual := valueshim.FromTValue(tftypes.NewValue(ls, abc)).AsValueSlice()
+		assert.Equal(t, len(abc), len(actual))
+		for i := 0; i < len(abc); i++ {
+			assert.Equal(t, abc[i].String(), actual[i].GoString())
+		}
+	}
+}
+
+func Test_TValue_AsValueMap(t *testing.T) {
+	t.Parallel()
+	for _, ls := range []tftypes.Type{
+		tftypes.Object{AttributeTypes: map[string]tftypes.Type{
+			"x": tftypes.String,
+			"y": tftypes.String,
+		}},
+		tftypes.Map{ElementType: tftypes.String},
+	} {
+		example := map[string]tftypes.Value{
+			"x": tftypes.NewValue(tftypes.String, "a"),
+			"y": tftypes.NewValue(tftypes.String, "b"),
+		}
+		actual := valueshim.FromTValue(tftypes.NewValue(ls, example)).AsValueMap()
+		assert.Equal(t, len(example), len(actual))
+		for k, v := range example {
+			assert.Equal(t, v.String(), actual[k].GoString())
+		}
+	}
+}
+
+func Test_TValue_Marshal(t *testing.T) {
+	t.Parallel()
+
+	ok := tftypes.NewValue(tftypes.String, "OK")
+	ok2 := tftypes.NewValue(tftypes.String, "OK2")
+	n42 := tftypes.NewValue(tftypes.Number, 42)
+	xyType := tftypes.Object{AttributeTypes: map[string]tftypes.Type{
+		"x": tftypes.String,
+		"y": tftypes.Number,
+	}}
+
+	bigI, _, err := big.ParseFloat("12345678901234567890", 0, 512, big.ToNearestEven)
+	require.NoError(t, err)
+
+	bigF, _, err := big.ParseFloat("1234567890.123456789", 0, 512, big.ToNearestEven)
+	require.NoError(t, err)
+
+	tupType := tftypes.Tuple{ElementTypes: []tftypes.Type{tftypes.String, tftypes.Number}}
+
+	type testCase struct {
+		v      tftypes.Value
+		expect autogold.Value
+	}
+
+	testCases := []testCase{
+		{
+			v:      tftypes.NewValue(tftypes.String, nil),
+			expect: autogold.Expect("null"),
+		},
+		{
+			v:      tftypes.NewValue(tftypes.String, "OK"),
+			expect: autogold.Expect(`"OK"`),
+		},
+		{
+			v:      tftypes.NewValue(tftypes.Number, nil),
+			expect: autogold.Expect("null"),
+		},
+		{
+			v:      tftypes.NewValue(tftypes.Number, 42),
+			expect: autogold.Expect("42"),
+		},
+		{
+			v:      tftypes.NewValue(tftypes.Number, bigI),
+			expect: autogold.Expect("12345678901234567890"),
+		},
+		{
+			v:      tftypes.NewValue(tftypes.Number, bigF),
+			expect: autogold.Expect("1234567890.123456789"),
+		},
+		{
+			v:      tftypes.NewValue(tftypes.Bool, nil),
+			expect: autogold.Expect("null"),
+		},
+		{
+			v:      tftypes.NewValue(tftypes.Bool, true),
+			expect: autogold.Expect("true"),
+		},
+		{
+			v:      tftypes.NewValue(tftypes.List{ElementType: tftypes.String}, nil),
+			expect: autogold.Expect("null"),
+		},
+		{
+			v:      tftypes.NewValue(tftypes.List{ElementType: tftypes.String}, []tftypes.Value{}),
+			expect: autogold.Expect("[]"),
+		},
+		{
+			v:      tftypes.NewValue(tftypes.List{ElementType: tftypes.String}, []tftypes.Value{ok}),
+			expect: autogold.Expect(`["OK"]`),
+		},
+		{
+			v:      tftypes.NewValue(tftypes.List{ElementType: tftypes.String}, []tftypes.Value{ok, ok2}),
+			expect: autogold.Expect(`["OK","OK2"]`),
+		},
+		{
+			v:      tftypes.NewValue(tftypes.Set{ElementType: tftypes.String}, nil),
+			expect: autogold.Expect("null"),
+		},
+		{
+			v:      tftypes.NewValue(tftypes.Set{ElementType: tftypes.String}, []tftypes.Value{}),
+			expect: autogold.Expect("[]"),
+		},
+		{
+			v:      tftypes.NewValue(tftypes.Set{ElementType: tftypes.String}, []tftypes.Value{ok}),
+			expect: autogold.Expect(`["OK"]`),
+		},
+		{
+			v:      tftypes.NewValue(tftypes.Set{ElementType: tftypes.String}, []tftypes.Value{ok, ok2}),
+			expect: autogold.Expect(`["OK","OK2"]`),
+		},
+		{
+			v:      tftypes.NewValue(tftypes.Map{ElementType: tftypes.String}, nil),
+			expect: autogold.Expect("null"),
+		},
+		{
+			v:      tftypes.NewValue(tftypes.Map{ElementType: tftypes.String}, map[string]tftypes.Value{}),
+			expect: autogold.Expect("{}"),
+		},
+		{
+			v: tftypes.NewValue(tftypes.Map{ElementType: tftypes.String},
+				map[string]tftypes.Value{"x": ok}),
+			expect: autogold.Expect(`{"x":"OK"}`),
+		},
+		{
+			v: tftypes.NewValue(tftypes.Map{ElementType: tftypes.String},
+				map[string]tftypes.Value{"x": ok, "y": ok2}),
+			expect: autogold.Expect(`{"x":"OK","y":"OK2"}`),
+		},
+		{
+			v:      tftypes.NewValue(xyType, nil),
+			expect: autogold.Expect("null"),
+		},
+		{
+			v:      tftypes.NewValue(xyType, map[string]tftypes.Value{"x": ok, "y": n42}),
+			expect: autogold.Expect(`{"x":"OK","y":42}`),
+		},
+		{
+			v:      tftypes.NewValue(tupType, nil),
+			expect: autogold.Expect("null"),
+		},
+		{
+			v:      tftypes.NewValue(tupType, []tftypes.Value{ok, n42}),
+			expect: autogold.Expect(`["OK",42]`),
+		},
+	}
+
+	for _, tc := range testCases {
+		raw, err := valueshim.FromTValue(tc.v).Marshal()
+		require.NoError(t, err)
+		tc.expect.Equal(t, string(raw))
+	}
+}
+
+func Test_TValue_Remove(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		v   tftypes.Value
+		exp autogold.Value
+	}
+
+	testCases := []testCase{
+		{
+			v:   tftypes.NewValue(tftypes.Object{}, map[string]tftypes.Value{}),
+			exp: autogold.Expect("tftypes.Object[]<>"),
+		},
+		{
+			v: tftypes.NewValue(tftypes.Object{
+				AttributeTypes: map[string]tftypes.Type{"x": tftypes.String},
+			}, map[string]tftypes.Value{"x": tftypes.NewValue(tftypes.String, "OK")}),
+			exp: autogold.Expect("tftypes.Object[]<>"),
+		},
+		{
+			v: tftypes.NewValue(tftypes.Object{
+				AttributeTypes: map[string]tftypes.Type{
+					"x": tftypes.String,
+					"y": tftypes.Number,
+				},
+			}, map[string]tftypes.Value{
+				"x": tftypes.NewValue(tftypes.String, "OK"),
+				"y": tftypes.NewValue(tftypes.Number, 42),
+			}),
+			exp: autogold.Expect(`tftypes.Object["y":tftypes.Number]<"y":tftypes.Number<"42">>`),
+		},
+	}
+
+	for _, tc := range testCases {
+		tc.exp.Equal(t, valueshim.FromTValue(tc.v).Remove("x").GoString())
+	}
+}
+
+func Test_TType(t *testing.T) {
+	assert.True(t, valueshim.FromTType(tftypes.Bool).IsBooleanType())
+	assert.True(t, valueshim.FromTType(tftypes.String).IsStringType())
+	assert.True(t, valueshim.FromTType(tftypes.Number).IsNumberType())
+	assert.True(t, valueshim.FromTType(tftypes.List{ElementType: tftypes.Number}).IsListType())
+	assert.True(t, valueshim.FromTType(tftypes.Set{ElementType: tftypes.Number}).IsSetType())
+	assert.True(t, valueshim.FromTType(tftypes.Map{ElementType: tftypes.Number}).IsMapType())
+	assert.True(t, valueshim.FromTType(tftypes.Object{}).IsObjectType())
+	assert.Equal(t, "tftypes.Object[]", valueshim.FromTType(tftypes.Object{}).GoString())
+}
