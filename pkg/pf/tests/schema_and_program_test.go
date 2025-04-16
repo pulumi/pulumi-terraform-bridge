838875b1c997a4b39e8c6a4fbbba6ab14e47df82
Venelin <venelin@pulumi.com>
2025-04-16 13:05:06 +0300

    PF handle nil runtime ID

--- pkg/pf/tests/schema_and_program_test.go
+++ pkg/pf/tests/schema_and_program_test.go
@@ -686,3 +686,43 @@ func TestPFAliasesRenameWithAlias(t *testing.T) {
 	pt2.ImportStack(t, stack)
 	pt2.Up(t, optup.ExpectNoChanges())
 }
+
+// A TF provider might misbehave and return a null ID at runtime. This should be handled gracefully.
+func TestPFResourceWithNullId(t *testing.T) {
+	t.Parallel()
+
+	provBuilder := providerbuilder.NewProvider(providerbuilder.NewProviderArgs{
+		AllResources: []providerbuilder.Resource{
+			providerbuilder.NewResource(providerbuilder.NewResourceArgs{
+				Name: "test",
+				ResourceSchema: rschema.Schema{
+					Attributes: map[string]rschema.Attribute{
+						"test": rschema.StringAttribute{Optional: true},
+						"id":   rschema.StringAttribute{Computed: true},
+					},
+				},
+				CreateFunc: func(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+					// NO ID
+					resp.State = tfsdk.State(req.Plan)
+					resp.State.SetAttribute(ctx, path.Root("id"), (*string)(nil))
+				},
+			}),
+		},
+	})
+
+	prov := provBuilder.ToProviderInfo()
+
+	program := `
+    name: test
+    runtime: yaml
+    resources:
+        mainRes:
+            type: testprovider:index/test:Test
+            properties:
+                test: "hello"
+    `
+
+	pt, err := pulcheck.PulCheck(t, prov, program)
+	require.NoError(t, err)
+	pt.Up(t)
+}
