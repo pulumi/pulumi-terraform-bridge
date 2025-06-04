1f7d5d0263476cc8a6e2d054561ef2b84cd70b4d
Venelin <venelin@pulumi.com>
2025-06-04 12:26:14 +0300

    add resource info override options to SDKv2 Diff cross-tests

--- pkg/internal/tests/cross-tests/diff.go
+++ pkg/internal/tests/cross-tests/diff.go
@@ -5,12 +5,15 @@ import (
 	"github.com/zclconf/go-cty/cty"
 
 	crosstestsimpl "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/internal/tests/cross-tests/impl"
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge/info"
 )
 
 type diffOpts struct {
 	deleteBeforeReplace      bool
 	resource2                *schema.Resource
 	skipDiffEquivalenceCheck bool
+	resourceInfo             *info.Resource
+	resourceInfo2Override    *info.Resource
 }
 
 // An option that can be used to customize [Diff].
@@ -31,6 +34,16 @@ func DiffSkipDiffEquivalenceCheck() DiffOption {
 	return func(o *diffOpts) { o.skipDiffEquivalenceCheck = true }
 }
 
+// DiffResourceInfo specifies the resource info to use for the diff.
+func DiffResourceInfo(resourceInfo *info.Resource) DiffOption {
+	return func(o *diffOpts) { o.resourceInfo = resourceInfo }
+}
+
+// DiffResourceInfo2Override specifies an optional override for the resource info to use for the second provider.
+func DiffResourceInfo2Override(resourceInfo *info.Resource) DiffOption {
+	return func(o *diffOpts) { o.resourceInfo2Override = resourceInfo }
+}
+
 func Diff(
 	t T, resource *schema.Resource, config1, config2 map[string]cty.Value, opts ...DiffOption,
 ) crosstestsimpl.DiffResult {
@@ -49,5 +62,7 @@ func Diff(
 		DeleteBeforeReplace:      o.deleteBeforeReplace,
 		Resource2:                o.resource2,
 		SkipDiffEquivalenceCheck: o.skipDiffEquivalenceCheck,
+		ResourceInfo:             o.resourceInfo,
+		ResourceInfo2Override:    o.resourceInfo2Override,
 	})
 }
