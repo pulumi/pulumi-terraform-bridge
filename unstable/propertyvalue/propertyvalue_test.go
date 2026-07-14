30533b14b3173602ce4a5c8be42fc515bcfc407e
Ian Wahbe <me@iwahbe.com>
2026-07-14 13:18:34 +0200

    Fix gci import ordering in propertyvalue test
    The pkg/v3 import must sort before the sdk/v3 import within the group.

--- unstable/propertyvalue/propertyvalue_test.go
+++ unstable/propertyvalue/propertyvalue_test.go
@@ -17,8 +17,8 @@ package propertyvalue
 import (
 	"testing"
 
-	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
 	rtesting "github.com/pulumi/pulumi/pkg/v3/resource/testing"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
 	"github.com/stretchr/testify/require"
 	"pgregory.net/rapid"
 )
